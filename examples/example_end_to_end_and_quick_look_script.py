5675cae7ed0b7ccd4ab135baf3d2c0600babc18a
gsteranka <gsterank@wellesley.edu>
2018-03-30 13:33:37 -0400

    Update example_end_to_end_and_quick_look_script.py

--- examples/example_end_to_end_and_quick_look_script.py
+++ examples/example_end_to_end_and_quick_look_script.py
@@ -18,7 +18,6 @@ import numpy as np
 
 from rsr_reader import RSRReader
 from freq_offset import calc_freq_offset
-#from geo_file_into_instance import geo_file_into_instance
 from calc_geometry_v2 import Geometry
 from freq_offset_fit import FreqOffsetFit
 from power_normalization import Normalization
@@ -26,9 +25,12 @@ from make_cal_file import make_cal_file
 from make_geo_file import make_geo_file
 from norm_diff_class import NormDiff
 
-rsr_file = 'rev007E_X43/S10EAOE2005_123_0740NNNX43D.2A1'
-#geo_file = 'rev007E_X43/rev007_E_X_geo.tab'
-geo_file = 'rev007E_X43/test_geo.tab'
+# Change to location of rsr file, and where to write out geo
+# and cal files
+rsr_file = '/your/path/name/here/example_rsr_file'
+geo_file = '/your/path/name/here/example_geo_file.tab'
+cal_file = '/your/path/name/here/example_cal_file.tab'
+obs_file = '/your/path/name/here/example_obs_file.tab'
 
 f_uso = 8427222034.34050
 kernels_dir = '../../../../kernels~/'
@@ -40,15 +42,10 @@ kernel_files = ['de421.bsp',
                 'naif0012.tls']
 kernels = [kernels_dir + this_kernel for this_kernel in kernel_files]
 
-#cal_file = 'rev007E_X43/rev007_E_X43_cal_example.tab'
-cal_file = 'rev007E_X43/test_cal.tab'
-
 dr_km_desired = 0.25
 rho_km_range = [70000, 155000]
 res_km = 'I do nothing yet'
 window_type = 'I do nothing yet'
-
-obs_file = 'rev007E_X43/test_obs.tab'
 
 rsr_inst = RSRReader(rsr_file)
 (spm_vals, IQ_m) = rsr_inst.get_IQ()

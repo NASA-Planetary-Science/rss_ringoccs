dfe7fd7fe8245fa0dfc212ae082bdfda32aa817f
Unknown <ryan_maguire@student.uml.edu>
2018-06-14 14:18:45 -0400

    rss_ringoccs
    __init__ .py file

--- rss_ringoccs/__init__.py
+++ rss_ringoccs/__init__.py
@@ -1,6 +1,32 @@
 """
-    This is a test
-    The best test
-    Like, ever
-        Hey
+    Package:
+        rss_ringoccs
+    Purpose:
+        Provide tools for analysis of ring occultation experiments,
+        particularly those pertaining to the Cassini Radio Science
+        Experiment. Mathematical procedures for diffraction
+        correction exist, as well as geometrical codes and tools, and
+        methods for reading RSR and extracting data. In addition,
+        there are several 'utilities' to aid the user in their data
+        analysis, as well as mathematical functions and routines for
+        modelling both diffraction and diffraction correction based
+        on the given geometry.
+    Dependencies:
+        [01] numpy
+        [02] spiceypy
+        [03] scipy
+        [04] time
+        [05] sys
+        [06] subprocess
+        [07] pdb
+        [08] matplotlib
+        [09] tkinter
+        [10] multiprocessing
+        [11] mayavi
+        [12] os
+        [13] platform
+    References:
+        [1] https://github.com/NASA-Planetary-Science/rss_ringoccs/
+    History:
+        Created: RJM - 2018/05/16 5:49 P.M.
 """

a7bb3a5adc8f64d11b3f554e26af437b8ed8a2f1
Jolene Fong <jfong@wellesley.edu>
2018-06-26 17:05:11 -0400

    debugged for occs spanning several days

--- rss_ringoccs/tools/et_to_spm.py
+++ rss_ringoccs/tools/et_to_spm.py
@@ -6,44 +6,53 @@ et_to_spm.py
 Purpose: Convert ephemeris time to seconds past midnight (SPM).
 
 Revisions:
-	2018 Mar 13 - jfong - original
+    2018 Mar 13 - jfong - original
+        2018 Jun 26 - jfong - add load_kernels if kernels present
+                            - debug for occs longer than a day
 """
 
 import numpy as np
 import spiceypy as spice
+import pdb
 
 def et_to_spm(et_vals, kernels=None):
-	"""Function to convert ephemeris time to SPM"""
+    """Function to convert ephemeris time to SPM"""
 
-	npts = len(et_vals)
+    if kernels:
+        spice.kclear()
+        for kernel in kernels:
+            spice.furnsh(kernel)
 
-	spm_vals = []
+    npts = len(et_vals)
 
-	for n in range(npts):
-		et = et_vals[n]
+    spm_vals = []
 
-		# Convert ET to UTC string
-		utc_str = spice.et2utc(et, "ISOD", 16)
-		
-		# Extract hour, minute, and second from UTC string
-		hour = float(utc_str[9:11])
-		minute = float(utc_str[12:14])
-		second = float(utc_str[15:])
-		spm = hour*3600. + minute*60. + second
-		spm_vals.append(spm)
+    for n in range(npts):
+        et = et_vals[n]
 
-	# Check if event goes through midnight
-	dr_vals = spm_vals - np.roll(spm_vals, 1)
-	ibrk = np.argwhere(dr_vals[1:] < 0)
+        # Convert ET to UTC string
+        utc_str = spice.et2utc(et, "ISOD", 16)
+        
+        # Extract hour, minute, and second from UTC string
+        hour = float(utc_str[9:11])
+        minute = float(utc_str[12:14])
+        second = float(utc_str[15:])
+        spm = hour*3600. + minute*60. + second
+        spm_vals.append(spm)
 
-	if ibrk.size > 0:
-		ind = ibrk[0] + 1
-		spm_brk = spm_vals[ind]
-		spm_vals_cont = np.zeros(npts)
-		spm_vals_cont[0:ind] = spm_vals[0:ind]
-		spm_vals_cont[ind+1:] = spm_vals[ind+1:] + spm_brk
-		spm_vals = spm_vals_cont
-	
-	return np.array(spm_vals)
+    # Check if event goes through midnight
+    dr_vals = spm_vals - np.roll(spm_vals, 1)
+#    ibrk = np.argwhere(dr_vals[1:] < 0)
+    ibrk = np.argwhere(dr_vals < 0)
+
+    if ibrk.size > 0:
+        spm_vals_cont = np.asarray(spm_vals)
+        for index in range(len(ibrk)):
+            ind = ibrk[index][0] 
+            spm_brk = spm_vals[ind-1]
+            spm_vals_cont[ind:] = spm_vals_cont[ind:] + spm_brk
+        spm_vals = spm_vals_cont
+
+    return np.array(spm_vals)
 
 

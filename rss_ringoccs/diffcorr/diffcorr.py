96cbc49e399724695395391ef019c1a30e53b51b
ryanmaguire <rmaguire@protonmail.com>
2018-07-23 13:59:04 -0400

    Further tweaks to diffcorr
    Simple changes in commenting styles and syntax. Nothing major from the last change.

--- rss_ringoccs/diffcorr/diffcorr.py
+++ rss_ringoccs/diffcorr/diffcorr.py
@@ -9,56 +9,13 @@
         Several low-level functions that perform error checks for
         the main functions also exist, as well as functions that
         enable running shell scripts in Python.
-    
-    Window (Taper) Functions:
-        rect................Rectangular window.
-        coss................Squared cossine window.
-        kb20................Kaiser-Bessel 2.0 window.
-        kb25................Kaiser-Bessel 2.5 window.
-        kb35................Kaiser-Bessel 3.5 window.
-        kbmd20..............Modified Kaiser-Bessel 2.0 window.
-        kbmd25..............Modified Kaiser-Bessel 2.5 window.
-    
-    Special Functions:
-        fresnel_sin.........The Fresnel sine integral.
-        fresnel_cos.........The Fresnel cosine integral.
-        sq_well_solve.......Diffraction pattern through square well.
-
-    Mathematical Functions:
-        compute_norm_eq.....Computes the normalized equivalent width.
-        get_norm_eq.........Quickly retrieve pre-computed normalized
-                            equivalent widths from strings with the
-                            name of common window functions.
-        resolution_inverse..Computes the inverse of the function
-                            y = x/(exp(-x)+x-1)
-        power_func..........Compute power from complex transmittance.
-        phase_func..........Compute phase from complex transmittance.
-        tau_func............Compute normalized optical depth from the
-                            complex transmittance.
-        wker................Computes a weighted kernel function.
-        freq_wav............Convert frequency to wavelength, and
-                            vice-versa. Kilometers or Herts only.
-        fresnel_scale.......Compute the Fresnel scale.
-    
-    Miscellaneous Functions:
-        get_range_request...Computes an array of the form [a,b] from
-                            a given array, list, or from a set of
-                            allowed strings.
-        get_range_actual....Given an array of numbers (usually the
-                            radial range of the data), a range
-                            request, and a window width, compute the
-                            allowed range of processing.
 """
 # Import dependencies for the diffcorr module
-import time,os,sys,platform
+import time,os,platform
 import numpy as np
 from scipy.special import lambertw, iv
-from scipy import interpolate
 from scipy.constants import speed_of_light
-sys.path.append("%s/../../" % os.path.dirname(os.path.realpath(__file__)))
-from rss_ringoccs.tools import check_boole,check_real
-from rss_ringoccs.tools import check_pos_real,check_complex
-import pdb
+from rss_ringoccs.tools import check_boole,check_real,check_pos_real
 
 region_dict = {
     'all'               : [65000.0,145000.0],
@@ -109,10 +66,9 @@ class rec_data(object):
         if not check_boole(bfac):
             raise TypeError("bfac must be Boolean: True/False")
         if (sigma == False): pass
-        else:
-            if (not check_pos_real(sigma)):
+        elif (not check_pos_real(sigma)):
                 raise TypeError("sigma must be a positive number.")
-            else: pass
+        else: pass
 
         self.res                     = None
         self.wtype                   = None
@@ -392,7 +348,6 @@ class diffraction_correction(object):
         self.history = self.__write_hist_dict()
 
         t2 = time.time()
-        sys.stdout.write("\033[K")
         print("Computation Time: ",t2-t1,end="\r")
 
     def __rect(w_in, dx):

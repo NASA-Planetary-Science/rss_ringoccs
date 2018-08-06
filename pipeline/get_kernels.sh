5ab12eb4dc75a9961c0ef826dc990c73760794d3
ryanmaguire <rmaguire@protonmail.com>
2018-08-06 09:57:01 -0400

    Added error checks to the get_kernels.sh shell script.
    get_kernels can now receive either 1 or 2 inputs. The first input MUST be the location of the list_of_kernels.txt text file containing all of the kernels that are to be downloaded. The second is where on your local machine you wish to download the kernels too. If the second input is left black, the current directory will be used. That is, if no second argument is given get_kernels.sh behaves exactly as it used to. If 3 or more arguments, or if no arguments, are given and error message is produced.

--- pipeline/get_kernels.sh
+++ pipeline/get_kernels.sh
@@ -3,31 +3,44 @@ if [ ! "$BASH_VERSION" ] ; then
 	echo "Please use BASH to run this script ($0)" 1>&2
 	exit 1
 fi
-if [ "$#" -ne 1 ]; then
-	echo "Illegal number of arguments: Only one required"
-	echo "Useage: get_kernels.sh list_of_kernels.txt"
+if [ "$#" != 1 ] && [ "$#" != 2 ]; then
+	echo "Illegal number of arguments: 2 required. Number given: $#"
+	echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
 	echo "Example: list_of_kernels.txt containing:"
 	echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
 	echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
-	echo "./get_kernels list_of_kernels.txt"
+	echo "./get_kernels list_of_kernels.txt ../kernels/"
 	exit 1
 fi
 web="https://naif.jpl.nasa.gov/pub/"
 input="$1"
 while IFS= read -r dirfil
 do
+	if [ "$#" = 1 ]; then
+		localdirfil=$dirfil
+	elif [ "$#" = 2 ]; then
+		localdirfil="$2$dirfil"
+	else
+		echo "Illegal number of arguments: Two required"
+		echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
+		echo "Example: list_of_kernels.txt containing:"
+		echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
+		echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
+		echo "./get_kernels list_of_kernels.txt ../kernels/"
+		exit 1
+	fi
 	webdirfil=$web$dirfil
-	if [ ! -e $dirfil ]
+	if [ ! -e $localdirfil ]
 	then
-    		echo Current File $dirfil does not locally exist. Fetching.
+    		echo Current File $localdirfil does not locally exist. Fetching.
 		if curl --output /dev/null --silent --head --fail "$webdirfil"; then
  			echo "URL exists: $webdirfil"
-			curl --create-dirs -o $dirfil $webdirfil
+			curl --create-dirs -o $localdirfil $webdirfil
 		else
 			echo "URL does not exist: $webdirfil"
 			exit 1
 		fi
 	else
-    		echo Current File "$dirfil" already exists.
+    		echo Current File "$localdirfil" already exists.
 	fi
 done < "$input"

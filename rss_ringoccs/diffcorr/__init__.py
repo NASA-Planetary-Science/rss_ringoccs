179c450069af2cbb5199605a3ae01e7b0bb27635
ryanmaguire <rmaguire@protonmail.com>
2018-08-07 18:34:21 -0400

    Changed the diffcorr __init__ file.
    Now only the diffraction_correction submodule, which only contains the DiffractionCorrection class, is loaded by the __init__.py script. The doc string has been updated to reflect the current state of the diffcorr subpackage.

--- rss_ringoccs/diffcorr/__init__.py
+++ rss_ringoccs/diffcorr/__init__.py
@@ -1,15 +1,45 @@
 """
-    Module Name:
+    Subpackage Name:
         diffcorr
     Purpose:
         Provide functions and classes that aid in the process of
         Diffraction Correction / Fresnel Inversion. Additional
         functions for the purpose of forward modelling of
         reconstructed data and diffraction modelling are included.
-        Several low-level functions that perform error checks for
-        the main functions also exist, as well as functions that
-        enable running shell scripts in Python.
+        Special mathematical functions and solutions to common
+        problems in diffraction theory are also included.
     
+    Sub-Modules:
+        advanced_tools:
+            This submodule is good for modeling the geometry of a
+            given occultation, and for comparing your results to
+            the results obtained by others (Ex. Results on the PDS).
+            This submodule contains the following:
+                compare_tau (Class):
+                    Used for running diffraction correction on a
+                    given set of diffracted data and then comparing
+                    the outcome to a given set of reconstructed data.
+                find_optimal_resolution (Class):
+                    Given a set of data and a reconstruction, this 
+                    class will run diffraction correction over a set
+                    of resolutions. The output contains the L_2 and
+                    L_infinity difference of the two reconstructions
+                    as a function of resolution.
+                delta_impulse_diffraction (Class):
+                    Given a set of geometry data, this class create
+                    modeled data of the solution of diffraction
+                    through a Dirac-Delta impulse function.
+                    Reconstruction is then perform on the mock-data.
+                    This tool is good for modeling the problem and
+                    determining resolution constraints based on the
+                    geometry available.
+                double_slit_diffraction (Class):
+                    Similar to delta_impulse_diffraction but with a
+                    double slit (Two square wells) instaead of a
+                    Dirac-Delta impulse function.
+        
+        diffraction_correction:
+            This
     Window (Taper) Functions:
         rect................Rectangular window.
         coss................Squared cossine window.
@@ -56,10 +86,6 @@
                             request, and a window width, compute the
                             allowed range of processing.
 """
-
-# Import advanced tools used for optimization and modeling.
-from .advanced_tools import compare_tau,find_optimal_resolution
-from .advanced_tools import delta_impulse_diffraction
 
 # Import the main classes used in diffraction reconstruction.
 from .diffraction_correction import DiffractionCorrection

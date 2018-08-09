69fa96aa1ae482241b4cb946907b3122cb4deb73
gsteranka <gsterank@wellesley.edu>
2018-08-09 16:14:53 -0400

    MakeCalInst --> CreateCalInst. Change __init__ file in tools to reflect this and eliminating make_*_file functions

--- rss_ringoccs/tools/__init__.py
+++ rss_ringoccs/tools/__init__.py
@@ -7,10 +7,7 @@ tools sub-package of rss_ringoccs package
 from .spm_to_et import spm_to_et
 from .et_to_spm import et_to_spm
 from .cassini_blocked import cassini_blocked
-from .make_cal_file import make_cal_file
-from .make_geo_file import make_geo_file
-from .make_dlp_file import make_dlp_file
-from .make_cal_inst import MakeCalInst
+from .cal_inst_from_file import CreateCalInst
 from .pds3_reader import PDS3Reader
 from .date_to_rev import date_to_rev
 from .get_rev_info import get_rev_info

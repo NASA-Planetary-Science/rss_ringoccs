a0c86996fa43ff3da1ece6ecb783b900968b9238
ryanmaguire <rmaguire@protonmail.com>
2018-09-18 16:17:31 -0400

    Removing tutorials directory.

--- rss_ringoccs/diffcorr/special_functions.py
+++ rss_ringoccs/diffcorr/special_functions.py
@@ -4,11 +4,12 @@ from math import factorial
 
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     """
-        Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-        The Savitzky-Golay filter removes high frequency noise from data.
-        It has the advantage of preserving the original shape and
-        features of the signal better than other types of filtering
-        approaches, such as moving averages techniques.
+        Smooth (and optionally differentiate) data with a
+        Savitzky-Golay filter. The Savitzky-Golay filter removes
+        high frequency noise from data. It has the advantage of
+        preserving the original shape and features of the signal
+        better than other types of filtering approaches, such as
+        moving averages techniques.
         Parameters
         ----------
         y : array_like, shape (N,)
@@ -57,14 +58,19 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
         order = np.abs(np.int(order))
     except ValueError:
         raise ValueError("window_size and order have to be of type int")
+
     if window_size % 2 != 1 or window_size < 1:
         raise TypeError("window_size size must be a positive odd number")
-    if window_size < order + 2:
+    elif window_size < order + 2:
         raise TypeError("window_size is too small for the polynomials order")
-    order_range = range(order+1)
+    else:
+        pass
+
+    order_range = np.arange(order+1)
     half_window = (window_size -1) // 2
+
     # precompute coefficients
-    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    b = np.mat([[k**i for i in order_range] for k in np.arange(-half_window, half_window+1)])
     m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
     # pad the signal at the extremes with
     # values taken from the signal itself

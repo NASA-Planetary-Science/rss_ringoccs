8b84e2b8b30ecdded2a649cbc0f16a0dfc7b68f7
ryanmaguire <rmaguire@protonmail.com>
2018-09-18 15:52:54 -0400

    Correct sourcing and editing of .bash_profile in config script.
    If you source that .bash_profile in a subshell, then it will become unsourced once the script has completed. To circumvent this, do . ./rss_ringoccs_config.sh. That is DOT SPACE DOT SLASH stuffs. Added a check to see if the PATH variable points to the proper directory for anaconda and spiceypy.

--- rss_ringoccs_config.sh
+++ rss_ringoccs_config.sh
@@ -16,19 +16,22 @@ else
 	exit 1
 fi
 
+echo "Running rss_ringoccs_config Script:"
+
 web="https://repo.continuum.io/archive/"
 webfil="$web$fil"
 
 # Go to your home directory
+MY_DIRECTORY = $(pwd)
 cd ~
 
 # Check to see if anaconda3 exists on your computer.
 if [ -d anaconda3 ]; then
-	echo "You already have Anaconda on your computer."
-	echo "Running updates..."
+	echo -e ' \t ' "You already have Anaconda on your computer."
+	echo -e ' \t ' "Running updates..."
 else
-	echo "Anaconda3 does not currently exist on your computer."
-	echo "Running setup scripts now..."
+	echo -e ' \t ' "Anaconda3 does not currently exist on your computer."
+	echo -e ' \t ' "Running setup scripts now..."
 
 	# Use cURL to download Anaconda 5.1.0.
 	curl -Ok "$webfil"
@@ -40,13 +43,28 @@ else
 	rm -f "$fil"
 fi
 
+# Check that anaconda3 is included in your path
+echo -e ' \t ' "Checking your PATH variable..."
+if [[ ":$PATH:" == *"~/anaconda3/bin"* ]]; then
+  echo -e ' \t ' "PATH variable is set"
+else
+	echo -e ' \t ' "Adding ~/anaconda3/bin to your PATH"
+	echo 'export "PATH=~/anaconda3/bin:$PATH"' >>~/.bash_profile
+fi
+
+# Source .bash_profile to update you path.
+echo -e ' \t ' "Sourcing .bash_profile"
+echo -e ' \t ' "Make sure you are using Bash when running rss_ringoccs"
+source .bash_profile
+
 # Update conda
 conda update conda
 
 # Install Spiceypy
 conda install -c https://conda.anaconda.org/andrewannex spiceypy
 
-# Source .bash_profile to update you path.
-echo "Sourcing .bash_profile"
-echo "Make sure you are using Bash when running rss_ringoccs"
+# Source the .bash_profile again after conda updates.
 source .bash_profile
+
+# Change back to working directory
+cd "$MY_DIRECTORY"

28cf641673d3cf7550275bf6b15ec0aff8af8adb
Ryan Maguire <ryan_maguire@student.uml.edu>
2018-09-19 19:30:38 -0400

    Updated and added error checks to all functions.

--- rss_ringoccs/diffcorr/special_functions.py
+++ rss_ringoccs/diffcorr/special_functions.py
@@ -1,6 +1,6 @@
 import numpy as np
-from scipy.special import erf
-from math import factorial
+from scipy.special import erf, lambertw
+SQRT_PI_2 = 0.886226925452758013649084
 
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     """
@@ -66,12 +66,15 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     else:
         pass
 
-    order_range = np.arange(order+1)
+    order_range = range(order+1)
     half_window = (window_size -1) // 2
 
     # precompute coefficients
-    b = np.mat([[k**i for i in order_range] for k in np.arange(-half_window, half_window+1)])
-    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = 1
+    for i in range(deriv):
+        m *= rate*(deriv-i)
+    m *= np.linalg.pinv(b).A[deriv]
     # pad the signal at the extremes with
     # values taken from the signal itself
     firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
@@ -86,23 +89,19 @@ def resolution_inverse(x):
         Purpose:
             Compute the inverse of y = x/(exp(-x)+x-1)
         Variables:
-            x:      A real or complex number.
+            x:
+                A real or complex number, or numpy array.
         Outputs:
-            f:      The inverse of x/(exp(-x)+x-1)
+            f:
+                The inverse of x/(exp(-x)+x-1)
         Dependencies:
             [1] numpy
             [2] scipy.special
-            [3] sys
-        Notes:
-            If the input is not in the atypes list, this function
-            will execute a system exit, returning the user to
-            whichever shell they ran this function from. This
-            function wants ints, floats, complex numbers, or arrays.
         Method:
             The inverse of x/(exp(-x)+x-1) is computed using the
             LambertW function. This function is the inverse of
             y = x * exp(x). This is computed using the scipy.special
-            lambertw function.
+            subpackage using their lambertw function.
         Warnings:
             [1] The real part of the argument must be greater than 1.
             [2] The scipy.special lambertw function is slightly
@@ -113,35 +112,76 @@ def resolution_inverse(x):
             [2] https://en.wikipedia.org/wiki/Lambert_W_function
         Examples:
             Plot the function on the interval (1,2)
-                In [1]: import diffcorr as dc
+                In [1]: import rss_ringoccs.diffcorr.special_functions as sf
                 In [2]: import numpy as np
                 In [3]: x = np.array(range(0,1001))*0.001+1.01
-                In [4]: y = dc.resolution_inverse(x)
+                In [4]: y = sf.resolution_inverse(x)
                 In [5]: import matplotlib.pyplot as plt
                 In [6]: plt.show(plt.plot(x,y))
                 (Beautiful plots appear here)
         History:
             Translated from IDL: RJM - 2018/05/15 1:49 P.M.
+            Added error checks: RJM - 2018/09/19 6:57 P.M.
     """
-    if (not check_real(x)) and (not check_complex(x)):
-        raise TypeError("Input must be real or complex")
-    f = ((x - 1) * lambertw(np.exp(x / (1 - x)) * x / (1 - x)) + x) / (x - 1)
+    y = x
+    try:
+        x = np.array(x)
+        if (not np.all(np.isreal(x))) and (not np.all(np.iscomplex(x))):
+            raise TypeError(
+                "\n\tError Encountered:\n"
+                "\trss_ringoccs: Diffcorr Subpackage\n"
+                "\tspecial_function.resolution_inverse:\n"
+                "\t\tInput must be a real or complex valued numpy array.\n"
+                "\t\tThe elements of your array have type: %s"
+                % (x.dtype)
+            )
+        else:
+            del y
+    except (TypeError, ValueError) as errmes:
+        raise TypeError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.resolution_inverse:\n"
+            "\t\tInput must be a real or complex valued numpy array.\n"
+            "\t\tYour input has type: %s\n"
+            "\tOriginal Error Mesage: %s\n"
+            % (type(y).__name__, errmes)
+        )
+    if (np.min(np.real(x)) <= 1.0):
+        raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.resolution_inverse:\n"
+            "\t\tThis function is only defined for inputs\n"
+            "\t\twhose real part is greater than 1.\n"
+            "\t\tYour input has real minimum: %f\n"
+            % (np.min(np.real(x)))
+        )
+    else:
+        P1 = x/(1.0-x)
+        P2 = P1*np.exp(P1)
+        f = lambertw(P2)-P1
+
+    if np.all(np.isreal(x)):
+        f = np.real(f)
+
     return f
 
-def fresnel_cos(x_in):
+def fresnel_cos(x):
     """
         Function:
             fresnel_cos
         Purpose:
             Compute the Fresnel cosine function.
         Variables:
-            x_in:   A real or complex argument.
+            x:
+                A real or complex number, or numpy array.
         Outputs:
-            f_cos:  The fresnel cosine integral of x_in.
+            f_cos:
+                The fresnel cosine integral of x.
         Dependences:
-            [1] diffcorr
-            [2] numpy
-            [3] sys
+            [1] numpy
+            [2] scipy
         Notes:
             [1] The Fresnel Cosine integral is the solution to the
                 equation dy/dx = cos(pi/2 * x^2), y(0) = 0. In other
@@ -155,7 +195,7 @@ def fresnel_cos(x_in):
                 to use this to solve for the Fresnel Cosine integral.
             [3] The Fresnel Cosine integral is used for the solution
                 of diffraction through a square well. Because of this
-                is is useful for forward modeling problems in 
+                it is useful for forward modeling problems in 
                 radiative transfer and diffraction.
         References:
             [1] https://en.wikipedia.org/wiki/Fresnel_integral
@@ -164,38 +204,62 @@ def fresnel_cos(x_in):
             [4] http://mathworld.wolfram.com/Erf.html
         Examples:
             Compute and plot the Fresnel Cosine integral.
-                In [1]: import diffcorr as dc
+                In [1]: import rss_ringoccs.diffcorr.special_functions as sf
                 In [2]: import numpy as np
                 In [3]: import matplotlib.pyplot as plt
                 In [4]: x = np.array(range(0,10001))*0.01 - 50.0
-                In [5]: y = dc.fresnel_cos(x)
+                In [5]: y = sf.fresnel_cos(x)
                 In [6]: plt.show(plt.plot(x,y))
         History:
             Translated from IDL:     RJM - 2018/05/14 2:13 P.M.
             Allow complex arguments: RJM - 2018/05/16 1:25 P.M.
+            Updated and added error check: RJM - 2018/09/19 7:00 P.M.
     """
-    if (not isinstance(x_in, np.ndarray)):
-        raise TypeError("Input must be real or complex.")
-    f_cos = ((1-1j)/4.0)*erf((1+1j)*x_in*np.sqrt(np.pi)/2.0)+\
-        ((1+1j)/4.0)*erf((1-1j)*x_in*np.sqrt(np.pi) / 2.0)
-    if (np.isreal(x_in).all()):
+    y = x
+    try:
+        x = np.array(x)
+        if (not np.all(np.isreal(x))) and (not np.all(np.iscomplex(x))):
+            raise TypeError(
+                "\n\tError Encountered:\n"
+                "\trss_ringoccs: Diffcorr Subpackage\n"
+                "\tspecial_function.fresnel_cos:\n"
+                "\t\tInput must be a real or complex valued numpy array.\n"
+                "\t\tThe elements of your array have type: %s"
+                % (x.dtype)
+            )
+        else:
+            del y
+    except (TypeError, ValueError) as errmes:
+        raise TypeError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.fresnel_cos:\n"
+            "\t\tInput must be a real or complex valued numpy array.\n"
+            "\t\tYour input has type: %s\n"
+            "\tOriginal Error Mesage: %s\n"
+            % (type(y).__name__, errmes)
+        )
+    f_cos = ((0.25-0.25j)*erf((1.0+1.0j)*x*SQRT_PI_2)+
+            (0.25+0.25j)*erf((1.0-1.0j)*x*SQRT_PI_2))
+    if (np.isreal(x).all()):
         f_cos = np.real(f_cos)
     return f_cos
 
-def fresnel_sin(x_in):
+def fresnel_sin(x):
     """
         Function:
             fresnel_sin
         Purpose:
             Compute the Fresnel sine function.
         Variables:
-            x_in:   A real or complex argument.
+            x:
+                A real or complex argument, or numpy array.
         Outputs:
-            f_cos:  The fresnel sine integral of x_in.
+            f_sin:
+                The fresnel sine integral of x.
         Dependences:
-            [1] diffcorr
-            [2] numpy
-            [3] sys
+            [1] numpy
+            [2] scipy
         Notes:
             [1] The Fresnel sine integral is the solution to the
                 equation dy/dx = sin(pi/2 * x^2), y(0) = 0. In other
@@ -217,51 +281,249 @@ def fresnel_sin(x_in):
             [3] http://mathworld.wolfram.com/FresnelIntegrals.html
             [4] http://mathworld.wolfram.com/Erf.html
         Examples:
-            Compute and plot the Fresnel Cosine integral.
-                In [1]: import diffcorr as dc
+            Compute and plot the Fresnel Sine integral.
+                In [1]: import rss_ringoccs.diffcorr.special_functions as sf
                 In [2]: import numpy as np
                 In [3]: import matplotlib.pyplot as plt
                 In [4]: x = np.array(range(0,10001))*0.01 - 50.0
-                In [5]: y = dc.fresnel_sin(x)
+                In [5]: y = sf.fresnel_sin(x)
                 In [6]: plt.show(plt.plot(x,y))
         History:
             Translated from IDL: RJM - 2018/05/14 3:53 P.M.
             Allow complex arguments: RJM - 2018/05/16 1:26 P.M.
+            Updated and added error check: RJM - 2018/09/19 7:01 P.M.
     """
-    if (not isinstance(x_in, np.ndarray)):
-        raise TypeError("Input must be real or complex.")
-    f_sin = ((1+1j)/4.0)*erf((1+1j)*x_in*np.sqrt(np.pi)/2.0)+\
-    ((1-1j)/4.0)*erf((1-1j)*x_in*np.sqrt(np.pi)/2.0)
-    if (np.isreal(x_in).all()):
+    y = x
+    try:
+        x = np.array(x)
+        if (not np.all(np.isreal(x))) and (not np.all(np.iscomplex(x))):
+            raise TypeError(
+                "\n\tError Encountered:\n"
+                "\trss_ringoccs: Diffcorr Subpackage\n"
+                "\tspecial_function.fresnel_cos:\n"
+                "\t\tInput must be a real or complex valued numpy array.\n"
+                "\t\tThe elements of your array have type: %s"
+                % (x.dtype)
+            )
+        else:
+            del y
+    except (TypeError, ValueError) as errmes:
+        raise TypeError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.fresnel_cos:\n"
+            "\t\tInput must be a real or complex valued numpy array.\n"
+            "\t\tYour input has type: %s\n"
+            "\tOriginal Error Mesage: %s\n"
+            % (type(y).__name__, errmes)
+        )
+    f_sin = ((0.25+0.25j)*erf((1.0+1.0j)*x*SQRT_PI_2)+
+            (0.25-0.25j)*erf((1.0-1.0j)*x*SQRT_PI_2))
+    if (np.isreal(x).all()):
         f_sin = np.real(f_sin)
     return f_sin
 
 def single_slit_diffraction_solve(x, z, a):
+    """
+        Function:
+            single_slit_diffraction_solve
+        Purpose:
+            Compute diffraction through a single slit for the
+            variable x with a distance z from the slit and
+            slit parameter a. This assume Fraunhofer diffraction.
+        Variables:
+            x:
+                A real or complex argument, or numpy array.
+            z:
+                Float
+                The perpendicular distance from the slit plane to
+                the observer.
+            a:
+                Float
+                The slit parameter. This is a unitless paramter
+                defined as the ratio between the slit width and
+                the wavelength of the incoming signal.
+
+        Outputs:
+            f:
+                Single slit diffraction pattern.
+        Dependences:
+            [1] numpy
+    """
+    y = x
+    try:
+        x = np.array(x)
+        if (not np.all(np.isreal(x))) and (not np.all(np.iscomplex(x))):
+            raise TypeError(
+                "\n\tError Encountered:\n"
+                "\trss_ringoccs: Diffcorr Subpackage\n"
+                "\tspecial_function.single_slit_diffraction_solve:\n"
+                "\t\tInput must be a real or complex valued numpy array.\n"
+                "\t\tThe elements of your array have type: %s"
+                % (x.dtype)
+            )
+        else:
+            del y
+    except (TypeError, ValueError) as errmes:
+        raise TypeError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.single_slit_diffraction_solve:\n"
+            "\t\tFirst input must be a real or complex valued numpy array.\n"
+            "\t\tYour input has type: %s\n"
+            "\tOriginal Error Mesage: %s\n"
+            % (type(y).__name__, errmes)
+        )
+    if not (isinstance(z, float)):
+        try:
+            z = float(z)
+        except ValueError:
+            raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.single_slit_diffraction_solve:\n"
+            "\t\tSecond input must be a floating point number.\n"
+            "\t\tYour input has type: %s\n"
+            % (type(z).__name__)
+            )
+    if not (isinstance(a, float)):
+        try:
+            a = float(a)
+        except ValueError:
+            raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.single_slit_diffraction_solve:\n"
+            "\t\tThird input must be a floating point number.\n"
+            "\t\tYour input has type: %s\n"
+            % (type(a).__name__)
+            )
+
     f = np.sinc(a*x/z)*np.sinc(a*x/z)
     return f
 
 def double_slit_diffraction_solve(x, z, a, d):
+    """
+        Function:
+            double_slit_diffraction_solve
+        Purpose:
+            Compute diffraction through a double slit for the
+            variable x with a distance z from the slit and
+            slit parameter a and a distance d between the slits.
+            This assume Fraunhofer diffraction.
+        Variables:
+            x:
+                A real or complex argument, or numpy array.
+            z:
+                Float
+                The perpendicular distance from the slit plane to
+                the observer.
+            a:
+                Float
+                The slit parameter. This is a unitless paramter
+                defined as the ratio between the slit width and
+                the wavelength of the incoming signal.
+            d:
+                Float
+                The distance between slits.
+
+        Outputs:
+            f:
+                Single slit diffraction pattern.
+        Dependences:
+            [1] numpy
+    """
+    y = x
+    try:
+        x = np.array(x)
+        if (not np.all(np.isreal(x))) and (not np.all(np.iscomplex(x))):
+            raise TypeError(
+                "\n\tError Encountered:\n"
+                "\trss_ringoccs: Diffcorr Subpackage\n"
+                "\tspecial_function.double_slit_diffraction_solve:\n"
+                "\t\tInput must be a real or complex valued numpy array.\n"
+                "\t\tThe elements of your array have type: %s"
+                % (x.dtype)
+            )
+        else:
+            del y
+    except (TypeError, ValueError) as errmes:
+        raise TypeError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.double_slit_diffraction_solve:\n"
+            "\t\tFirst input must be a real or complex valued numpy array.\n"
+            "\t\tYour input has type: %s\n"
+            "\tOriginal Error Mesage: %s\n"
+            % (type(y).__name__, errmes)
+        )
+    if not (isinstance(z, float)):
+        try:
+            z = float(z)
+        except ValueError:
+            raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.double_slit_diffraction_solve:\n"
+            "\t\tSecond input must be a floating point number.\n"
+            "\t\tYour input has type: %s\n"
+            % (type(z).__name__)
+            )
+    if not (isinstance(a, float)):
+        try:
+            a = float(a)
+        except ValueError:
+            raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.double_slit_diffraction_solve:\n"
+            "\t\tThird input must be a floating point number.\n"
+            "\t\tYour input has type: %s\n"
+            % (type(a).__name__)
+            )
+    if not (isinstance(d, float)):
+        try:
+            d = float(d)
+        except ValueError:
+            raise ValueError(
+            "\n\tError Encountered:\n"
+            "\trss_ringoccs: Diffcorr Subpackage\n"
+            "\tspecial_function.double_slit_diffraction_solve:\n"
+            "\t\tFourth input must be a floating point number.\n"
+            "\t\tYour input has type: %s\n"
+            % (type(a).__name__)
+            )
     f1 = np.sinc(a*x/z)*np.sinc(a*x/z)
     f2 = np.sin(2.0*np.pi*d*x/z)*np.sin(2.0*np.pi*d*x/z)
     f3 = 4.0*np.sin(np.pi*d*x/z)*np.sin(np.pi*d*x/z)
     f = f1*f2/f3
     return f
 
-def sq_well_solve(x,a,b,F,Inverted=False):
+def sq_well_solve(x,a,b,F,invert=False):
     """
         Function:
             sq_well_solve
         Purpose:
             Computes the solution of diffraction through a square well.
         Variables:
-            x: The independent variable.
-            a: The LEFTMOST endpoint of the square well (Real number).
-            b: The RIGHTMOST endpoint of the square well (Real number).
+            x:
+                Real numpy array.
+                The independent variable.
+            a:
+                Float
+                The LEFTMOST endpoint of the square well.
+            b:
+                Float
+                The RIGHTMOST endpoint of the square well.
             F: The Fresnel scale.
         Output:
-            H: Diffraction pattern of a square well on the interval [a,b].
+            H:
+                Complex numpy array.
+                Diffraction pattern of a square well on
+                the interal [a,b].
         History:
             Translated from IDL: RJM - 2018/05/15 8:03 P.M.
+            Updated and added error checks: RJM - 2018/09/19 7:19 P.M.
     """
     if (np.size(a) != 1):
         raise TypeError("Endpoints must be floating point numbers")
@@ -294,8 +556,10 @@ def sq_well_solve(x,a,b,F,Inverted=False):
         raise TypeError("Independant variable must be a numpy array")
     elif (not np.isreal(x).all()):
         raise ValueError("Independent variable must be real valued")
-    H = ((1 - 1j) / 2.) * (fresnel_cos((b - x) / F) - fresnel_cos((a - x) / F)\
-    + 1j*(fresnel_sin((b - x) / F) - fresnel_sin((a - x) / F)))
-    if not Inverted:
+    H = (0.5 - 0.5j) * (
+        fresnel_cos((b - x) / F) - fresnel_cos((a - x) / F)
+        + 1j*(fresnel_sin((b - x) / F) - fresnel_sin((a - x) / F))
+    )
+    if not invert:
         H = 1-H
     return H

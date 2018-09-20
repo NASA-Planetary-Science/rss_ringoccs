6adafa7e8d774bc3be9c34a38f1b33726bdd0a9b
Jolene Fong <jfong@wellesley.edu>
2018-09-20 14:42:01 -0400

    updated benchmark_test with new e2e structure

--- pipeline/benchmark_run.py
+++ pipeline/benchmark_run.py
@@ -1,6 +1,6 @@
 """
 
-Rev007EX43_time_test.py
+benchmark_run.py
 
 Purpose: rss_ringoccs package sample run to produce a 1-km resolution profile
          of the full Rev007E X43 occultation. Since this is intended as a time
@@ -19,6 +19,7 @@ Revisions:
     2018 Aug 31 - gsteranka - Edited to not use any pre-computed files, use
                               default fits for both cases rather than GUI, and
                               to not plot anything
+    2018 Sep 20 - jfong - updated to work with modified e2e structure
 """
 
 import numpy as np
@@ -35,10 +36,10 @@ rsr_file = ('../data/co-s-rss-1-sroc1-v10/cors_0105/sroc1_123/rsr/'
     + 's10sroe2005123_0740nnnx43rd.2a2')
 kernels = '../tables/Rev007_meta_kernel.ker'
 
-f_USO = 8427222034.34050
 dr_km_desired = 0.05
-res_km = 0.100
+res_km = 0.10
 
+file_search = False
 verbose = True
 planet = 'Saturn'
 spacecraft = 'Cassini'
@@ -56,31 +57,14 @@ rsr_inst = rss.rsr_reader.RSRReader(rsr_file, verbose=verbose)
 geo_inst = rss.occgeo.Geometry(rsr_inst, planet, spacecraft, kernels,
         verbose=verbose)
 
-f_spm, f_offset, f_offset_history = rss.calibration.calc_freq_offset(rsr_inst)
-
-# Use default residual frequency fit
-fit_inst = rss.calibration.FreqOffsetFit(rsr_inst, geo_inst, f_spm,
-        f_offset, f_USO, USE_GUI=False, verbose=verbose)
-
-# Get corrected I's and Q's
-spm_vals, IQ_c = fit_inst.get_IQ_c()
-
-# Create instance with normalized power
-norm_inst = rss.calibration.Normalization(spm_vals, IQ_c, geo_inst, rsr_inst,
-    verbose=verbose)
-
-# Use default power fit
-spm_power_fit, power_spline_fit = norm_inst.get_spline_fit(USE_GUI=False,
-    verbose=verbose)
-
 # Create instance with calibrated data
-cal_inst = rss.calibration.Calibration(fit_inst, norm_inst, geo_inst,
-        verbose=verbose)
+cal_inst = rss.calibration.Calibration(rsr_inst, geo_inst,
+        file_search=file_search, verbose=verbose)
 
 # Create instance with diffraction-limited profile and other
 #   inputs needed for diffraction correction
-dlp_inst = rss.calibration.NormDiff(rsr_inst, dr_km_desired, geo_inst,
-        cal_inst, verbose=verbose)
+dlp_inst = rss.calibration.NormDiff(rsr_inst, geo_inst, cal_inst,
+        dr_km_desired, verbose=verbose)
 
 # Invert profile for full occultation
 tau_inst = rss.diffcorr.DiffractionCorrection(dlp_inst, res_km,

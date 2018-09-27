f5c0c27fbac58b52a9f7a716421d440edb2d0919
Sophia Flury <sflury@wellesley.edu>
2018-09-27 14:34:35 -0400

    Added license to each __init__ file

--- rss_ringoccs/diffrec/__init__.py
+++ rss_ringoccs/diffrec/__init__.py
@@ -1,4 +1,23 @@
 """
+Copyright (C) 2018 Team Cassini
+
+This program is free software: you  can redistribute it and/or modify it
+under the  terms of the GNU  General Public License as  published by the
+Free Software Foundation,  either version 3 of the License,  or (at your
+option) any later version.
+
+This  program  is distributed  in  the  hope  that  it will  be  useful,
+but WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy  of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This program is part of the rss_ringoccs repository hosted at
+https://github.com/NASA-Planetary-Science/rss_ringoccs and developed
+with the financial support of NASA's Cassini Mission to Saturn.
+
     Subpackage Name:
         diffrec
     Purpose:
@@ -21,7 +40,7 @@
                     the outcome to a given set of reconstructed data.
                 find_optimal_resolution:
                     Class
-                    Given a set of data and a reconstruction, this 
+                    Given a set of data and a reconstruction, this
                     class will run diffraction correction over a set
                     of resolutions. The output contains the L_2 and
                     L_infinity difference of the two reconstructions

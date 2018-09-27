0a45ea5e0f627d165e63fb372665dd7263540795
Sophia Flury <sflury@wellesley.edu>
2018-09-27 14:34:35 -0400

    Added license to each __init__ file

--- rss_ringoccs/tools/__init__.py
+++ rss_ringoccs/tools/__init__.py
@@ -1,5 +1,24 @@
 """
 
+Copyright (C) 2018 Team Cassini
+
+This program is free software: you  can redistribute it and/or modify it
+under the  terms of the GNU  General Public License as  published by the
+Free Software Foundation,  either version 3 of the License,  or (at your
+option) any later version.
+
+This  program  is distributed  in  the  hope  that  it will  be  useful,
+but WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy  of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This program is part of the rss_ringoccs repository hosted at
+https://github.com/NASA-Planetary-Science/rss_ringoccs and developed
+with the financial support of NASA's Cassini Mission to Saturn.
+
 tools sub-package of rss_ringoccs package
 
 """
@@ -7,7 +26,7 @@ tools sub-package of rss_ringoccs package
 from .spm_to_et import spm_to_et
 from .et_to_spm import et_to_spm
 from .cassini_blocked import cassini_blocked
-from .cal_inst_from_file import CreateCalInst 
+from .cal_inst_from_file import CreateCalInst
 from .pds3_reader import PDS3Reader
 from .date_to_rev import date_to_rev
 from .get_rev_info import get_rev_info

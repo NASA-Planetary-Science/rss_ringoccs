4904a0585fcf87870ec8f73f1c1b162e6c44586a
Jolene Fong <jfong@wellesley.edu>
2018-12-08 17:30:26 -0500

    version of rss_occgeo.py with new documentation formatting

---
+++ rss_ringoccs/occgeo/occgeo.py
@@ -0,0 +1,443 @@
+"""
+occgeo.py
+
+:Purpose: 
+    
+    Calculate occultation geometry for RSS ring events.
+
+:Notes:
+    #. kernels list must include:
+        1) spacecraft ephemeris kernel
+        2) planetary constants kernel
+        3) leapseconds kernel
+        4) planet and lunar ephemeris kernel
+        5) earth stations kernel
+        6) earth rotation and constants kernel
+    #. This code has only been tested thoroughly for planet='Saturn'.
+
+:Dependencies:
+    #. scipy.interpolate
+    #. numpy
+    #. spiceypy
+    #. sys
+    #. rss_ringoccs.occgeo.calc_occ_geometry
+    #. rss_ringoccs.tools
+
+"""
+from ..tools.spm_to_et import spm_to_et
+from ..tools.et_to_spm import et_to_spm
+from ..tools.write_output_files import write_output_files
+from ..tools.write_history_dict import write_history_dict
+from .calc_elevation_deg import calc_elevation_deg
+from .get_pole import get_pole
+from .calc_rho_vec_km import calc_rho_vec_km
+from .calc_set_et import calc_set_et
+from .calc_phi_deg import calc_phi_deg
+from .calc_D_km import calc_D_km
+from .calc_B_deg import calc_B_deg
+from .calc_F_km import calc_F_km
+from .xform_j2k_to_pcf import xform_j2k_to_pcf
+from .calc_sc_state import calc_sc_state
+from .calc_rip_velocity import calc_rip_velocity
+from .calc_impact_radius_km import calc_impact_radius_km
+from .get_planet_occ_times import get_planet_occ_times
+from .calc_beta import calc_beta
+from .calc_beta import calc_B_eff_deg
+from .get_freespace import get_freespace
+from scipy.interpolate import splrep
+from scipy.interpolate import splev
+
+
+import spiceypy as spice
+import numpy as np
+import sys
+import pdb
+
+class Geometry(object):
+
+    """
+    :Purpose:
+        This is an object that calculates occultation geometry needed for
+        diffraction reconstruction as well as other relevant geometry 
+        parameters.
+
+    Arguments:
+        :rsr_inst (*class*): Instance of RSRReader class.
+        :kernels (*str* or *list*): List of NAIF kernels, including path.
+        :planet (*str*): Planet name
+        :spacecraft (*str*): Spacecraft name
+
+    Keyword Arguments:
+        :pt_per_sec (*float*): Number of points calculated per second
+            for all geometry calculations.
+        :verbose (*bool*): Boolean for whether processing steps are printed.
+        :write_file (*bool*): Boolean for whether output *GEO.TAB and
+            *GEO.LBL files will be created.
+
+    Attributes:
+        :t_oet_spm_vals (*np.ndarray*): Observed event time in seconds 
+            past midnight.
+        :t_ret_spm_vals (*np.ndarray*): Ring event time in seconds past 
+            midnight.
+        :t_set_spm_vals (*np.ndarray*): Spacecraft event time in seconds 
+            past midnight.
+        :rho_km_vals (*np.ndarray*): Distance in km from the center of 
+            Saturn to ring intercept point.
+        :phi_rl_deg_vals (*np.ndarray*): Ring longitude (inertial longitude) 
+            in degrees.
+        :phi_ora_deg_vals (*np.ndarray*): Observed ring azimuth in degrees.
+        :D_km_vals (*np.ndarray*): Spacecraft to ring intercept point 
+            distance in km.
+        :B_deg_vals (*np.ndarray*): Ring opening angle in degrees.
+        :rho_dot_kms_vals (*np.ndarray*): Ring intercept radial velocity 
+            in km/s.
+        :phi_rl_dot_kms_vals (*np.ndarray*): Ring intercept azimuthal velocity 
+            in km/s.
+        :F_km_vals (*np.ndarray*): Fresnel scale in km.
+        :R_imp_km_vals (*np.ndarray*): Impact radius in km.
+        :rx_km_vals (*np.ndarray*): x-component of spacecraft position in 
+            a planetocentric frame, in km.
+        :ry_km_vals (*np.ndarray*): y-component of spacecraft position in 
+            a planetocentric frame, in km.
+        :rz_km_vals (*np.ndarray*): z-component of spacecraft position in 
+            a planetocentric frame, in km.
+        :vx_kms_vals (*np.ndarray*): x-component of spacecraft velocity in 
+            a planetocentric frame, in km/s.
+        :vy_kms_vals (*np.ndarray*): y-component of spacecraft velocity in 
+            a planetocentric frame, in km/s
+        :vz_kms_vals (*np.ndarray*): z-component of spacecraft velocity in 
+            a planetocentric frame, in km/s
+        :elev_deg_vals (*np.ndarray*): Elevation angle in degrees.
+        :kernels (*str* or *list*): List of NAIF kernels, including path.
+        :rev_info (*dict*): RSR file specific info
+        :history (*dict*): Dictionary of processing history.
+        :naif_toolkit_version (*str*): NAIF toolkit version used 
+            (e.g., "V.N0066").
+        :B_eff_deg_vals (*np.ndarray*): Effective ring opening angle in deg.
+        :beta_vals (*np.ndarray*): Optical depth enhancement factor.
+        :ionos_occ_spm_vals (*np.ndarray*): Array of seconds past midnight
+            when the signal is occulted by the planet ionosphere, defined as
+            5000km above an ellipsoid with radii from cpck file.
+        :atmos_occ_spm_vals (*np.ndarray*): Array of seconds past midnight
+            when the signal is occulted by the planet atmosphere, defined as
+            500km above an ellipsoid with radii from cpck file.
+        :freespace_spm (*list*): List of 2x1 lists of seconds past midnight
+            values that define the inner and outer edge of a free-space 
+            gap in the ring system
+        :freespace_km (*np.ndarray*): An array of 2x1 lists of km values
+            that define the inner and outer edge of a free-space gap
+            in the ring system
+
+
+    """
+    def __init__(self, rsr_inst, planet, spacecraft, kernels, pt_per_sec=1.,
+            verbose=False, write_file=True):
+
+        if verbose:
+            print('Calculating occultation geometry...')
+
+        if verbose:
+            print('\tChecking for valid inputs...')
+
+        if type(planet) != str:
+            sys.exit('WARNING (Geometry): Input planet is NOT a string!')
+
+        if type(spacecraft) != str:
+            sys.exit('WARNING (Geometry): Input spacecraft is NOT a string!')
+
+        if not isinstance(pt_per_sec, (int, float)):
+            sys.exit('WARNING (Geometry): Input pt_per_sec is NOT an int or '
+                    + 'float!')
+
+        if verbose:
+            print('\tExtracting information from rsr file...')
+
+        # Extract information from rsr instance
+        year = rsr_inst.year
+        doy = rsr_inst.doy
+        dsn = rsr_inst.dsn
+        band = rsr_inst.band
+        spm_full = rsr_inst.spm_vals
+        spm_start = rsr_inst.spm_vals[0]
+        spm_end = rsr_inst.spm_vals[-1]
+        rsr_hist = rsr_inst.history
+        (f_spm, f_sky) = rsr_inst.get_f_sky_pred()
+
+        if verbose:
+            print('\tSetting rev info attribute...')
+
+            # Setting rev info attribute
+            self.rev_info = rsr_inst.rev_info
+
+        # Create new spm array with defined points per second
+        step = 1./pt_per_sec
+        t_oet_spm_vals = np.arange(spm_start, spm_end, step)
+        t_oet_et_vals = spm_to_et(t_oet_spm_vals, doy, year, kernels=kernels)
+        #self.__et_vals = np.arange(t_oet_et_vals[0], t_oet_et_vals[-1],
+        #        0.1)
+
+        # Interpolate to get sky frequency
+        f_sky_hz_vals = np.interp(t_oet_spm_vals, f_spm, f_sky)
+
+        # Calculate Saturn center to ring intercept vector
+        if verbose:
+            print('\tCalculating ring intercept event time and vector...')
+        rho_vec_vals, t_ret_et_vals = calc_rho_vec_km(t_oet_et_vals, planet,
+                spacecraft, dsn, kernels=kernels)
+
+        rho_km_vals = [spice.vnorm(vec) for vec in rho_vec_vals]
+
+        # Calculate spacecraft event time
+        if verbose:
+            print('\tCalculating spacecraft event time...')
+        t_set_et_vals = calc_set_et(t_oet_et_vals, spacecraft, dsn)
+
+        # Retrieve Saturn pole unit vector
+        if verbose:
+            print('\tRetrieving Saturn pole unit vector...')
+        nhat_p = get_pole(t_set_et_vals[0], planet)
+
+        if verbose:
+            print('\tCalculating ring longitude and ring azimuth...')
+
+        # Calculated ring longitude and observed ring azimuth
+        phi_rl_deg_vals, phi_ora_deg_vals = calc_phi_deg(t_oet_et_vals,
+                rho_vec_vals, spacecraft, dsn, nhat_p)
+
+        if verbose:
+            print('\tCalculating distance from spacecraft to ring intercept '
+                + 'point...')
+        # Calculate distance from spacecraft to ring intercept point
+        D_km_vals = calc_D_km(t_ret_et_vals, t_set_et_vals)
+
+        if verbose:
+            print('\tCalculating ring opening angle...')
+
+        # Calculate ring opening angle
+        B_deg_vals = calc_B_deg(t_oet_et_vals, spacecraft, dsn, nhat_p)
+
+        if verbose:
+            print('\tCalculating Fresnel scale...')
+
+        # Calculate Fresnel scale
+        F_km_vals = calc_F_km(D_km_vals, f_sky_hz_vals, B_deg_vals,
+                phi_ora_deg_vals)
+
+        t_ret_spm_vals = et_to_spm(t_ret_et_vals)
+        t_set_spm_vals = et_to_spm(t_set_et_vals)
+
+        if verbose:
+            print('\tCalculating ring intercept velocities...')
+        # Calculate ring intercept velocities
+        rho_dot_kms_vals, phi_rl_dot_kms_vals = calc_rip_velocity(rho_km_vals,
+                phi_rl_deg_vals, step)
+
+        if verbose:
+            print('\tCalculating spacecraft state vector...')
+        # Calculate spacecraft state vector
+        R_sc_km_vals, R_sc_dot_kms_vals = calc_sc_state(t_set_et_vals,
+                spacecraft, planet, dsn, nhat_p)
+
+        if verbose:
+            print('\tCalculating impact radius...')
+        # Calculate impact radius
+        R_imp_km_vals = calc_impact_radius_km(R_sc_km_vals, t_set_et_vals,
+                spacecraft, dsn, nhat_p)
+
+        if verbose:
+            print('\tCalculating elevation angle...')
+
+        # Calculate target angle above the horizon
+        elev_deg_vals = calc_elevation_deg(t_oet_et_vals, spacecraft, dsn)
+
+        # Calculate beta
+        if verbose:
+            print('\tCalculating beta...')
+        beta_vals = calc_beta(B_deg_vals, phi_ora_deg_vals)
+        B_eff_deg_vals = calc_B_eff_deg(B_deg_vals, phi_ora_deg_vals)
+
+        # Set attributes, first block contains the inputs to *GEO.TAB
+        self.t_oet_spm_vals = np.asarray(t_oet_spm_vals)
+        self.t_ret_spm_vals = np.asarray(t_ret_spm_vals)
+        self.t_set_spm_vals = np.asarray(t_set_spm_vals)
+        self.rho_km_vals = np.asarray(rho_km_vals)
+        self.phi_rl_deg_vals = np.asarray(phi_rl_deg_vals)
+        self.phi_ora_deg_vals = np.asarray(phi_ora_deg_vals)
+        self.D_km_vals = np.asarray(D_km_vals)
+        self.B_deg_vals = np.asarray(B_deg_vals)
+        self.rho_dot_kms_vals = np.asarray(rho_dot_kms_vals)
+        self.phi_rl_dot_kms_vals = np.asarray(phi_rl_dot_kms_vals)
+        self.F_km_vals = np.asarray(F_km_vals)
+        self.R_imp_km_vals = np.asarray(R_imp_km_vals)
+        self.rx_km_vals = np.stack(R_sc_km_vals)[:, 0]
+        self.ry_km_vals = np.stack(R_sc_km_vals)[:, 1]
+        self.rz_km_vals = np.stack(R_sc_km_vals)[:, 2]
+        self.vx_kms_vals = np.stack(R_sc_dot_kms_vals)[:, 0]
+        self.vy_kms_vals = np.stack(R_sc_dot_kms_vals)[:, 1]
+        self.vz_kms_vals = np.stack(R_sc_dot_kms_vals)[:, 2]
+
+        self.kernels = kernels
+        self.elev_deg_vals = np.asarray(elev_deg_vals)
+        self.naif_toolkit_version = self.__get_naif_version()
+        self.beta_vals = np.asarray(beta_vals)
+        self.B_eff_deg_vals = np.asarray(B_eff_deg_vals)
+
+
+
+        # Calculate when signal passes atmosphere + ionosphere
+        ionos_occ_et_vals = get_planet_occ_times(t_oet_et_vals, dsn,
+                planet, spacecraft, height_above=5000.)
+        self.ionos_occ_spm_vals = et_to_spm(ionos_occ_et_vals)
+
+        atmos_occ_et_vals = get_planet_occ_times(t_oet_et_vals, dsn,
+                planet, spacecraft, height_above=500.)
+        self.atmos_occ_spm_vals = et_to_spm(atmos_occ_et_vals)
+        # Write processing history dictionary attribute
+        if verbose:
+            print('\tWriting history dictionary...')
+
+        input_vars = {
+                "rsr_inst": rsr_hist,
+                "kernels": kernels,
+                "planet": planet,
+                "spacecraft": spacecraft
+                }
+        input_kwds = {
+                "pt_per_sec": pt_per_sec
+                }
+        self.history = write_history_dict(input_vars, input_kwds, __file__)
+
+        # Add prof_dir entry to rev_info dict
+        self.rev_info['prof_dir'] = self.get_profile_dir()
+
+        if self.rev_info['prof_dir'] == '"BOTH"':
+            self.split_ind = self.get_chord_ind()
+        else:
+            self.split_ind = None
+
+        self.freespace_km, self.freespace_spm = get_freespace(
+                t_ret_spm_vals, year, doy, rho_km_vals,
+                phi_rl_deg_vals, t_oet_spm_vals, self.atmos_occ_spm_vals,
+                split_ind=self.split_ind, kernels=kernels)
+
+        # Write output data and label file if set
+        if write_file:
+            write_output_files(self)
+
+    def __get_naif_version(self):
+        """
+        Return NAIF toolkit version used.
+
+        Returns:
+            (*str*): NAIF toolkit version in format '"V.N0066"'.
+            
+        """
+        naif_ver = spice.tkvrsn('TOOLKIT')
+        return ('"V.' + naif_ver.split('_')[-1] + '"')
+
+    def get_profile_dir(self):
+        """
+        Return observed profile direction.
+
+        Returns:
+            (*str*): Profile direction as '"INGRESS"', '"EGRESS"', or '"BOTH"'.
+        """
+        rho = self.rho_km_vals
+        dr_start = rho[1] - rho[0]
+        dr_end = rho[-1] - rho[-2]
+
+        if (dr_start < 0) and (dr_end < 0):
+            prof_dir = '"INGRESS"'
+        elif (dr_start > 0) and (dr_end > 0):
+            prof_dir = '"EGRESS"'
+        else:
+            # check if actually a chord occultation
+            prof_dir = self.verify_chord()
+        return prof_dir
+
+    def verify_chord(self):
+        """
+        Verify that an occultation with an increasing and decreasing
+        radial velocity is actually a chord occultation.
+
+        Returns:
+            (*str*): Profile direction as '"INGRESS"', '"EGRESS"', or '"BOTH"'.
+        """
+        split_ind = self.get_chord_ind()
+
+        t_oet_ing = self.t_oet_spm_vals[:split_ind]
+        t_oet_egr = self.t_oet_spm_vals[split_ind:]
+
+        n_ing = len(t_oet_ing)
+        n_egr = len(t_oet_egr)
+
+
+        atmos_blocked_spm = list(self.atmos_occ_spm_vals)
+        
+        ing_blocked = [x for x in t_oet_ing if x in atmos_blocked_spm]
+        egr_blocked = [x for x in t_oet_egr if x in atmos_blocked_spm]
+
+        if len(ing_blocked) == n_ing:
+            prof_dir = '"EGRESS"'
+        elif len(egr_blocked) == n_egr:
+            prof_dir = '"INGRESS"'
+        else:
+            prof_dir = '"BOTH"'
+    
+        return prof_dir
+
+    def get_chord_ind(self):
+        """
+        Return index of where radial velocity sign change occurs in a chord
+        occultation.
+
+        Returns:
+            (*int*): Index of where chord occultation goes from '"INGRESS"'
+                to '"EGRESS"' or vice versa.
+        """
+
+        # add 2 for the index of the first element after the sign change
+        ind = np.argwhere(np.diff(np.sign(self.rho_dot_kms_vals)))
+
+        if len(ind) > 1:
+            print('WARNING! ring radius changes direction twice!')
+            pdb.set_trace()
+        elif len(ind) == 0:
+            ind = None
+        else:
+            ind = int(ind[0]) + 2
+
+        return ind
+
+"""
+Revisions:
+    2018 Jul 10 - jfong - original
+    2018 Jul 11 - jfong - remove write_hist method and use
+                          write_history_dict() from tools/
+    2018 Jul 12 - jfong - add planetary_occultation_flag and
+                          ring_occultation_direction and
+                          ring_profile_direction and
+                          revolution_number attributes and methods
+    2018 Jul 16 - jfong - add beta_vals attribute
+    2018 Jul 26 - jfong - remove write_history_dict() import
+                        - add __write_history_dict method
+    2018 Jul 27 - jfong - remove __write_history_dict method and use 
+                          write_history_dict() again, which now requires
+                          source file as an additional input
+    2018 Aug 02 - jfong - remove methods pertaining to data catalog entries
+                        - remove planet_occ_flag, rev_num, ring_occ_dir,
+                          rsr_inst attributes
+                        - remove rsr_file and sample_rate_khz attribute 
+                          extraction from rsr_inst
+    2018 Sep 20 - jfong - add write_file kwarg
+                        - set rev_info from rsr_inst as an attribute
+                        - add prof_dir to rev_info dict attribute
+    2018 Oct 15 - jfong - add freespace_km attribute using find_gaps()
+    2018 Oct 17 - jfong - add ionos_occ_spm_vals, atmos_occ_spm_vals attr
+    2018 Oct 23 - jfong - update freespace_km attr to freespace_spm
+                        - add ingress_spm, egress_spm attributes
+    2018 Nov 05 - jfong - use SET for atmosphere calculation
+    2018 Nov 19 - jfong - add chord verification
+                        - replace splrep, splev for interp1d for rho interp
+
+"""

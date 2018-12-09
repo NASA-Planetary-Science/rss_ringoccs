f8a6d27f69fd0cb410a564efe4b38d07caf42cdb
Sophia Flury <sflury@wellesley.edu>
2018-12-08 23:24:42 -0500

    Updated PDF merging script to match directory structure.

--- rss_ringoccs/tools/pdf_merge.sh
+++ rss_ringoccs/tools/pdf_merge.sh
@@ -2,15 +2,20 @@
 
 for dir in $( ls ../../output/ ); do
 
-    if [ -d ../output/${dir} ]
+    if [ -d ../../output/${dir} ]
     then
 
-        for subdir in $( ls ../output/$dir ); do
+        for subdir in $( ls ../../output/$dir ); do
 
-            echo "Merging calibration FORFIT PDFs in ../../output/${dir}/${subdir}/"
-            "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ../output/${dir}/${subdir}_FORFITS.PDF ../output/${dir}/${subdir}/*/*FORFIT*.PDF
-            echo "Merging calibration FORFIT PDFs in ../../output/${dir}/${subdir}/"
-            "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ../output/${dir}/${subdir}_FSPFITS.PDF ../output/${dir}/${subdir}/*/*FSPFIT*.PDF
+            if [ -d ../../output/${dir}/${subdir} ]
+            then
+
+                echo "Merging calibration FORFIT PDFs in ../../output/${dir}/${subdir}/"
+                "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ../../output/${dir}/${subdir}_FORFITS.PDF ../../output/${dir}/${subdir}/*/*FORFIT*.PDF
+                echo "Merging calibration FSPFIT PDFs in ../../output/${dir}/${subdir}/"
+                "/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ../../output/${dir}/${subdir}_FSPFITS.PDF ../../output/${dir}/${subdir}/*/*FSPFIT*.PDF
+
+            fi
 
         done
 

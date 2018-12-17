d90491d81a2762f60a770a86309caf0531471ed3
Jolene Fong <jfong@wellesley.edu>
2018-12-17 18:39:01 -0500

    added accidentally removed spm_to_et.py

---
+++ rss_ringoccs/tools/spm_to_et.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+"""
+
+spm_to_et.py
+
+Purpose: Calculate ephemeris time given a set of SPM values and appropriate
+         kernels. Called inside of calc_f_sky_recon.py
+
+Revisions:
+      gjs_spm_to_et.py
+   2018 Feb 22 - gsteranka - Original version
+      spm_to_et.py
+   2018 Mar 20 - gsteranka - Copy to official version and remove
+                             debug steps
+    2018 Jul 23 - jfong - add input error checks (doy and year can't be 0)
+    2018 Jul 30 - jfong - allow float inputs for spm (in addition to array)
+    
+"""
+
+import numpy as np
+import spiceypy as spice
+import sys
+import pdb
+
+def spm_to_et(spm, doy, year, kernels=None):
+    """
+    Function to calculate ephemeris time from given SPM
+    """
+
+    if doy == 0:
+        sys.exit('WARNING (spm_to_et): input doy is 0!')
+    
+    if year == 0:
+        sys.exit('WARNING (spm_to_et): input year is 0!')
+
+
+
+
+    # Leap seconds kernel
+    if kernels is None:
+        kernels = '../../kernels/naif/CASSINI/kernels/lsk/naif0012.tls'
+
+    spice.kclear()
+    spice.furnsh(kernels)
+
+    hours = (spm / 3600.0).astype(int)
+    remainder_spm = (spm - hours*3600.0).astype(int)
+    minutes = (remainder_spm / 60.0).astype(int)
+    seconds = spm - hours*3600.0 - minutes*60.0
+
+    try:
+        n_pts = len(spm)
+    except TypeError:
+        n_pts = len([spm])
+        hours = [hours]
+        minutes = [minutes]
+        seconds = [seconds]
+
+    et_sec_vals = np.zeros(n_pts)
+
+    # Check for leap year
+    if (year % 4) == 0:
+        days_per_year = 366
+    else:
+        days_per_year = 365
+
+    for i in range(n_pts):
+
+        # Adjust if hour makes it go on to next day
+        doy_adjusted = doy + hours[i]/24
+
+        # Adjust if day goes on to next year
+        this_year = (year + 
+                np.floor((doy_adjusted - 1)/days_per_year)).astype(int)
+        this_doy = (1 + (doy_adjusted-1) % days_per_year).astype(int)
+
+        utc_str = (str(this_year) + '-' + str(this_doy).zfill(3) + 'T'
+            + str(hours[i] % 24).zfill(2) + ':'
+            + str(minutes[i]).zfill(2) + ':'
+            + '{:2.13f}'.format(seconds[i]))
+
+        et_sec_vals[i] = spice.utc2et(utc_str)
+
+    return et_sec_vals
+
+

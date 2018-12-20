827813cac3fd2dd29ff63b6d1b11b9b31dbd2f21
Sophia Flury <sflury@wellesley.edu>
2018-12-20 12:44:50 -0500

    Updated profile fit plot.

--- rss_ringoccs/calibration/power_normalization.py
+++ rss_ringoccs/calibration/power_normalization.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Purpose:
-    Normalize frequency-corrected power using a polynomial fit of specified 
+    Normalize frequency-corrected power using a polynomial fit of specified
     order.
 
 """
@@ -32,7 +32,7 @@ class Normalization(object):
 
     Arguments:
         :spm_raw (*np.ndarray*): SPM as sampled in the raw data
-        :IQ_c (*np.ndarray*): frequency-corrected signal corresponding to 
+        :IQ_c (*np.ndarray*): frequency-corrected signal corresponding to
             spm_raw
         :geo_inst (*np.ndarray*):
                        instance of the Geometry class, used to estimate
@@ -54,7 +54,7 @@ class Normalization(object):
                         to the freespace power. Default is False.
     """
 
-    def __init__(self, spm_raw, IQ_c, geo_inst, rsr_inst, order=3, 
+    def __init__(self, spm_raw, IQ_c, geo_inst, rsr_inst, order=3,
             fittype='poly', interact=False, verbose=False):
 
         # store rev info
@@ -132,11 +132,11 @@ class Normalization(object):
             freespace power fitting step. This is done by prompting the user for
             input in the command line and displaying the results of their input
             for the polynomial fit to the freespace power.
-            Only called if the Normalization keyword ``interact`` is set to 
+            Only called if the Normalization keyword ``interact`` is set to
             True.
 
         Arguments:
-            :gaps_str (*str*): string containing the user input freespace 
+            :gaps_str (*str*): string containing the user input freespace
                 regions.
 
         Returns:
@@ -172,7 +172,7 @@ class Normalization(object):
                     new_gaps_str = input('Please input new freespace gaps in '
                                         + 'SPM and press enter twice: ')
                     print('\n')
-                    # if the string doesn't start with double brackets 
+                    # if the string doesn't start with double brackets
                     #   (not an NxM list) then keep current gaps
                     if '[[' not in new_gaps_str and ']]' not in new_gaps_str :
                         print('Invalid entry for freespace gaps.\nMust be Nx2 '
@@ -184,7 +184,7 @@ class Normalization(object):
                         new_gaps = self.extract_list_from_str(
                                             new_gaps_str[1:-1])
                 # create new mask based on changes to freespace regions
-                self.create_mask(spm_down, new_gaps, 
+                self.create_mask(spm_down, new_gaps,
                             p_obs_down/np.nanmax(p_obs_down))
 
             # Prompt for fit type
@@ -197,7 +197,7 @@ class Normalization(object):
             print('\n')
 
             # update fit
-            self.fit_freespace_power(spm_down, p_obs_down, 
+            self.fit_freespace_power(spm_down, p_obs_down,
                         order=order,fittype=fittype)
             # update plot for user
             self.plot_power_profile(spm_down,p_obs_down,new_gaps,order)
@@ -213,7 +213,7 @@ class Normalization(object):
             Extract an Nx2 list from the string of user input freespace regions.
 
         Arguments:
-            :gaps_str (*str*): string containing the user input freespace 
+            :gaps_str (*str*): string containing the user input freespace
                 regions.
 
         Returns:
@@ -244,12 +244,12 @@ class Normalization(object):
         """
         Arguments:
             :spm (*np.ndarray*): SPM in seconds of the downsampled signal
-            :rho (*np.ndarray*): occultation intercept radius of the 
+            :rho (*np.ndarray*): occultation intercept radius of the
                 downsampled signal
-            :gaps_spm (*list*): location of freespace regions as predicted 
+            :gaps_spm (*list*): location of freespace regions as predicted
                 by the gap-finding routines ``get_freespace.py`` using
                 tabulated orbital parameters.
-            :pc (*np.ndarray*): phase-corrected downsampled power where 
+            :pc (*np.ndarray*): phase-corrected downsampled power where
                 :math:`P_c=|I_c^2+iQ_c^2|`. Sets attributes
             :mask (*np.ndarray*):
                            array of booleans wherein True designates data
@@ -385,22 +385,22 @@ class Normalization(object):
     def plot_power_profile(self,spm,pow,gaps,order,save=False):
         """
         Purpose:
-            Plot results of the freespace power for total profile and 
-            each individual freespace region and either show plot in a 
+            Plot results of the freespace power for total profile and
+            each individual freespace region and either show plot in a
             GUI or save it to a file.
             File name will match the *.LBL and *.TAB nomenclature.
 
         Arguments:
-            :spm (*np.ndarray*): SPM sampled by 
-                ``calc_freq_offset`` when calculating the offset frequencies 
+            :spm (*np.ndarray*): SPM sampled by
+                ``calc_freq_offset`` when calculating the offset frequencies
                 for the occultation
             :pow (*np.ndarray*): residual sky frequency
             :gaps (*np.ndarray*): gap edges used to select freespace power
-            :order (*float*): order of polynomial fit to the residual sky 
+            :order (*float*): order of polynomial fit to the residual sky
                 frequency
 
         Keyword Arguments:
-            :save (*bool*): If True, saves plots to file. Otherwise, plot 
+            :save (*bool*): If True, saves plots to file. Otherwise, plot
                 is shown in GUI. Default is False.
         """
         # max power inside the occultation
@@ -417,7 +417,8 @@ class Normalization(object):
 
         # draw figure and create subplots
         plt.close('all')
-        fig = plt.figure(figsize=(1+1.25*float(ncols),8))
+        #figsize = 1+1.25*float(ncols)
+        fig = plt.figure(figsize=(6,8))
         gs = GridSpec(6,ncols)
         ### total profile - takes two rows ###
         ax_prof = fig.add_subplot(gs[0:2,:])
@@ -425,8 +426,7 @@ class Normalization(object):
                     ,ms=6,zorder=1)
         ax_prof.plot(spm/1e3-spm_off, pow/pmax, color='blue',zorder=2)
         ax_prof.plot(spm/1e3-spm_off, self.pnorm_fit/pmax, color='red',zorder=3)
-        ax_prof.set_xlim((gaps[0][0]-100)/1e3-spm_off,
-                        (gaps[-1][1]+100)/1e3-spm_off)
+        ax_prof.set_xlim(spm[0]/1e3-spm_off,spm[-1]/1e3-spm_off)
         ax_prof.set_ylim(0,1.1)
         ### individual freespace regions ###
         # add subplots
@@ -467,7 +467,7 @@ class Normalization(object):
         # axis labels
         fig.text(0.45,0.01,r'SPM - '+str(int(spm_off))+' ($10^3$ sec)')
         fig.text(0.01,0.5,r'Power (arb.)',rotation=90)
-        fig.text(0.3,0.925,'PolyOrder: '+str(order))
+        fig.text(0.4,0.925,'PolyOrder: '+str(order))
         if save:
             #generate plot file names
             filenames,outdirs = construct_filepath(self.rev_info,'FSPFIT')
@@ -489,7 +489,7 @@ class Normalization(object):
 
         Arguments:
             :spm_raw (*np.ndarray*): raw SPM values
-            :IQ_c_raw (*np.nparray*): :math:`I_c+iQ_c` sampled at the 
+            :IQ_c_raw (*np.nparray*): :math:`I_c+iQ_c` sampled at the
                 raw SPM rate
 
         Keyword Arguments:

68d180079de230bb4967183ebc0baa56fffb4c48
ryanmaguire <rmaguire@protonmail.com>
2019-01-07 16:00:23 -0500

    More polynomial stuff.

--- rss_ringoccs/diffrec/diffraction_correction.py
+++ rss_ringoccs/diffrec/diffraction_correction.py
@@ -24,16 +24,26 @@ IV0_35 = 7257.7994923041760
 # Dictionary containing regions of interest within the Saturnian Rings.
 region_dict = {
     'all':             [1.0, 400000.0],
+    'besselbarnard':   [120210.0, 120330.0],
+    'bessel-barnard':  [120210.0, 120330.0],
     'cringripples':    [77690.0, 77760.0],
     'encke':           [132900.0, 134200.0],
     'enckegap':        [132900.0, 134200.0],
+    'herschel':        [118100.0, 118380.0],
+    'herschelgap':     [118100.0, 118380.0],
+    'huygens':         [117650.0, 117950.0],
+    'huygensringlet':  [117650.0, 117950.0],
     'janusepimetheus': [96200.0, 96800.0],
+    'jeffreys':        [118900.0, 119000.0],
+    'jeffreysgap':     [118900.0, 119000.0],
+    'kuiper':          [119300.0, 119500.0],
+    'kuipergap':       [119300.0, 119500.0],
     'maxwell':         [87410.0, 87610.0],
     'maxwellringlet':  [87410.0, 87610.0],
+    'russell':         [118550.0, 118660.0],
+    'russellgap':      [118550.0, 118660.0],
     'titan':           [77870.0, 77930.0],
-    'titanringlet':    [77870.0, 77930.0],
-    'huygens':         [117650.0, 117950.0],
-    'huygensringlet':  [117650.0, 117950.0]
+    'titanringlet':    [77870.0, 77930.0]
 }
 
 
@@ -2285,7 +2295,6 @@ class DiffractionCorrection(object):
                     loop += 1
                     if (loop > 5):
                         break
-
 
                 # Compute Eta variable (MTR86 Equation 4c).
                 psi_vals = psif(kD, r, r0, phi, phi0, b, d)

c5a39f215fb898a99b9533c40b29abdaa7e892d3
Jolene Fong <jfong@wellesley.edu>
2019-01-15 19:41:59 -0500

    remove dependencies section

--- rss_ringoccs/occgeo/__init__.py
+++ rss_ringoccs/occgeo/__init__.py
@@ -3,9 +3,6 @@
     Calculate occultation geometry parameters listed within CORSS_8001 v2
     *GEO.TAB and *GEO.LBL files, as well as geometrical quantities needed for
     calibrating RSS ring data.
-
-:Dependencies:
-
 """
 
 from .occgeo import Geometry

635af35ed6f544451f7a0e1fd6e974f9c8af47fc
Jolene Fong <jfong@wellesley.edu>
2019-01-22 15:27:40 -0500

    use 50m spacing and 500m reconstruction resolution, added back the try/except so that batch continues after hitting an error,errors are now written to a fail_file that is specified by users while running script in terminal

--- pipeline/e2e_batch.py
+++ pipeline/e2e_batch.py
@@ -20,9 +20,9 @@ spacecraft = 'Cassini'
 # 2.9 GHZ CPU, 16 GB RAM TO PROCESS
 with16 = False
 # specify the DLP radial sampling rate in km
-dr_km_desired = 0.25
+dr_km_desired = 0.05
 # specify the TAU processing resolution in km
-res_km = 1.0
+res_km = 0.5
 # specify the radial range over which to
 # reconstruct the optical depth profile
 inversion_range = [6e4,1.5e5]
@@ -36,6 +36,10 @@ psitype='Fresnel4'
 write_file = True
 # specify whether to output progress to terminal
 verbose = False
+
+fail_file = open('../output/' + sys.argv[1], 'w')
+
+#
 ###
 ### ***** END USER INPUT *****
 ###
@@ -60,54 +64,61 @@ for rsr_file in files:
     if rsr_file in skips:
         print('SKIPPING FILE WITH BAD HEADER: '+rsr_file)
         continue
-
-    st = time.time()
-
-    # print RSR file
-    print(rsr_file)
-
-    # Create instance with rsr file contents
-    rsr_inst = rss.rsr_reader.RSRReader(rsr_file, verbose=verbose,
-            decimate_16khz_to_1khz=with16)
-
-    # Create instance with geometry parameters
-    geo_inst = rss.occgeo.Geometry(rsr_inst, planet, spacecraft, kernels,
-            verbose=verbose, write_file=write_file)
-
-    # Create instance with calibrated data
-    cal_inst = rss.calibration.Calibration(rsr_inst, geo_inst,
-            verbose=verbose, write_file=write_file,
-            fof_order=fof_order, pnf_order=pnf_order)
-
-    # Create instaces of DLP class for one or both directions
-    # depending on occultation type (diametric or chord, respectively)
-    dlp_inst_ing, dlp_inst_egr = (
-            rss.calibration.DiffractionLimitedProfile.create_dlps(
-                rsr_inst, geo_inst, cal_inst, dr_km_desired,
-                write_file=write_file, verbose=verbose))
-
-    # Create instances of the diffraction-reconstructed profile
-    # for existing instances of the DLP
-    if dlp_inst_ing is not None:
-        tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst_ing, res_km,
-                write_file=write_file, rng=inversion_range,
-                verbose=verbose, psitype=psitype)
-        rss.tools.plot_summary_doc_v2(geo_inst, cal_inst, dlp_inst_ing,
-                tau_inst)
-    if dlp_inst_egr is not None:
-        tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst_egr, res_km,
-                write_file=write_file, rng=inversion_range,
-                verbose=verbose, psitype=psitype)
-        rss.tools.plot_summary_doc_v2(geo_inst, cal_inst, dlp_inst_egr,
-                tau_inst)
-
-
-    # if verbose, let user know this file processed successfuly
-    et = time.time()
-    run_time = str((et-st)/60.)
-    print('File processing time (min): ' + str(run_time))
+    try:
+        st = time.time()
+    
+        # print RSR file
+        print(rsr_file)
+    
+        # Create instance with rsr file contents
+        rsr_inst = rss.rsr_reader.RSRReader(rsr_file, verbose=verbose,
+                decimate_16khz_to_1khz=with16)
+    
+        # Create instance with geometry parameters
+        geo_inst = rss.occgeo.Geometry(rsr_inst, planet, spacecraft, kernels,
+                verbose=verbose, write_file=write_file)
+    
+        # Create instance with calibrated data
+        cal_inst = rss.calibration.Calibration(rsr_inst, geo_inst,
+                verbose=verbose, write_file=write_file,
+                fof_order=fof_order, pnf_order=pnf_order)
+    
+        # Create instaces of DLP class for one or both directions
+        # depending on occultation type (diametric or chord, respectively)
+        dlp_inst_ing, dlp_inst_egr = (
+                rss.calibration.DiffractionLimitedProfile.create_dlps(
+                    rsr_inst, geo_inst, cal_inst, dr_km_desired,
+                    write_file=write_file, verbose=verbose))
+    
+        # Create instances of the diffraction-reconstructed profile
+        # for existing instances of the DLP
+        if dlp_inst_ing is not None:
+            tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst_ing, res_km,
+                    write_file=write_file, rng=inversion_range,
+                    verbose=verbose, psitype=psitype)
+            rss.tools.plot_summary_doc_v2(geo_inst, cal_inst, dlp_inst_ing,
+                    tau_inst)
+        if dlp_inst_egr is not None:
+            tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst_egr, res_km,
+                    write_file=write_file, rng=inversion_range,
+                    verbose=verbose, psitype=psitype)
+            rss.tools.plot_summary_doc_v2(geo_inst, cal_inst, dlp_inst_egr,
+                    tau_inst)
+    
+    
+        # if verbose, let user know this file processed successfuly
+        et = time.time()
+        run_time = str((et-st)/60.)
+        print('File processing time (min): ' + str(run_time))
+    except:
+        tb = traceback.format_exc()
+        fail_file.write('-'*48+'\n')
+        print(tb)
+        fail_file.write('  '+rsr_file+'\n'+'-'*36+'\n\n'+ 'n='+
+                         str(ind)+'\n'+ tb+'\n')
 
 
 final_time = time.time()
 total_batch_time = (final_time - init_time)/60./60.
 print('Total processing time (hrs): ' + str(total_batch_time))
+fail_file.close()

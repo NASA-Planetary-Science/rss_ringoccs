835ade6bc99a79ae839576e81314e80acab4e962
Sophia Flury <sflury@wellesley.edu>
2019-01-22 12:19:25 -0500

    Revert "Fixed resolutions, filepath, and plot commands"
    This reverts commit 9ca8490748ec4f680b1ed072b1a58ad08b027b24.

--- examples/quick_look_run.py
+++ examples/quick_look_run.py
@@ -1,25 +1,28 @@
 """
-Purpose:
-    Example rss_ringoccs/ 'quick-look' process script.
+quick_look_run.py
+
+Purpose: Example rss_ringoccs/ 'quick-look' process script.
+
 """
 
 import sys
 sys.path.append('../')
 import rss_ringoccs as rss
 sys.path.remove('../')
-import pdb
 import matplotlib.pyplot as plt
 
 # ***** Begin user input *****
 data_dir = '../output/Rev007/Rev007E/Rev007E_RSS_2005_123_X43_E/'
-geo_file = data_dir + 'RSS_2005_123_X43_E_GEO_20190122_0001.TAB'
-cal_file = data_dir + 'RSS_2005_123_X43_E_CAL_20190122_0001.TAB'
-dlp_file = data_dir + 'RSS_2005_123_X43_E_DLP_0500M_20190122_0001.TAB'
+geo_file = data_dir + ''
+cal_file = data_dir + ''
+dlp_file = data_dir + ''
 
 verbose = True
-inversion_range = 'all'
-res_list = [2.0, 1.6, 1.0, 0.67]
-Maxwell_xrange = [87400., 87600.]
+res_km = 1.0
+inversion_range = [87400., 87600.]  # Maxwell range
+res_list = [1.5, 1.2, 0.75, 0.50]
+
+plot_center_km = 87515. # radius to center plot at
 
 # ***** End user input *****
 
@@ -27,15 +30,15 @@ dlp_inst = rss.tools.ExtractCSVData(geo_file, cal_file, dlp_file,
         verbose=verbose)
 
 nres = len(res_list)
-fig, axes = plt.subplots(nres, 1, figsize=(8,8), sharex=True)
+fig, axes = plt.subplots(nres, 1, figsize=(8.5,11), sharex=True)
 plt.subplots_adjust(hspace=0)
 for n in range(nres):
     res = res_list[n]
     label = str(res*1000.) + 'm'
     print('Reconstructing at ', label, '...')
     tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst, res,
-            rng=Maxwell_xrange, verbose=verbose)
-    rho = tau_inst.rho_km_vals - 87515.
+            rng=inversion_range, verbose=verbose)
+    rho = tau_inst.rho_km_vals - plot_center_km
     tau = tau_inst.tau_vals
     ax = axes[n]
     ax.plot(rho, tau, label=label)
@@ -44,7 +47,10 @@ for n in range(nres):
     ax.legend()
 
 axes[0].set_title('Rev007E X43 Maxwell Ringlet Optical Depth \nReconstruction Resolution Comparison', fontsize=13)
-axes[-1].set_xlabel('$\\rho$ - 87515 (km)')
+axes[-1].set_xlabel('$\\rho$ - ' + str(plot_center_km) + ' (km)')
 axes[-1].set_xlim([-45.,45.])
-
 plt.show()
+
+"""
+Revisions:
+"""

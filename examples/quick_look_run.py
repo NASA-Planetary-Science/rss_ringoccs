f6ee79c574058c1d4e22ae335b33f43e7ed0cfd4
Jolene Fong <jfong@wellesley.edu>
2019-01-22 11:31:56 -0500

    remove pdb, update new data directory structure, show plot

--- examples/quick_look_run.py
+++ examples/quick_look_run.py
@@ -1,31 +1,28 @@
 """
-
 quick_look_run.py
 
 Purpose: Example rss_ringoccs/ 'quick-look' process script.
 
-Revisions:
-    2018 Sep 19 - jfong - original
 """
 
 import sys
 sys.path.append('../')
 import rss_ringoccs as rss
 sys.path.remove('../')
-import pdb
 import matplotlib.pyplot as plt
 
 # ***** Begin user input *****
-data_dir = '../output/Rev007/E/Rev007E_RSS_2005_123_X43_E/'
+data_dir = '../output/Rev007/Rev007E/Rev007E_RSS_2005_123_X43_E/'
 geo_file = data_dir + ''
 cal_file = data_dir + ''
 dlp_file = data_dir + ''
 
 verbose = True
 res_km = 1.0
-inversion_range = 'all'
+inversion_range = [87400., 87600.]  # Maxwell range
 res_list = [1.5, 1.2, 0.75, 0.50]
-Maxwell_xrange = [87400., 87600.]
+
+plot_center_km = 87515. # radius to center plot at
 
 # ***** End user input *****
 
@@ -40,8 +37,8 @@ for n in range(nres):
     label = str(res*1000.) + 'm'
     print('Reconstructing at ', label, '...')
     tau_inst = rss.diffrec.DiffractionCorrection(dlp_inst, res,
-            rng=Maxwell_xrange, verbose=verbose)
-    rho = tau_inst.rho_km_vals - 87515.
+            rng=inversion_range, verbose=verbose)
+    rho = tau_inst.rho_km_vals - plot_center_km
     tau = tau_inst.tau_vals
     ax = axes[n]
     ax.plot(rho, tau, label=label)
@@ -50,7 +47,10 @@ for n in range(nres):
     ax.legend()
 
 axes[0].set_title('Rev007E X43 Maxwell Ringlet Optical Depth \nReconstruction Resolution Comparison', fontsize=13)
-axes[-1].set_xlabel('$\\rho$ - 87515 (km)')
+axes[-1].set_xlabel('$\\rho$ - ' + str(plot_center_km) + ' (km)')
 axes[-1].set_xlim([-45.,45.])
+plt.show()
 
-pdb.set_trace()
+"""
+Revisions:
+"""

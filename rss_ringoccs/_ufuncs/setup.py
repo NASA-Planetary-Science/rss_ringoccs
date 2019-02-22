4bbffa4b49bdccd6d6dfd7e6294f8a7a8d0548d5
ryanmaguire <rmaguire@protonmail.com>
2019-02-22 11:53:16 -0500

    Messing around with the setup script.

--- rss_ringoccs/_ufuncs/setup.py
+++ rss_ringoccs/_ufuncs/setup.py
@@ -2,14 +2,16 @@ def configuration(parent_package=None, top_path=None):
     import numpy
     from numpy.distutils.misc_util import Configuration
 
-    config = Configuration(package_name='_special_functions',
+    config = Configuration(package_name='rss_ringoccs',
                            package_path=None,
                            parent_name=parent_package,
                            top_path=top_path)
     config.add_extension('_special_functions', ['_special_functions.c'])
+    config.add_extension('_fresnel_integrals', ['_fresnel_integrals.c'])
 
     return config
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     setup(configuration=configuration)
+

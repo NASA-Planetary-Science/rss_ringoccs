b698d79ecec0406cb77a2025459eace7ae3c7b7f
ryanmaguire <rmaguire@protonmail.com>
2019-03-03 13:48:22 -0500

    Updated error messages.

--- pipeline/get_rsr_files.sh
+++ pipeline/get_rsr_files.sh
@@ -5,11 +5,7 @@ if [ ! "$BASH_VERSION" ] ; then
 fi
 if [ "$#" != 1 ] && [ "$#" != 2 ]; then
     echo "Illegal number of arguments: 2 required. Number given: $#"
-    echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
-    echo "Example: list_of_kernels.txt containing:"
-    echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
-    echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
-    echo "./get_kernels list_of_kernels.txt ../kernels/"
+    echo "Useage: get_rsr_files.sh list_of_rsr_files.txt /path/to/data"
     exit 1
 fi
 web="https://atmos.nmsu.edu/pdsd/archive/data/"
@@ -21,18 +17,15 @@ do
     elif [ "$#" = 2 ]; then
         localdirfil="$2$dirfil"
     else
-        echo "Illegal number of arguments: Two required"
-        echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
-        echo "Example: list_of_kernels.txt containing:"
-        echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
-        echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
-        echo "./get_kernels list_of_kernels.txt ../kernels/"
+    echo "Illegal number of arguments: 2 required. Number given: $#"
+    echo "Useage: get_rsr_files.sh list_of_rsr_files.txt /path/to/data"
+    exit 1
         exit 1
     fi
     webdirfil=$web$dirfil
     if [ ! -e $localdirfil ]
     then
-            echo Current File $localdirfil does not locally exist. Fetching.
+        echo Current File $localdirfil does not locally exist. Fetching.
         if curl --output /dev/null --silent --head --fail "$webdirfil"; then
             echo "URL exists: $webdirfil"
             curl --create-dirs -o $localdirfil $webdirfil

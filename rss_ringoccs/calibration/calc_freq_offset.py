2cd13a3436a46f1c7c175032a75a17f2954fe4c3
Sophia Flury <sflury@wellesley.edu>
2019-06-26 11:57:04 -0400

    Updated resolution of frequency offset sampling from 0.05 Hz to 0.1 Hz.

--- rss_ringoccs/calibration/calc_freq_offset.py
+++ rss_ringoccs/calibration/calc_freq_offset.py
@@ -59,7 +59,7 @@ class calc_freq_offset(object):
             and sets f_spm and f_offset attributes.
         """
         # hard-set the spacing to 10 spm between each window center
-        delta_t_cent = 20.
+        delta_t_cent = 10.
 
         # storage lists -- later converted to arrays and stored as attributes
         spms = []

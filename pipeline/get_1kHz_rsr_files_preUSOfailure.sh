03891a26708ac19cd925c368d8a9f115809a8605
Sophia Flury <sflury@wellesley.edu>
2019-06-26 12:29:48 -0400

    Updates to include a script for downloading 16 kHz files.

---
+++ pipeline/get_1kHz_rsr_files_preUSOfailure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ ! "$BASH_VERSION" ] ; then
+        echo "Please use BASH to run this script ($0)" 1>&2
+        exit 1
+fi
+web="https://atmos.nmsu.edu/pdsd/archive/data/"
+# pre-USO failure 1 kHz RSR files
+input=../tables/rsr_1kHz_files_before_USO_failure.txt
+while IFS= read -r dirfil
+do
+    localdirfil="../data/$dirfil"
+    webdirfil=$web$dirfil
+    if [ ! -e $localdirfil ]
+    then
+        echo Current File $localdirfil does not locally exist. Fetching.
+        if curl --output /dev/null --silent --head --fail "$webdirfil"; then
+            echo "URL exists: $webdirfil"
+            curl --create-dirs -o $localdirfil $webdirfil
+        else
+            echo "URL does not exist: $webdirfil"
+            exit 1
+        fi
+    else
+            echo Current File "$localdirfil" already exists.
+    fi
+done < "$input"

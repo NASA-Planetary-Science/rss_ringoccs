ca1f096081a5c468261b8c1b4213e67b51e24cc7
ryanmaguire <rmaguire@protonmail.com>
2019-08-09 08:59:54 -0400

    Test function using C-Python API to pass numpy arrays (Without ufuncs).

---
+++ rss_ringoccs/_ufuncs/test.c
@@ -0,0 +1,91 @@
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#include "../../include/Python.h"
+#include "../../include/arrayobject.h"
+#include <math.h>
+
+static PyObject *MaxValue(PyObject *self, PyObject *args)
+{
+    PyArrayObject *arr;
+    int i;
+
+    /*  parse single numpy array argument */
+    if (!PyArg_ParseTuple(args, "O!", &PyArray_Type, &arr)){
+        PyErr_Format(PyExc_RuntimeError, "You done messed up.");
+        return NULL;
+    }
+
+    // Useful metadata about the data
+    int typenum    = PyArray_TYPE(arr);
+    if( typenum != NPY_DOUBLE ){
+        PyArrayObject* arr2 = (PyArrayObject*)PyArray_FromArray(
+            arr, PyArray_DescrFromType(NPY_DOUBLE), NPY_ARRAY_ALIGNED
+        );
+        Py_DECREF(arr);
+        arr = arr2;
+    }
+
+    // Useful metadata about the data
+    npy_intp* dims = PyArray_DIMS(arr);
+    npy_intp* strides = PyArray_STRIDES(arr);
+    void *data0 = PyArray_DATA(arr);
+    int n = dims[0];
+
+    double max = *(double *)data0;
+
+    for (i=0; i<n; ++i){
+        data0 += strides[0];
+        if (*(double *)data0 > max){
+            max = *(double *)data0;
+        }
+    }
+
+    /*
+     *  Another way to do it:
+     *      void* data0 = PyArray_DATA(arr);
+     *      npy_intp *strides = PyArray_STRIDES (arr);
+     *      double test, max = *(double *)&data0[0];
+     *
+     *      for (i=0; i<n; ++i){
+     *          test = *(double *)&data0[i*strides[0]];
+     *          if (test > max){
+     *              max = test;
+     *          }
+     *      }
+    */
+
+    return Py_BuildValue("d", max);
+}                
+                
+/*  define functions in module */
+static PyMethodDef DiffMethods[] =
+{
+    {
+        "MaxValue",
+        MaxValue,
+        METH_VARARGS,
+        "Evaluate the maximum of a numpy array."
+    },
+    {
+        NULL,
+        NULL,
+        0,
+        NULL
+    }
+};
+    
+static struct PyModuleDef cModPyDem =
+{
+    PyModuleDef_HEAD_INIT,
+    "testmodule", /* name of module */
+    "",
+    -1,
+    DiffMethods
+};
+    
+/* module initialization */
+PyMODINIT_FUNC PyInit_testmodule(void)
+{
+    /* IMPORTANT: this must be called */
+    import_array();
+    return PyModule_Create(&cModPyDem);
+}

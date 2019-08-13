f262d014dc18f42aa83033846d1bd9dc34f640a8
ryanmaguire <ryan_maguire@student.uml.edu>
2019-08-13 19:31:27 -0400

    Adding support for other data types.

--- rss_ringoccs/_ufuncs/__normalized_equivalent_width.h
+++ rss_ringoccs/_ufuncs/__normalized_equivalent_width.h
@@ -1,7 +1,24 @@
 #ifndef RSS_RINGOCCS_NORMALIZED_EQUIVALENT_WIDTH_H
 #define RSS_RINGOCCS_NORMALIZED_EQUIVALENT_WIDTH_H
 
-double Normeq_Double_Func(double *w_func, long n_elements){
+double Normeq_Float(float *w_func, long n_elements){
+    float tot_sq = 0.0;
+    float sq_tot = 0.0;
+    long i;
+
+    for (i = 0; i < n_elements; i++) {
+
+        /* Compute the sum and the sum of the squares.                        */
+        tot_sq += w_func[i];
+        sq_tot += w_func[i]*w_func[i];
+    }
+
+    /*  Square the sum.                                                       */
+    tot_sq *= tot_sq;
+    return n_elements * sq_tot / tot_sq;
+}
+
+double Normeq_Double(double *w_func, long n_elements){
     double tot_sq = 0.0;
     double sq_tot = 0.0;
     long i;
@@ -18,7 +35,24 @@ double Normeq_Double_Func(double *w_func, long n_elements){
     return n_elements * sq_tot / tot_sq;
 }
 
-double Normeq_Long_Func(long *w_func, long n_elements){
+double Normeq_Long_Double(long double *w_func, long n_elements){
+    long double tot_sq = 0.0;
+    long double sq_tot = 0.0;
+    long i;
+
+    for (i = 0; i < n_elements; i++) {
+
+        /* Compute the sum and the sum of the squares.                        */
+        tot_sq += w_func[i];
+        sq_tot += w_func[i]*w_func[i];
+    }
+
+    /*  Square the sum.                                                       */
+    tot_sq *= tot_sq;
+    return n_elements * sq_tot / tot_sq;
+}
+
+double Normeq_Long(long *w_func, long n_elements){
     double tot_sq = 0.0;
     double sq_tot = 0.0;
     long i;

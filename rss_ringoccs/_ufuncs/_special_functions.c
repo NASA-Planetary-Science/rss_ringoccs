d4c9c179204c40efc304320a81021a518b131ffc
ryanmaguire <ryan_maguire@student.uml.edu>
2019-08-13 18:46:43 -0400

    Replaced pointer arithmetic with indexing.

--- rss_ringoccs/_ufuncs/_special_functions.c
+++ rss_ringoccs/_ufuncs/_special_functions.c
@@ -1,78 +1,106 @@
-/*  To avoid compiler warnings about deprecated numpy stuff.        */
+/*  To avoid compiler warnings about deprecated numpy stuff.                 */
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
 /* cosine and sine are defined here. */
 #include <math.h>
 
-/*  complex data types, as well as _Complex_I, are defined here.    */
+/*  complex data types, as well as _Complex_I, are defined here.             */
 #include <complex.h>
 
-/* Include fresnel integrals header. This includes frensel_sin/cos. */
+/* Include fresnel integrals header. This includes frensel_sin/cos.          */
 #include "__fresnel_integrals.h"
 #include "__fresnel_kernel.h"
 
-/*  Various header files required for the C-Python API to work.     */
+/*  Where compute_norm_eq lives.                                             */
+#include "__normalized_equivalent_width.h"
+
+/*  Various header files required for the C-Python API to work.              */
 #include "../../include/Python.h"
 #include "../../include/ndarraytypes.h"
 #include "../../include/ufuncobject.h"
 
-static PyMethodDef _special_functions_methods[] = {{NULL, NULL, 0, NULL}};
-/*---------------------------DEFINE PYTHON FUNCTIONS--------------------------*
- * This contains the Numpy-C and Python-C API parts that allow for the above  *
- * functions to be called in Python. Numpy arrays, as well as floating point  *
- * and integer valued arguments may then be passed into these functions for   *
- * improvement in performance, as opposed to the routines written purely in   *
- * Python. Successful compiling requires the Numpy and Python header files.   *
- *----------------------------------------------------------------------------*/
-static void double_compute_normeq(char **args, npy_intp *dimensions,
-                                  npy_intp* steps, void* data)
+/*---------------------------DEFINE PYTHON FUNCTIONS-------------------------*
+ * This contains the Numpy-C and Python-C API parts that allow for the above *
+ * functions to be called in Python. Numpy arrays, as well as floating point *
+ * and integer valued arguments may then be passed into these functions for  *
+ * improvement in performance, as opposed to the routines written purely in  *
+ * Python. Successful compiling requires the Numpy and Python header files.  *
+ *---------------------------------------------------------------------------*/
+static PyObject *compute_norm_eq(PyObject *self, PyObject *args)
 {
-    double tot_sq = 0.0;
-    double sq_tot = 0.0;
-    npy_intp i;
-    npy_intp n = dimensions[0];
-    char *w_func  = args[0];
-    char *norm_eq = args[1];
+    #define FNAME "rss_ringoccs.diffrec.special_functions.compute_norm_eq\n"
+    PyArrayObject *arr;
+    PyObject *tuple = PyTuple_GetItem(args, 0);
 
-
-    npy_intp w_step = steps[0];
-
-    for (i = 0; i < n; i++) {
-        /* Compute the sum and the sum of the squares.                        */
-        tot_sq += *(double *)w_func;
-        sq_tot += *(double *)w_func * *(double *)w_func;
-
-        /*  Push the pointer forward by the appropriate increment.            */
-        w_func  += w_step;
+    if (PyLong_Check(tuple)){
+        long normeq;
+        PyArg_ParseTuple(args, "l", &normeq);
+        return PyLong_FromLong(normeq);
     }
+    else if (PyFloat_Check(tuple)){
+        double normeq;
+        PyArg_ParseTuple(args, "d", &normeq);
+        return PyFloat_FromDouble(normeq);
+    }
+    else if (PyArg_ParseTuple(args, "O!", &PyArray_Type, &arr)){
+        npy_int typenum, dim;
+        void *data;
 
-    /*  Square the sum.                                                       */
-    tot_sq *= tot_sq;
-    *(double *)norm_eq = (double)n * sq_tot / tot_sq;
+        // Check to make sure input isn't zero dimensional!
+        if (PyArray_NDIM(arr) != 1){
+            PyErr_Format(PyExc_TypeError, FNAME
+                         "\rInput must be a one-dimensional array.");
+            return NULL;
+        }
+
+        // Useful information about the data.
+        typenum = PyArray_TYPE(arr);
+        dim     = PyArray_DIMS(arr)[0];
+        data    = PyArray_DATA(arr);
+        if (dim == 0){
+            PyErr_Format(PyExc_TypeError, FNAME
+                         "\rInput array is empty.");
+            return NULL;
+        }
+
+        if (typenum == NPY_DOUBLE){
+            return PyFloat_FromDouble(Normeq_Double_Func((double *)data, dim));
+        }
+        else if (typenum == NPY_LONG){
+            return PyFloat_FromDouble(Normeq_Long_Func((long *)data, dim));
+        }
+        else {
+            PyErr_Format(PyExc_TypeError, FNAME
+                         "\rInput should be a numpy array of real numbers.");
+            return NULL;
+        }
+    }
+    else {
+        PyErr_Format(PyExc_TypeError, FNAME
+                     "\rInput should be a numpy array of real numbers.");
+        return NULL;
+    }
 }
+
+static PyMethodDef _special_functions_methods[] =
+{
+    {"compute_norm_eq", compute_norm_eq,
+     METH_VARARGS, "Compute the maximum of a numpy array."},
+    {NULL, NULL, 0, NULL}
+};
+/*-------------------------DEFINE UNIVERSAL FUNCTIONS-------------------------*/
 
 /*  Functions from __fresnel_integrals.h                                      */
 static void double_fresnelsin(char **args, npy_intp *dimensions,
                               npy_intp* steps, void* data)
 {
     npy_intp i;
-    npy_intp n = dimensions[0];
-    char *in  = args[0];
-    char *out = args[1];
-
-    npy_intp in1_step = steps[0];
-    npy_intp out1_step = steps[1];
+    npy_intp n  = dimensions[0];
+    double *in  = (double *)args[0];
+    double *out = (double *)args[1];
 
     for (i = 0; i < n; i++) {
-        /*  The function Fresnel_Sine_Taylor_to_Asymptotic_Func is defined in *
-         *  _fresnel_sin.h. Make sure this is in the current directory!       */
-        *((double *)out) = Fresnel_Sine_Taylor_to_Asymptotic_Func(
-            *(double *)in
-        );
-
-        /* Push the pointers forward by the appropriate increment.            */
-        in  += in1_step;
-        out += out1_step;
+        out[i] = Fresnel_Sine_Taylor_to_Asymptotic_Func(in[i]);
     }
 }
 
@@ -80,23 +108,14 @@ static void double_fresnelcos(char **args, npy_intp *dimensions,
                               npy_intp* steps, void* data)
 {
     npy_intp i;
-    npy_intp n = dimensions[0];
-    char *in = args[0];
-    char *out = args[1];
-
-    npy_intp in1_step = steps[0];
-    npy_intp out1_step = steps[1];
+    npy_intp n  = dimensions[0];
+    double *in  = (double *)args[0];
+    double *out = (double *)args[1];
 
     for (i = 0; i < n; i++) {
         /*  The function Fresnel_Cosine_Taylor_to_Asymptotic_Func is defined  *
          *  in _fresnel_cosine.h. Make sure this is in the current directory! */
-        *((double *)out) = Fresnel_Cosine_Taylor_to_Asymptotic_Func(
-            *(double *)in
-        );
-
-        /* Push the pointers forward by the appropriate increment.            */
-        in  += in1_step;
-        out += out1_step;
+        out[i] = Fresnel_Cosine_Taylor_to_Asymptotic_Func(in[i]);
     }
 }
 
@@ -105,23 +124,14 @@ static void complex_sqwellsol(char **args, npy_intp *dimensions,
 {
     npy_intp i;
     npy_intp n = dimensions[0];
-    char *x = args[0];
-    char *a = args[1];
-    char *b = args[2];
-    char *F = args[3];
-    char *out = args[4];
-    npy_intp in_step = steps[0];
-    npy_intp out_step = steps[4];
+    double *x =  (double *)args[0];
+    double  a  = *(double *)args[1];
+    double  b  = *(double *)args[2];
+    double  F  = *(double *)args[3];
+    double complex *out = (double complex *)args[4];
 
     for (i = 0; i < n; i++) {
-        /*BEGIN main ufunc computation*/
-        *((double complex*)out) = Square_Well_Diffraction_Solution_Func(
-            *(double *)x, *(double *)a, *(double *)b, *(double *)F
-        );
-        /*END main ufunc computation*/
-
-        x += in_step;
-        out += out_step;
+        out[i] = Square_Well_Diffraction_Solution_Func(x[i], a, b, F);
     }
 }
 
@@ -130,23 +140,14 @@ static void complex_invsqwellsol(char **args, npy_intp *dimensions,
 {
     npy_intp i;
     npy_intp n = dimensions[0];
-    char *x = args[0];
-    char *a = args[1];
-    char *b = args[2];
-    char *F = args[3];
-    char *out = args[4];
-    npy_intp in_step = steps[0];
-    npy_intp out_step = steps[4];
+    double *x  =  (double *)args[0];
+    double a   = *(double *)args[1];
+    double b   = *(double *)args[2];
+    double F   = *(double *)args[3];
+    double complex *out = (double complex *)args[4];
 
     for (i = 0; i < n; i++) {
-        /*BEGIN main ufunc computation*/
-        *((double complex*)out) = Inverted_Square_Well_Diffraction_Solution_Func(
-            *(double *)x, *(double *)a, *(double *)b, *(double *)F
-        );
-        /*END main ufunc computation*/
-
-        x += in_step;
-        out += out_step;
+        out[i] = Inverted_Square_Well_Diffraction_Solution_Func(x[i], a, b, F);
     }
 }
 
@@ -257,7 +258,6 @@ static void double_dpsi_dphi(char **args, npy_intp *dimensions,
 }
 
 /*  Define pointers to the C functions.                                       */
-PyUFuncGenericFunction compute_normeq_funcs[1]  = {&double_compute_normeq};
 PyUFuncGenericFunction fresnel_sin_funcs[1]     = {&double_fresnelsin};
 PyUFuncGenericFunction fresnel_cos_funcs[1]     = {&double_fresnelcos};
 PyUFuncGenericFunction sqwellsol_funcs[1]       = {&complex_sqwellsol};
@@ -294,7 +294,6 @@ static struct PyModuleDef moduledef = {
 
 PyMODINIT_FUNC PyInit__special_functions(void)
 {
-    PyObject *compute_norm_eq;
     PyObject *fresnel_sin;
     PyObject *fresnel_cos;
     PyObject *square_well_diffraction;
@@ -312,11 +311,6 @@ PyMODINIT_FUNC PyInit__special_functions(void)
     import_array();
     import_umath();
 
-    compute_norm_eq = PyUFunc_FromFuncAndData(
-        compute_normeq_funcs, PyuFunc_data, double_double_types, 1, 1, 1,
-        PyUFunc_None, "compute_norm_eq", "comput_norm_eq_docstring", 0
-    );
-
     fresnel_sin = PyUFunc_FromFuncAndData(fresnel_sin_funcs, PyuFunc_data,
                                           double_double_types, 1, 1, 1,
                                           PyUFunc_None, "fresnel_sin",
@@ -357,7 +351,6 @@ PyMODINIT_FUNC PyInit__special_functions(void)
 
     d = PyModule_GetDict(m);
 
-    PyDict_SetItemString(d, "compute_norm_eq", compute_norm_eq);
     PyDict_SetItemString(d, "fresnel_sin", fresnel_sin);
     PyDict_SetItemString(d, "fresnel_cos", fresnel_cos);
     PyDict_SetItemString(d, "square_well_diffraction", square_well_diffraction);
@@ -367,7 +360,6 @@ PyMODINIT_FUNC PyInit__special_functions(void)
     PyDict_SetItemString(d, "fresnel_psi", fresnel_psi);
     PyDict_SetItemString(d, "fresnel_dpsi_dphi", fresnel_dpsi_dphi);
 
-    Py_DECREF(compute_norm_eq);
     Py_DECREF(fresnel_sin);
     Py_DECREF(fresnel_cos);
     Py_DECREF(square_well_diffraction);
@@ -381,7 +373,6 @@ PyMODINIT_FUNC PyInit__special_functions(void)
 #else
 PyMODINIT_FUNC init__funcs(void)
 {
-    PyObject *compute_norm_eq;
     PyObject *fresnel_sin;
     PyObject *fresnel_cos;
     PyObject *square_well_diffraction;
@@ -399,11 +390,6 @@ PyMODINIT_FUNC init__funcs(void)
     import_array();
     import_umath();
 
-    compute_norm_eq = PyUFunc_FromFuncAndData(
-        compute_normeq_funcs,PyuFunc_data, double_double_types, 1, 1, 1,
-        PyUFunc_None, "compute_norm_eq", "comput_norm_eq_docstring", 0
-    );
-
     fresnel_sin = PyUFunc_FromFuncAndData(fresnel_sin_funcs, PyuFunc_data,
                                           double_double_types, 1, 1, 1,
                                           PyUFunc_None, "fresnel_sin",
@@ -444,7 +430,6 @@ PyMODINIT_FUNC init__funcs(void)
 
     d = PyModule_GetDict(m);
 
-    PyDict_SetItemString(d, "compute_norm_eq", compute_norm_eq);
     PyDict_SetItemString(d, "fresnel_sin", fresnel_sin);
     PyDict_SetItemString(d, "fresnel_cos", fresnel_cos);
     PyDict_SetItemString(d, "square_well_diffraction", square_well_diffraction);
@@ -454,7 +439,6 @@ PyMODINIT_FUNC init__funcs(void)
     PyDict_SetItemString(d, "fresnel_psi", fresnel_psi);
     PyDict_SetItemString(d, "fresnel_dpsi_dphi", fresnel_dpsi_dphi);
 
-    Py_DECREF(compute_norm_eq);
     Py_DECREF(fresnel_sin);
     Py_DECREF(fresnel_cos);
     Py_DECREF(square_well_diffraction);

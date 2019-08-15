930af8adbfbd421fa48deb002d02c05b651a9ac8
ryanmaguire <rmaguire@protonmail.com>
2019-08-15 16:29:36 -0400

    Changed setup script and moved to diffrec directory.

---
+++ rss_ringoccs/diffrec/setup.py
@@ -0,0 +1,22 @@
+def configuration(parent_package=None, top_path=None):
+    import numpy
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration(package_name=None,
+                           package_path=None,
+                           parent_name=parent_package,
+                           top_path=top_path)
+    config.add_extension('_special_functions',
+                         ['src/_special_functions.c'])
+    config.add_extension('_window_functions',
+                         ['src/_window_functions.c'])
+    config.add_extension('_diffraction_functions',
+                         ['src/_diffraction_functions.c'])
+    config.add_extension('_math_functions',
+                         ['src/_math_functions.c'])
+
+    return config
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)

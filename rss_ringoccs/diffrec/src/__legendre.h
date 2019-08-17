07e6b03e5a6a410f05d3117303b25ea0fd139f46
ryanmaguire <ryan_maguire@student.uml.edu>
2019-08-16 21:29:14 -0400

    Adding Legendre Polynomials to special_functions.

---
+++ rss_ringoccs/diffrec/src/__legendre.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void Legendre_Coeffs(double *poly, double x, int order){
+    int i;
+    poly[0] = 1.0;
+    poly[1] = x;
+    for (i=2; i<order; ++i){
+        poly[i] = ((2.0*i-1.0)*x*poly[i-1]-(i-1.0)*poly[i-2])/i;
+    }
+}
+
+void Fresnel_Coeffs(double *poly, double *legendre_p, int order){
+    int i;
+    for (i=0; i<order; ++i){
+        poly[i] = (legendre_p[i]-legendre_p[1]*legendre_p[i+1])/(i+2.0);
+    }
+}

1dac4faa4eed165e380e021f98533f5613ad8cc8
ryanmaguire <rmaguire@protonmail.com>
2019-08-16 21:28:49 -0400

    Updating function names.

--- rss_ringoccs/diffrec/src/__window_functions.h
+++ rss_ringoccs/diffrec/src/__window_functions.h
@@ -8,7 +8,17 @@
 #include "__kaiser_bessel.h"
 #include "__modified_kaiser_bessel.h"
 
-double Rect_Window_Func(double x, double W)
+float Rect_Window_Float(float x, float W)
+{
+    if (fabsf(x) <= W/2.0){
+        return 1.0;
+    }
+    else {
+        return 0.0;
+    }
+}
+
+double Rect_Window_Double(double x, double W)
 {
     if (fabs(x) <= W/2.0){
         return 1.0;
@@ -18,9 +28,45 @@ double Rect_Window_Func(double x, double W)
     }
 }
 
-double Coss_Window_Func(double x, double W)
+long double Rect_Window_Long_Double(long double x, long double W)
+{
+    if (fabsl(x) <= W/2.0){
+        return 1.0;
+    }
+    else {
+        return 0.0;
+    }
+}
+
+float Coss_Window_Float(float x, float W)
+{
+    if (fabsf(x) <= W/2.0){
+        x *= ONE_PI/W;
+        x = cos(x);
+        x *=x;
+        return x;
+    }
+    else {
+        return 0.0;
+    }
+}
+
+double Coss_Window_Double(double x, double W)
 {
     if (fabs(x) <= W/2.0){
+        x *= ONE_PI/W;
+        x = cos(x);
+        x *=x;
+        return x;
+    }
+    else {
+        return 0.0;
+    }
+}
+
+long double Coss_Window_Long_Double(long double x, long double W)
+{
+    if (fabsl(x) <= W/2.0){
         x *= ONE_PI/W;
         x = cos(x);
         x *=x;

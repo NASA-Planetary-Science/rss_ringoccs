55a89d1c6d10d5a5533d244e444e4d564197b9c5
Unknown <ryan_maguire@student.uml.edu>
2019-08-25 20:41:01 -0400

    Adding basic conversion functions used for variables in physics.

---
+++ rss_ringoccs/diffrec/src/__physics_functions.h
@@ -0,0 +1,35 @@
+#ifndef RSS_RINGOCCS_PHYSICS_FUNCTIONS_H
+#define RSS_RINGOCCS_PHYSICS_FUNCTIONS_H
+
+#include "__math_constants.h"
+
+/*  Wavelength to Wavenumber function.                                        */
+
+/* Convert wavelength to wavenumber for a float input. Returns float.         */
+float Wavelength_To_Wavenumber_Float(float lambda){
+    return TWO_PI / lambda;
+}
+
+
+double Wavelength_To_Wavenumber_Double(double lambda){
+    return TWO_PI / lambda;
+}
+
+long double Wavelength_To_Wavenumber_Long_Double(long double lambda){
+    return TWO_PI / lambda;
+}
+
+/*  Frequency to Wavelength function.                                         */
+float Frequency_To_Wavelength_Float(float frequency){
+    return SPEED_OF_LIGHT_KM / frequency;
+}
+
+double Frequency_To_Wavelength_Double(double frequency){
+    return SPEED_OF_LIGHT_KM / frequency;
+}
+
+long double Frequency_To_Wavelength_Long_Double(long double frequency){
+    return SPEED_OF_LIGHT_KM / frequency;
+}
+
+#endif

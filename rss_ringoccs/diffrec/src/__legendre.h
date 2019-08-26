95f08d0d6f765bd978a033dfd40c2444d0a1385e
ryanmaguire <rmaguire@protonmail.com>
2019-08-25 20:40:05 -0400

    Updated polynomial functions, added Fresnel kernel coefficient function.

--- rss_ringoccs/diffrec/src/__legendre.h
+++ rss_ringoccs/diffrec/src/__legendre.h
@@ -1,17 +1,49 @@
 #include <stdio.h>
 
-void Legendre_Coeffs(double *poly, double x, int order){
+void Legendre_Polynomials(double *legendre_p, double x, int order){
     int i;
-    poly[0] = 1.0;
-    poly[1] = x;
+    legendre_p[0] = 1.0;
+    legendre_p[1] = x;
     for (i=2; i<order; ++i){
-        poly[i] = ((2.0*i-1.0)*x*poly[i-1]-(i-1.0)*poly[i-2])/i;
+        legendre_p[i] = ((2.0*i-1.0)*x*legendre_p[i-1] -
+                         (i-1.0) * legendre_p[i-2]) / i;
     }
 }
 
-void Alt_Legendre_Coeffs(double *poly, double *legendre_p, int order){
+void Alt_Legendre_Polynomials(double *poly, double *legendre_p, int order){
     int i;
     for (i=0; i<order; ++i){
         poly[i] = (legendre_p[i]-legendre_p[1]*legendre_p[i+1])/(i+2.0);
     }
+}
+
+void Fresnel_Kernel_Coefficients(double *fresnel_ker_coeffs, double *legendre_p,
+                                 double *alt_legendre_p, double Legendre_Coeff,
+                                 int order){
+    int i, j;
+    int order_by_2 = (order+1)/2;
+
+    for (i=1; i<=order_by_2; ++i){
+        fresnel_ker_coeffs[i-1] = 0.0;
+        for (j=0; j<i; ++j){
+            fresnel_ker_coeffs[i-1] += legendre_p[j+1]*legendre_p[i-j];
+        }
+        fresnel_ker_coeffs[i-1] = alt_legendre_p[i-1] -
+                                  Legendre_Coeff*fresnel_ker_coeffs[i-1];
+    }
+
+    /*  Compute along the upper triangle of the square.                   */
+    for (i=order_by_2+1; i<order; ++i){
+        fresnel_ker_coeffs[i-1] = 0.0;
+        for (j=i-order_by_2; j<order_by_2; ++j){
+            fresnel_ker_coeffs[i-1] += legendre_p[j+1]*legendre_p[i-j];
+        }
+        fresnel_ker_coeffs[i-1] = alt_legendre_p[i-1] -
+                                  Legendre_Coeff*fresnel_ker_coeffs[i-1];
+    }
+
+    /* Compute the last coefficient.                                      */
+    fresnel_ker_coeffs[i-1] = legendre_p[order_by_2]*legendre_p[order_by_2];
+    fresnel_ker_coeffs[i-1] = alt_legendre_p[order-1] -
+                              Legendre_Coeff*fresnel_ker_coeffs[i-1];
 }

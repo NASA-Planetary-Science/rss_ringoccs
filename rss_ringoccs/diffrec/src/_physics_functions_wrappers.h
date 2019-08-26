1f24c3ae2c075eed2cf717a641ba2b1b78ff5bd3
ryanmaguire <ryan_maguire@student.uml.edu>
2019-08-26 15:58:16 -0400

    Moving C wrappers to their own file.

---
+++ rss_ringoccs/diffrec/src/_physics_functions_wrappers.h
@@ -0,0 +1,49 @@
+#ifndef RSS_RINGOCCS_PHYSICS_FUNCTIONS_WRAPPER_H
+#define RSS_RINGOCCS_PHYSICS_FUNCTIONS_WRAPPER_H
+
+/*  Where npy_intp is defined.                                                */
+#include <numpy/ndarraytypes.h>
+
+/*  Location of the C functions to be wrapped.                                */
+#include "__physics_functions.h"
+
+/*  Wrappers for the wavelength_to_wavenumber function.                       */
+
+static void float_wavelength_to_wavenumber(char **args, npy_intp *dimensions,
+                                           npy_intp *steps, void *data){
+    long i;
+    long n  = dimensions[0];
+    float *in  = (float *)args[0];
+    float *out = (float *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = Wavelength_To_Wavenumber_Float(in[i]);
+    }
+}
+
+static void double_wavelength_to_wavenumber(char **args, npy_intp *dimensions,
+                                            npy_intp *steps, void *data){
+    long i;
+    long n  = dimensions[0];
+    double *in  = (double *)args[0];
+    double *out = (double *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = Wavelength_To_Wavenumber_Double(in[i]);
+    }
+}
+
+static void long_double_wavelength_to_wavenumber(char **args,
+                                                 npy_intp *dimensions,
+                                                 npy_intp *steps, void *data){
+    long i;
+    long n  = dimensions[0];
+    long double *in  = (long double *)args[0];
+    long double *out = (long double *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = Wavelength_To_Wavenumber_Long_Double(in[i]);
+    }
+}
+
+#endif

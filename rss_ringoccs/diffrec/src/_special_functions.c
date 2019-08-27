ccf5ed392ce4e4f3ff49851ee753bb250554da5d
ryanmaguire <rmaguire@protonmail.com>
2019-08-27 13:56:43 -0400

    Added support for float and long double (float32 and float128 in numpy).

--- rss_ringoccs/diffrec/src/_special_functions.c
+++ rss_ringoccs/diffrec/src/_special_functions.c
@@ -9,11 +9,14 @@
 
 /* Include fresnel integrals header. This includes frensel_sin/cos.          */
 #include "__fresnel_kernel.h"
+#include "_fresnel_diffraction_wrappers.h"
 #include "_physics_functions_wrappers.h"
 #include "_fresnel_integrals_wrappers.h"
 
 /*  Where compute_norm_eq lives.                                             */
 #include "__normalized_equivalent_width.h"
+#include "__max.h"
+#include "__min.h"
 
 /*  Various header files required for the C-Python API to work.              */
 #include <Python.h>
@@ -27,9 +30,7 @@
  * improvement in performance, as opposed to the routines written purely in  *
  * Python. Successful compiling requires the Numpy and Python header files.  *
  *---------------------------------------------------------------------------*/
-static PyObject *compute_norm_eq(PyObject *self, PyObject *args)
-{
-    #define FNAME "rss_ringoccs.diffrec.special_functions.compute_norm_eq\n"
+static PyObject *compute_norm_eq(PyObject *self, PyObject *args){
     PyArrayObject *arr;
     PyObject *tuple = PyTuple_GetItem(args, 0);
 
@@ -49,8 +50,11 @@ static PyObject *compute_norm_eq(PyObject *self, PyObject *args)
 
         // Check to make sure input isn't zero dimensional!
         if (PyArray_NDIM(arr) != 1){
-            PyErr_Format(PyExc_TypeError, FNAME
-                         "\rInput must be a one-dimensional array.");
+            PyErr_Format(
+                PyExc_TypeError,
+                "rss_ringoccs.diffrec.special_functions.compute_norm_eq\n"
+                "\rInput must be a one-dimensional array."
+            );
             return NULL;
         }
 
@@ -59,32 +63,151 @@ static PyObject *compute_norm_eq(PyObject *self, PyObject *args)
         dim     = PyArray_DIMS(arr)[0];
         data    = PyArray_DATA(arr);
         if (dim == 0){
-            PyErr_Format(PyExc_TypeError, FNAME
-                         "\rInput array is empty.");
+            PyErr_Format(
+                PyExc_TypeError,
+                "rss_ringoccs.diffrec.special_functions.compute_norm_eq\n"
+                "\rInput array is empty."
+            );
             return NULL;
         }
 
         if (typenum == NPY_DOUBLE){
             return PyFloat_FromDouble(Normeq_Double((double *)data, dim));
         }
-        else if (typenum == NPY_LONGDOUBLE){
-            return PyFloat_FromDouble(Normeq_Long_Double((long double *)data, dim));
-        }
         else if (typenum == NPY_FLOAT){
             return PyFloat_FromDouble(Normeq_Float((float *)data, dim));
+        }
+        else if (typenum == NPY_LONGDOUBLE){
+            return PyFloat_FromDouble(Normeq_Long_Double((long double *)data, dim));
         }
         else if (typenum == NPY_LONG){
             return PyFloat_FromDouble(Normeq_Long((long *)data, dim));
         }
         else {
-            PyErr_Format(PyExc_TypeError, FNAME
-                         "\rInput should be a numpy array of real numbers.");
+            PyErr_Format(
+                PyExc_TypeError,
+                "rss_ringoccs.diffrec.special_functions.compute_norm_eq\n"
+                "\rInput should be a numpy array of real numbers."
+            );
             return NULL;
         }
     }
     else {
-        PyErr_Format(PyExc_TypeError, FNAME
+        PyErr_Format(PyExc_TypeError, 
                      "\rInput should be a numpy array of real numbers.");
+        return NULL;
+    }
+}
+
+static PyObject *max(PyObject *self, PyObject *args){
+    PyArrayObject *arr;
+    PyObject *tuple = PyTuple_GetItem(args, 0);
+
+    if (PyLong_Check(tuple)){
+        long max;
+        PyArg_ParseTuple(args, "l", &max);
+        return PyLong_FromLong(max);
+    }
+    else if (PyFloat_Check(tuple)){
+        double max;
+        PyArg_ParseTuple(args, "d", &max);
+        return PyFloat_FromDouble(max);
+    }
+    else if (PyArg_ParseTuple(args, "O!", &PyArray_Type, &arr)){
+        npy_int typenum, dim;
+        void *data;
+
+        // Check to make sure input isn't zero dimensional!
+        if (PyArray_NDIM(arr) != 1){
+            PyErr_Format(PyExc_TypeError,
+                         "rss_ringoccs.diffrec.math_functions.max\n"
+                         "\rInput must be a one-dimensional array.");
+            return NULL;
+        }
+
+        // Useful information about the data.
+        typenum = PyArray_TYPE(arr);
+        dim     = PyArray_DIMS(arr)[0];
+        data    = PyArray_DATA(arr);
+
+        if (dim == 0){
+            PyErr_Format(PyExc_TypeError,
+                         "rss_ringoccs.diffrec.math_functions.max\n"
+                         "\rInput array is empty.");
+            return NULL;
+        }
+
+        if (typenum == NPY_DOUBLE){
+            return PyFloat_FromDouble(Max_Double((double *)data, dim));
+        }
+        else if (typenum == NPY_LONG){
+            return PyLong_FromLong(Max_Long((long *)data, dim));
+        }
+        else {
+            PyErr_Format(PyExc_TypeError,
+                         "rss_ringoccs.diffrec.math_functions.max\n"
+                         "\rInput should be a numpy array of numbers.");
+            return NULL;
+        }
+    }
+    else {
+        PyErr_Format(PyExc_TypeError,
+                     "rss_ringoccs.diffrec.math_functions.max\n"
+                     "\rInput should be a numpy array of numbers.");
+        return NULL;
+    }
+}
+
+static PyObject *min(PyObject *self, PyObject *args){
+    PyArrayObject *arr;
+    PyObject *tuple = PyTuple_GetItem(args, 0);
+
+    if (PyLong_Check(tuple)){
+        long min;
+        PyArg_ParseTuple(args, "l", &min);
+        return PyLong_FromLong(min);
+    }
+    else if (PyFloat_Check(tuple)){
+        double min;
+        PyArg_ParseTuple(args, "d", &min);
+        return PyFloat_FromDouble(min);
+    }
+    else if (PyArg_ParseTuple(args, "O!", &PyArray_Type, &arr)){
+        npy_int typenum, dim;
+        void *data;
+
+        // Check to make sure input isn't zero dimensional!
+        if (PyArray_NDIM(arr) != 1){
+            PyErr_Format(PyExc_TypeError,
+                         "rss_ringoccs.diffrec.math_functions.min\n"
+                         "\rInput must be a one-dimensional array.");
+            return NULL;
+        }
+
+        // Useful information about the data.
+        typenum = PyArray_TYPE(arr);
+        dim     = PyArray_DIMS(arr)[0];
+        data    = PyArray_DATA(arr);
+
+        if (typenum == NPY_DOUBLE){
+            return PyFloat_FromDouble(Min_Double((double *)data, dim));
+        }
+        else if (typenum == NPY_LONG){
+            return PyLong_FromLong(Min_Long((long *)data, dim));
+        }
+        else {
+            PyErr_Format(PyExc_TypeError,
+                        "rss_ringoccs.diffrec.math_functions.min\n"
+                        "\rInput should be a numpy array of real numbers"
+                        "or a floating point/integer value.");
+            return NULL;
+        }
+    }
+    else {
+        PyErr_Format(PyExc_TypeError,
+                     "rss_ringoccs.diffrec.math_functions.min\n"
+                     "\rInput should be a numpy array of numbers,"
+                     "or a floating point/integer value.");
         return NULL;
     }
 }
@@ -92,7 +215,9 @@ static PyObject *compute_norm_eq(PyObject *self, PyObject *args)
 static PyMethodDef _special_functions_methods[] =
 {
     {"compute_norm_eq", compute_norm_eq,
-     METH_VARARGS, "Compute the maximum of a numpy array."},
+     METH_VARARGS, "Compute the normalized equivalent width of an array."},
+    {"max", max, METH_VARARGS, "Compute the maximum of a numpy array."},
+    {"min", min, METH_VARARGS, "Compute the minimum of a numpy array."},
     {NULL, NULL, 0, NULL}
 };
 /*-------------------------DEFINE UNIVERSAL FUNCTIONS-------------------------*/
@@ -179,25 +304,76 @@ static void double_dpsi_dphi(char **args, npy_intp *dimensions,
 }
 
 /*  Define pointers to the C functions.                                       */
+PyUFuncGenericFunction invsqwellsol_funcs[3] = {
+    &complex_float_inv_square_well,
+    &complex_double_inv_square_well,
+    &complex_long_double_inv_square_well
+};
+
+PyUFuncGenericFunction frequency_to_wavelength_funcs[3] = {
+    &float_frequency_to_wavelength,
+    &double_frequency_to_wavelength,
+    &long_double_frequency_to_wavelength
+};
+
+PyUFuncGenericFunction fresnel_cos_funcs[3] = {
+    &float_fresnelcos,
+    &double_fresnelcos,
+    &long_double_fresnelcos
+
+};
+
+PyUFuncGenericFunction fresnel_sin_funcs[3] = {
+    &float_fresnelsin,
+    &double_fresnelsin,
+    &long_double_fresnelsin
+};
+
+PyUFuncGenericFunction psi_funcs[1]             = {&double_psi};
+PyUFuncGenericFunction dpsi_funcs[1]            = {&double_dpsi_dphi};
+
+PyUFuncGenericFunction sqwellsol_funcs[3] = {
+    &complex_float_square_well,
+    &complex_double_square_well,
+    &complex_long_double_square_well
+};
+
+PyUFuncGenericFunction sqwellphase_funcs[3] = {
+    &float_square_well_phase,
+    &double_square_well_phase,
+    &long_double_square_well_phase
+};
+
 PyUFuncGenericFunction wavelength_to_wavenumber_funcs[3] = {
     &float_wavelength_to_wavenumber,
     &double_wavelength_to_wavenumber,
     &long_double_wavelength_to_wavenumber
 };
 
-PyUFuncGenericFunction fresnel_sin_funcs[1]     = {&double_fresnelsin};
-PyUFuncGenericFunction fresnel_cos_funcs[1]     = {&double_fresnelcos};
-PyUFuncGenericFunction psi_funcs[1]             = {&double_psi};
-PyUFuncGenericFunction dpsi_funcs[1]            = {&double_dpsi_dphi};
-
 /*  Input and return types for double input and out.                          */
+static void *PyuFunc_None_3[3] = {NULL, NULL, NULL};
+static void *PyuFunc_data[1]   = {NULL};
+
 static char double_double_types[2] = {NPY_DOUBLE, NPY_DOUBLE};
-static void *PyuFunc_data[1] = {NULL};
 
 /* Input and return types for fresnel_psi.                                    */
 static char octo_double_types[8] = {NPY_DOUBLE, NPY_DOUBLE, NPY_DOUBLE,
                                     NPY_DOUBLE, NPY_DOUBLE, NPY_DOUBLE,
                                     NPY_DOUBLE, NPY_DOUBLE};
+
+static char real_in_real_out[6] = {NPY_FLOAT, NPY_FLOAT,
+                                   NPY_DOUBLE, NPY_DOUBLE,
+                                   NPY_LONGDOUBLE, NPY_LONGDOUBLE};
+
+static char four_real_in_one_real_out[15] = {NPY_FLOAT, NPY_FLOAT,
+                                             NPY_FLOAT, NPY_FLOAT,
+                                             NPY_CFLOAT,
+                                             NPY_DOUBLE, NPY_DOUBLE,
+                                             NPY_DOUBLE, NPY_DOUBLE,
+                                             NPY_CDOUBLE,
+                                             NPY_LONGDOUBLE, NPY_LONGDOUBLE,
+                                             NPY_LONGDOUBLE, NPY_LONGDOUBLE,
+                                             NPY_CLONGDOUBLE};
 
 #if PY_VERSION_HEX >= 0x03000000
 static struct PyModuleDef moduledef = {
@@ -214,10 +390,15 @@ static struct PyModuleDef moduledef = {
 
 PyMODINIT_FUNC PyInit__special_functions(void)
 {
+    PyObject *inverse_square_well_diffraction;
+    PyObject *frequency_to_wavelength;
     PyObject *fresnel_sin;
     PyObject *fresnel_cos;
     PyObject *fresnel_psi;
     PyObject *fresnel_dpsi_dphi;
+    PyObject *square_well_diffraction;
+    PyObject *square_well_phase;
+    PyObject *wavelength_to_wavenumber;
     PyObject *m, *d;
 
     m = PyModule_Create(&moduledef);
@@ -228,19 +409,16 @@ PyMODINIT_FUNC PyInit__special_functions(void)
     import_array();
     import_umath();
 
-    fresnel_sin = PyUFunc_FromFuncAndData(fresnel_sin_funcs, PyuFunc_data,
-                                          double_double_types, 1, 1, 1,
-                                          PyUFunc_None, "fresnel_sin",
-                                          "fresnel_sin_docstring", 0);
+    inverse_square_well_diffraction = PyUFunc_FromFuncAndData(
+        invsqwellsol_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "inverse_square_well_diffraction", 
+        "inverse_square_well_diffraction_docstring", 0
+    );
 
-    fresnel_cos = PyUFunc_FromFuncAndData(fresnel_cos_funcs, PyuFunc_data,
-                                          double_double_types, 1, 1, 1,
-                                          PyUFunc_None, "fresnel_cos",
-                                          "fresnel_cos_docstring", 0);
-
-    fresnel_psi = PyUFunc_FromFuncAndData(
-        psi_funcs, PyuFunc_data, octo_double_types, 1, 7, 1,
-        PyUFunc_None, "fresnel_psi",  "fresnel_psi_docstring", 0
+    frequency_to_wavelength = PyUFunc_FromFuncAndData(
+        frequency_to_wavelength_funcs, PyuFunc_None_3, real_in_real_out,
+        3, 1, 1, PyUFunc_None, "frequency_to_wavelength",
+        "frequency_to_wavelength_docstring", 0
     );
 
     fresnel_dpsi_dphi = PyUFunc_FromFuncAndData(
@@ -248,27 +426,77 @@ PyMODINIT_FUNC PyInit__special_functions(void)
         PyUFunc_None, "fresnel_dpsi_dphi",  "fresnel_dpsi_dphi_docstring", 0
     );
 
+    fresnel_cos = PyUFunc_FromFuncAndData(
+        fresnel_cos_funcs, PyuFunc_None_3, real_in_real_out, 3, 1, 1,
+        PyUFunc_None, "fresnel_cos", "fresnel_cos_docstring", 0
+    );
+
+    fresnel_psi = PyUFunc_FromFuncAndData(
+        psi_funcs, PyuFunc_data, octo_double_types, 1, 7, 1,
+        PyUFunc_None, "fresnel_psi",  "fresnel_psi_docstring", 0
+    );
+
+    fresnel_sin = PyUFunc_FromFuncAndData(
+        fresnel_sin_funcs, PyuFunc_data, double_double_types, 1, 1, 1,
+        PyUFunc_None, "fresnel_sin", "fresnel_sin_docstring", 0
+    );
+
+    square_well_diffraction = PyUFunc_FromFuncAndData(
+        sqwellsol_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "square_well_diffraction", 
+        "square_well_diffraction_docstring", 0
+    );
+
+    square_well_phase = PyUFunc_FromFuncAndData(
+        sqwellphase_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "square_well_phase", 
+        "square_well_phase_docstring", 0
+    );
+
+    wavelength_to_wavenumber = PyUFunc_FromFuncAndData(
+        wavelength_to_wavenumber_funcs, PyuFunc_None_3, real_in_real_out,
+        3, 1, 1, PyUFunc_None, "wavelength_to_wavenumber",
+        "wavelength_to_wavenumber_docstring", 0
+    );
+
     d = PyModule_GetDict(m);
 
+    PyDict_SetItemString(d, "inverse_square_well_diffraction",
+                         inverse_square_well_diffraction);
+    PyDict_SetItemString(d, "frequency_to_wavelength", frequency_to_wavelength);
     PyDict_SetItemString(d, "fresnel_sin", fresnel_sin);
     PyDict_SetItemString(d, "fresnel_cos", fresnel_cos);
     PyDict_SetItemString(d, "fresnel_psi", fresnel_psi);
     PyDict_SetItemString(d, "fresnel_dpsi_dphi", fresnel_dpsi_dphi);
+    PyDict_SetItemString(d, "square_well_diffraction", square_well_diffraction);
+    PyDict_SetItemString(d, "square_well_phase", square_well_phase);
+    PyDict_SetItemString(d, "wavelength_to_wavenumber",
+                         wavelength_to_wavenumber);
 
+    Py_DECREF(inverse_square_well_diffraction);
+    Py_DECREF(frequency_to_wavelength);
     Py_DECREF(fresnel_sin);
     Py_DECREF(fresnel_cos);
     Py_DECREF(fresnel_psi);
     Py_DECREF(fresnel_dpsi_dphi);
+    Py_DECREF(square_well_diffraction);
+    Py_DECREF(square_well_phase);
+    Py_DECREF(wavelength_to_wavenumber);
 
     return m;
 }
 #else
 PyMODINIT_FUNC init__funcs(void)
 {
+    PyObject *inverse_square_well_diffraction;
+    PyObject *frequency_to_wavelength;
     PyObject *fresnel_sin;
     PyObject *fresnel_cos;
     PyObject *fresnel_psi;
     PyObject *fresnel_dpsi_dphi;
+    PyObject *square_well_diffraction;
+    PyObject *square_well_phase;
+    PyObject *wavelength_to_wavenumber;
     PyObject *m, *d;
 
     m = Py_InitModule("__funcs", _special_functions_methods);
@@ -279,19 +507,16 @@ PyMODINIT_FUNC init__funcs(void)
     import_array();
     import_umath();
 
-    fresnel_sin = PyUFunc_FromFuncAndData(fresnel_sin_funcs, PyuFunc_data,
-                                          double_double_types, 1, 1, 1,
-                                          PyUFunc_None, "fresnel_sin",
-                                          "fresnel_sin_docstring", 0);
+    inverse_square_well_diffraction = PyUFunc_FromFuncAndData(
+        invsqwellsol_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "inverse_square_well_diffraction", 
+        "inverse_square_well_diffraction_docstring", 0
+    );
 
-    fresnel_cos = PyUFunc_FromFuncAndData(fresnel_cos_funcs, PyuFunc_data,
-                                          double_double_types, 1, 1, 1,
-                                          PyUFunc_None, "fresnel_cos",
-                                          "fresnel_cos_docstring", 0);
-
-    fresnel_psi = PyUFunc_FromFuncAndData(
-        psi_funcs, PyuFunc_data, octo_double_types, 1, 7, 1,
-        PyUFunc_None, "fresnel_psi",  "fresnel_psi_docstring", 0
+    frequency_to_wavelength = PyUFunc_FromFuncAndData(
+        frequency_to_wavelength_funcs, PyuFunc_None_3, real_in_real_out,
+        3, 1, 1, PyUFunc_None, "frequency_to_wavelength",
+        "frequency_to_wavelength_docstring", 0
     );
 
     fresnel_dpsi_dphi = PyUFunc_FromFuncAndData(
@@ -299,17 +524,62 @@ PyMODINIT_FUNC init__funcs(void)
         PyUFunc_None, "fresnel_dpsi_dphi",  "fresnel_dpsi_dphi_docstring", 0
     );
 
+    fresnel_cos = PyUFunc_FromFuncAndData(
+        fresnel_cos_funcs, PyuFunc_None_3, real_in_real_out, 3, 1, 1,
+        PyUFunc_None, "fresnel_cos", "fresnel_cos_docstring", 0
+    );
+
+    fresnel_psi = PyUFunc_FromFuncAndData(
+        psi_funcs, PyuFunc_data, octo_double_types, 1, 7, 1,
+        PyUFunc_None, "fresnel_psi",  "fresnel_psi_docstring", 0
+    );
+
+    fresnel_sin = PyUFunc_FromFuncAndData(
+        fresnel_sin_funcs, PyuFunc_data, double_double_types, 1, 1, 1,
+        PyUFunc_None, "fresnel_sin", "fresnel_sin_docstring", 0
+    );
+
+    square_well_diffraction = PyUFunc_FromFuncAndData(
+        sqwellsol_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "square_well_diffraction", 
+        "square_well_diffraction_docstring", 0
+    );
+
+    square_well_phase = PyUFunc_FromFuncAndData(
+        sqwellphase_funcs, PyuFunc_data, four_real_in_one_real_out,
+        3, 4, 1, PyUFunc_None, "square_well_phase", 
+        "square_well_phase_docstring", 0
+    );
+
+    wavelength_to_wavenumber = PyUFunc_FromFuncAndData(
+        wavelength_to_wavenumber_funcs, PyuFunc_None_3, real_in_real_out,
+        3, 1, 1, PyUFunc_None, "wavelength_to_wavenumber",
+        "wavelength_to_wavenumber_docstring", 0
+    );
+
     d = PyModule_GetDict(m);
 
+    PyDict_SetItemString(d, "inverse_square_well_diffraction",
+                         inverse_square_well_diffraction);
+    PyDict_SetItemString(d, "frequency_to_wavelength", frequency_to_wavelength);
     PyDict_SetItemString(d, "fresnel_sin", fresnel_sin);
     PyDict_SetItemString(d, "fresnel_cos", fresnel_cos);
     PyDict_SetItemString(d, "fresnel_psi", fresnel_psi);
     PyDict_SetItemString(d, "fresnel_dpsi_dphi", fresnel_dpsi_dphi);
+    PyDict_SetItemString(d, "square_well_diffraction", square_well_diffraction);
+    PyDict_SetItemString(d, "square_well_phase", square_well_phase);
+    PyDict_SetItemString(d, "wavelength_to_wavenumber",
+                         wavelength_to_wavenumber);
 
+    Py_DECREF(inverse_square_well_diffraction);
+    Py_DECREF(frequency_to_wavelength);
     Py_DECREF(fresnel_sin);
     Py_DECREF(fresnel_cos);
     Py_DECREF(fresnel_psi);
     Py_DECREF(fresnel_dpsi_dphi);
+    Py_DECREF(square_well_diffraction);
+    Py_DECREF(square_well_phase);
+    Py_DECREF(wavelength_to_wavenumber);
 
     return m;
 }

29496443172f85569be58269243e3f68a0da13a2
ryanmaguire <rmaguire@protonmail.com>
2019-08-27 18:22:04 -0400

    Updating #include statements.

--- rss_ringoccs/diffrec/src/_window_functions.c
+++ rss_ringoccs/diffrec/src/_window_functions.c
@@ -8,7 +8,9 @@
 #include <complex.h>
 
 /*  Window functions defined here.                                           */
-#include "__window_functions.h"
+#include "__misc_windows.h"
+#include "__kaiser_bessel.h"
+#include "__modified_kaiser_bessel.h"
 
 /*  Various header files required for the C-Python API to work.              */
 #include <Python.h>

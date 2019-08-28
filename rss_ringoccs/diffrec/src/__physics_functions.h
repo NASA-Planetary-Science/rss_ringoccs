2c754d9c5a5f2bd1a7cae6d2afe20a56227c5a37
ryanmaguire <rmaguire@protonmail.com>
2019-08-27 20:15:03 -0400

    C routines for computing the Fresnel Scale.

--- rss_ringoccs/diffrec/src/__physics_functions.h
+++ rss_ringoccs/diffrec/src/__physics_functions.h
@@ -1,6 +1,7 @@
 #ifndef RSS_RINGOCCS_PHYSICS_FUNCTIONS_H
 #define RSS_RINGOCCS_PHYSICS_FUNCTIONS_H
 
+#include <math.h>
 #include "__math_constants.h"
 
 /*  Wavelength to Wavenumber function.                                        */
@@ -30,5 +31,47 @@ double Frequency_To_Wavelength_Double(double frequency){
 long double Frequency_To_Wavelength_Long_Double(long double frequency){
     return SPEED_OF_LIGHT_KM / frequency;
 }
+
+/*  The Fresnel Scale.                                                        */
+
+float Fresnel_Scale_Float(float lambda, float d, float phi, float b){
+    float cb_2_sp_2, sb_2;
+
+    cb_2_sp_2  = cosf(b)*sinf(phi);
+    cb_2_sp_2 *= cb_2_sp_2;
+
+    sb_2  = sinf(b);
+    sb_2 *= sb_2;
+
+    return sqrt(0.5 * lambda * d * (1.0 - cb_2_sp_2) / sb_2);
+}
+
+double Fresnel_Scale_Double(double lambda, double d, double phi, double b){
+    double cb_2_sp_2, sb_2;
+
+    cb_2_sp_2  = cos(b)*sin(phi);
+    cb_2_sp_2 *= cb_2_sp_2;
+
+    sb_2  = sin(b);
+    sb_2 *= sb_2;
+
+    return sqrt(0.5 * lambda * d * (1.0 - cb_2_sp_2) / sb_2);
+}
+
+
+long double Fresnel_Scale_Long_Double(long double lambda, long double d,
+                                      long double phi, long double b){
+    long double cb_2_sp_2, sb_2;
+
+    cb_2_sp_2  = cosl(b)*sinl(phi);
+    cb_2_sp_2 *= cb_2_sp_2;
+
+    sb_2  = sinl(b);
+    sb_2 *= sb_2;
+
+    return sqrt(0.5 * lambda * d * (1.0 - cb_2_sp_2) / sb_2);
+}
+
+
 
 #endif

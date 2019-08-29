32ba88c315e0f6209676421e85495a50416414c0
Unknown <ryan_maguire@student.uml.edu>
2019-08-29 10:16:34 -0400

    And short support and type conversions.

--- rss_ringoccs/diffrec/src/__normalized_equivalent_width.h
+++ rss_ringoccs/diffrec/src/__normalized_equivalent_width.h
@@ -52,6 +52,40 @@ long double Normeq_Long_Double(long double *w_func, long n_elements){
     return n_elements * sq_tot / tot_sq;
 }
 
+double Normeq_Short(short *w_func, long n_elements){
+    double tot_sq = 0.0;
+    double sq_tot = 0.0;
+    long i;
+
+    for (i = 0; i < n_elements; i++) {
+
+        /* Compute the sum and the sum of the squares.                        */
+        tot_sq += (double)w_func[i];
+        sq_tot += (double)w_func[i] * (double)w_func[i];
+    }
+
+    /*  Square the sum.                                                       */
+    tot_sq *= tot_sq;
+    return n_elements * sq_tot / tot_sq;
+}
+
+double Normeq_Int(int *w_func, long n_elements){
+    double tot_sq = 0.0;
+    double sq_tot = 0.0;
+    long i;
+
+    for (i = 0; i < n_elements; i++) {
+
+        /* Compute the sum and the sum of the squares.                        */
+        tot_sq += (double)w_func[i];
+        sq_tot += (double)w_func[i] * (double)w_func[i];
+    }
+
+    /*  Square the sum.                                                       */
+    tot_sq *= tot_sq;
+    return n_elements * sq_tot / tot_sq;
+}
+
 double Normeq_Long(long *w_func, long n_elements){
     double tot_sq = 0.0;
     double sq_tot = 0.0;
@@ -60,8 +94,25 @@ double Normeq_Long(long *w_func, long n_elements){
     for (i = 0; i < n_elements; i++) {
 
         /* Compute the sum and the sum of the squares.                        */
-        tot_sq += w_func[i];
-        sq_tot += w_func[i]*w_func[i];
+        tot_sq += (double)w_func[i];
+        sq_tot += (double)w_func[i] * (double)w_func[i];
+    }
+
+    /*  Square the sum.                                                       */
+    tot_sq *= tot_sq;
+    return n_elements * sq_tot / tot_sq;
+}
+
+double Normeq_Long_Long(long long *w_func, long n_elements){
+    double tot_sq = 0.0;
+    double sq_tot = 0.0;
+    long i;
+
+    for (i = 0; i < n_elements; i++) {
+
+        /* Compute the sum and the sum of the squares.                        */
+        tot_sq += (double)w_func[i];
+        sq_tot += (double)w_func[i] * (double)w_func[i];
     }
 
     /*  Square the sum.                                                       */

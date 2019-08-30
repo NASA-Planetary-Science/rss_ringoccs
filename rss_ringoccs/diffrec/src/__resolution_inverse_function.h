3d20f4c2dba8cd609a99a8c26165034625d8c68f
ryanmaguire <rmaguire@protonmail.com>
2019-08-29 22:01:39 -0400

    Add inf and nan handling to LambertW and resolution functions.

--- rss_ringoccs/diffrec/src/__resolution_inverse_function.h
+++ rss_ringoccs/diffrec/src/__resolution_inverse_function.h
@@ -11,10 +11,13 @@ double Resolution_Inverse_Float(float x){
     if (x <= 1.0){
         return nanf("0");
     }
-    else {
+    else if (x < INFINITY) {
         P1 = x/(1.0-x);
         P2 = P1*expf(P1);
         return LambertW_Float(P2)-P1;
+    }
+    else {
+        return 0.0;
     }
 }
 
@@ -23,10 +26,13 @@ double Resolution_Inverse_Double(double x){
     if (x <= 1.0){
         return nan("0");
     }
-    else {
+    else if (x < INFINITY) {
         P1 = x/(1.0-x);
         P2 = P1*exp(P1);
         return LambertW_Double(P2)-P1;
+    }
+    else {
+        return 0.0;
     }
 }
 
@@ -35,10 +41,13 @@ long double Resolution_Inverse_Long_Double(long double x){
     if (x <= 1.0){
         return nanl("0");
     }
-    else {
+    else if (x < INFINITY) {
         P1 = x/(1.0-x);
         P2 = P1*expl(P1);
         return LambertW_Long_Double(P2)-P1;
+    }
+    else {
+        return 0.0;
     }
 }
 

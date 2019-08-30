b7c5192773b20ba4ba261a69b5c241197432868b
ryanmaguire <rmaguire@protonmail.com>
2019-08-29 21:50:52 -0400

    Added right_straightedge and left_straightedge.

--- rss_ringoccs/diffrec/src/__fresnel_diffraction.h
+++ rss_ringoccs/diffrec/src/__fresnel_diffraction.h
@@ -356,10 +356,46 @@ complex long double Right_Straightedge_Diffraction_Long_Double(
     return 0.5-SQRT_2_BY_PI*T_hat;
 }
 
-    //  center = np.max((self.rho_km_vals <= rho).nonzero())
-    //  x = window_functions.SQRT_PI_2*(rho-self.rho_km_vals)/F[center]
-    //  T_hat = (special_functions.fresnel_cos(x)+
-    //           special_functions.fresnel_sin(x)*1j)*(0.5-0.5j)
-    //  T_hat = T_hat/window_functions.SQRT_PI_2 + 0.5
+/*---------Left Straight-Edge Diffraction Using Fresnel Approximation---------*/
 
+complex float Left_Straightedge_Diffraction_Float(float x, float edge, float F){
+    complex float T_hat;
+    float re, im;
+
+    x = SQRT_PI_BY_2*(edge-x)/F;
+
+    im = Fresnel_Sine_Taylor_to_Asymptotic_Float(x);
+    re = Fresnel_Cosine_Taylor_to_Asymptotic_Float(x);
+
+    T_hat = (0.5 - 0.5*_Complex_I)*(re+_Complex_I*im);
+    return 0.5 + SQRT_2_BY_PI*T_hat;
+}
+
+complex double Left_Straightedge_Diffraction_Double(double x, double edge,
+                                                    double F){
+    complex double T_hat;
+    double re, im;
+
+    x = SQRT_PI_BY_2*(edge-x)/F;
+
+    im = Fresnel_Sine_Taylor_to_Asymptotic_Double(x);
+    re = Fresnel_Cosine_Taylor_to_Asymptotic_Double(x);
+
+    T_hat = (0.5 - 0.5*_Complex_I)*(re+_Complex_I*im);
+    return 0.5 + SQRT_2_BY_PI*T_hat;
+}
+
+complex long double Left_Straightedge_Diffraction_Long_Double(
+    long double x, long double edge, long double F){
+    complex long double T_hat;
+    long double re, im;
+
+    x = SQRT_PI_BY_2*(edge-x)/F;
+
+    im = Fresnel_Sine_Taylor_to_Asymptotic_Long_Double(x);
+    re = Fresnel_Cosine_Taylor_to_Asymptotic_Long_Double(x);
+
+    T_hat = (0.5 - 0.5*_Complex_I)*(re+_Complex_I*im);
+    return 0.5 + SQRT_2_BY_PI*T_hat;
+}
 #endif

c0612f593fb8e9dec5dcd98cf5431fd534a71d27
ryanmaguire <rmaguire@protonmail.com>
2019-08-31 21:54:10 -0400

    Functions for fraunhofer diffraction

---
+++ rss_ringoccs/diffrec/src/__fraunhofer_diffraction_single_slit.c
@@ -0,0 +1,18 @@
+#include "__fraunhofer_diffraction.h"
+
+float Single_Slit_Fraunhofer_Diffraction_Float(float x, float z, float a){
+    float result = Sinc_Float(a*x/z);
+    return result*result;
+}
+
+double Single_Slit_Fraunhofer_Diffraction_Double(double x, double z, double a){
+    double result = Sinc_Double(a*x/z);
+    return result*result;
+}
+
+long double Single_Slit_Fraunhofer_Diffraction_Long_Double(long double x,
+                                                           long double z,
+                                                           long double a){
+    long double result = Sinc_Long_Double(a*x/z);
+    return result*result;
+}

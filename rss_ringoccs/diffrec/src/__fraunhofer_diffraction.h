62cf3e76fa5235e401173c218ed68df220987272
Unknown <ryan_maguire@student.uml.edu>
2019-08-31 21:54:10 -0400

    Functions for fraunhofer diffraction

--- rss_ringoccs/diffrec/src/__fraunhofer_diffraction.h
+++ rss_ringoccs/diffrec/src/__fraunhofer_diffraction.h
@@ -5,71 +5,32 @@
 #include "__sinc.h"
 #include "__math_constants.h"
 
-float Single_Slit_Fraunhofer_Diffraction_Float(float x, float z, float a){
-    float result = Sinc_Float(a*x/z);
-    return result*result;
-}
+/******************************************************************************
+ *--------------------Single Slit Fraunhofer Diffraction----------------------*
+ ******************************************************************************/
 
-double Single_Slit_Fraunhofer_Diffraction_Double(double x, double z, double a){
-    double result = Sinc_Double(a*x/z);
-    return result*result;
-}
+extern float Single_Slit_Fraunhofer_Diffraction_Float(float x, float z,
+                                                      float a);
 
-long double Single_Slit_Fraunhofer_Diffraction_Long_Double(long double x,
-                                                           long double z,
-                                                           long double a){
-    long double result = Sinc_Long_Double(a*x/z);
-    return result*result;
-}
+extern double Single_Slit_Fraunhofer_Diffraction_Double(double x, double z,
+                                                        double a);
 
-float Double_Slit_Fraunhofer_Diffraction_Float(float x, float z,
-                                               float a, float d){
-    float var_1, var_2, var_3;
+extern long double Single_Slit_Fraunhofer_Diffraction_Long_Double(
+    long double x, long double z, long double a
+);
 
-    var_1  = Sinc_Float(a*x/z);
-    var_1 *= var_1;
+/******************************************************************************
+ *--------------------Double Slit Fraunhofer Diffraction----------------------*
+ ******************************************************************************/
 
-    var_2  = sinf(TWO_PI*d*x/z);
-    var_2 *= var_2;
+extern float Double_Slit_Fraunhofer_Diffraction_Float(float x, float z,
+                                                      float a, float d);
 
-    var_3  = 2.0*sinf(ONE_PI*d*x/z);
-    var_3 *= var_3;
+extern double Double_Slit_Fraunhofer_Diffraction_Double(double x, double z,
+                                                        double a, double d);
 
-    return var_1*var_2/var_3;
-}
-
-double Double_Slit_Fraunhofer_Diffraction_Double(double x, double z,
-                                                 double a, double d){
-    double var_1, var_2, var_3;
-
-    var_1  = Sinc_Double(a*x/z);
-    var_1 *= var_1;
-
-    var_2  = sin(TWO_PI*d*x/z);
-    var_2 *= var_2;
-
-    var_3  = 2.0*sin(ONE_PI*d*x/z);
-    var_3 *= var_3;
-
-    return var_1*var_2/var_3;
-}
-
-long double Double_Slit_Fraunhofer_Diffraction_Long_Double(long double x,
-                                                           long double z,
-                                                           long double a,
-                                                           long double d){
-    long double var_1, var_2, var_3;
-
-    var_1  = Sinc_Long_Double(a*x/z);
-    var_1 *= var_1;
-
-    var_2  = sinl(TWO_PI*d*x/z);
-    var_2 *= var_2;
-
-    var_3  = 2.0*sinl(ONE_PI*d*x/z);
-    var_3 *= var_3;
-
-    return var_1*var_2/var_3;
-}
+extern long double Double_Slit_Fraunhofer_Diffraction_Long_Double(
+    long double x, long double z, long double a, long double d
+);
 
 #endif

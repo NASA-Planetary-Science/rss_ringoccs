e1d3a7db5766938bdd800f510f5f87cd3a86af27
ryanmaguire <rmaguire@protonmail.com>
2019-09-01 14:33:17 -0400

    Updated wrappers.

--- rss_ringoccs/diffrec/src/_math_function_wrappers.h
+++ rss_ringoccs/diffrec/src/_math_function_wrappers.h
@@ -41,3 +41,42 @@ static void long_double_besselJ0(char **args, npy_intp *dimensions,
         out[i] = BesselJ0_Long_Double(in[i]);
     }
 }
+
+static void float_besselI0(char **args, npy_intp *dimensions,
+                           npy_intp *steps, void *data){
+    npy_intp i;
+    npy_intp n = dimensions[0];
+
+    float *in  = (float *)args[0];
+    float *out = (float *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = BesselI0_Float(in[i]);
+    }
+}
+
+static void double_besselI0(char **args, npy_intp *dimensions,
+                            npy_intp *steps, void *data){
+    npy_intp i;
+    npy_intp n = dimensions[0];
+
+    double *in  = (double *)args[0];
+    double *out = (double *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = BesselI0_Double(in[i]);
+    }
+}
+
+static void long_double_besselI0(char **args, npy_intp *dimensions,
+                                 npy_intp *steps, void *data){
+    npy_intp i;
+    npy_intp n = dimensions[0];
+
+    long double *in  = (long double *)args[0];
+    long double *out = (long double *)args[1];
+
+    for (i = 0; i < n; i++) {
+        out[i] = BesselI0_Long_Double(in[i]);
+    }
+}

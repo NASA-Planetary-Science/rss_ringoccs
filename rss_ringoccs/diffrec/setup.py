c2faff4366395f8071ea695161a074868414f94b
Unknown <ryan_maguire@student.uml.edu>
2019-09-15 20:44:16 -0400

    Updated setup scripts.

--- rss_ringoccs/diffrec/setup.py
+++ rss_ringoccs/diffrec/setup.py
@@ -8,6 +8,8 @@ def configuration(parent_package=None, top_path=None):
                            top_path=top_path)
     config.add_extension(
         '_special_functions', [
+            "src/__bessel_J0.c",
+            "src/__bessel_I0.c",
             "src/__fraunhofer_diffraction_double_slit.c",
             "src/__fraunhofer_diffraction_single_slit.c",
             "src/__fresnel_diffraction_inverted_square_well.c",
@@ -16,11 +18,14 @@ def configuration(parent_package=None, top_path=None):
             "src/__fresnel_diffraction_square_well_phase.c",
             "src/__fresnel_diffraction_square_well.c",
             "src/__fresnel_integral_cosine.c",
+            "src/__fresnel_integral_cosine_heald.c",
+            "src/__fresnel_integral_cosine_while.c",
             "src/__fresnel_integral_sine.c",
+            "src/__fresnel_integral_sine_heald.c",
+            "src/__fresnel_integral_sine_while.c",
             "src/__fresnel_integral.c",
             "src/__fresnel_kernel.c",
             "src/__get_array.c",
-            "src/__math_function_bessel.c",
             "src/__math_function_lambertw.c",
             "src/__math_function_legendre.c",
             "src/__math_function_max.c",
@@ -29,12 +34,13 @@ def configuration(parent_package=None, top_path=None):
             "src/__math_function_resolution_inverse.c",
             "src/__math_function_sinc.c",
             "src/_special_functions.c",
-            "src/__window_width.c"
+            "src/__where_greater.c",
+            "src/__where_lesser.c"
         ]
     )
     config.add_extension('_window_functions', [
             "src/_window_functions.c",
-            "src/__math_function_bessel.c",
+            "src/__bessel_I0.c",
             "src/__window_function_squared_cosine.c",
             "src/__window_function_kaiser_bessel.c",
             "src/__window_function_normalization.c",
@@ -46,8 +52,12 @@ def configuration(parent_package=None, top_path=None):
             "src/_diffraction_functions.c",
             "src/__diffraction_functions.c",
             "src/__fresnel_kernel.c",
-            "src/__math_function_bessel.c",
+            "src/__bessel_I0.c",
             "src/__math_function_legendre.c",
+            "src/__fresnel_transform.c",
+            "src/__fresnel_transform_ellipse.c",
+            "src/__fresnel_transform_legendre.c",
+            "src/__fresnel_transform_newton.c",
             "src/__window_function_squared_cosine.c",
             "src/__window_function_kaiser_bessel.c",
             "src/__window_function_modified_kaiser_bessel.c",

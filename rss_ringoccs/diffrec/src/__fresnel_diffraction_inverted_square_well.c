30cac56e86ca32776ac6453b612341829ff08b96
ryanmaguire <rmaguire@protonmail.com>
2019-10-26 11:59:13 -0400

    Updates to fresnel diffraction code.

--- rss_ringoccs/diffrec/src/__fresnel_diffraction_inverted_square_well.c
+++ rss_ringoccs/diffrec/src/__fresnel_diffraction_inverted_square_well.c
@@ -32,23 +32,6 @@ complex float Inverted_Square_Well_Diffraction_Float(float x, float a,
     return (0.5 - 0.5*_Complex_I)*result;
 }
 
-/******************************************************************************
- *  Function:                                                                 *
- *      Inverted_Square_Well_Diffraction_Double                               *
- *  Purpose:                                                                  *
- *      Acts as Inverted_Square_Well_Diffraction_Solution_Float for doubles.  *
- *  Arguments:                                                                *
- *      x (double):                                                           *
- *          The location on the x-axis for the point being computed.          *
- *      a (double):                                                           *
- *          The left-most endpoint of the square well.                        *
- *      b (double):                                                           *
- *          The right-most endpoint of the square well.                       *
- *      F (double):                                                           *
- *          The Fresnel scale.                                                *
- *  Notes:                                                                    *
- *      1.) This function relies on the C99 standard, or higher.              *
- ******************************************************************************/
 complex double Inverted_Square_Well_Diffraction_Double(double x, double a,
                                                        double b, double F)
 {
@@ -61,26 +44,10 @@ complex double Inverted_Square_Well_Diffraction_Double(double x, double a,
     return (0.5 - 0.5*_Complex_I)*result;
 }
 
-/******************************************************************************
- *  Function:                                                                 *
- *      Inverted_Square_Well_Diffraction_Long_Double                          *
- *  Purpose:                                                                  *
- *      Inverted_Square_Well_Diffraction_Solution_Float for long doubles.     *
- *  Arguments:                                                                *
- *      x (double):                                                           *
- *          The location on the x-axis for the point being computed.          *
- *      a (double):                                                           *
- *          The left-most endpoint of the square well.                        *
- *      b (double):                                                           *
- *          The right-most endpoint of the square well.                       *
- *      F (double):                                                           *
- *          The Fresnel scale.                                                *
- *  Notes:                                                                    *
- *      1.) This function relies on the C99 standard, or higher.              *
- ******************************************************************************/
-complex long double Inverted_Square_Well_Diffraction_Long_Double(
-    long double x, long double a, long double b, long double F
-){
+complex long double
+Inverted_Square_Well_Diffraction_Long_Double(long double x, long double a,
+                                             long double b, long double F)
+{
     long double arg1 = SQRT_PI_BY_2*(a-x)/F;
     long double arg2 = SQRT_PI_BY_2*(b-x)/F;
     complex long double result;

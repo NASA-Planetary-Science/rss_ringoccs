09d68036748f49a50b59768265bb819bbdd15f28
Ryan Maguire <ryan_maguire@student.uml.edu>
2020-11-12 10:06:32 -0500

    Reorganizing some of the complex functions.

--- rss_ringoccs/src/complex/rss_ringoccs_complex_polynomial.c
+++ rss_ringoccs/src/complex/rss_ringoccs_complex_polynomial.c
@@ -1,9 +1,56 @@
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify it   *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************
+ *                     rss_ringoccs_complex_polynomial                        *
+ ******************************************************************************
+ *  Purpose:                                                                  *
+ *      Efficiently compute polynomials of a complex variable using Horner's  *
+ *      method. Two functions are provided allowing for real coefficients and *
+ *      complex coefficients.                                                 *
+ ******************************************************************************
+ *                               DEPENDENCIES                                 *
+ *  1.) stdio.h:                                                              *
+ *          The standard C library for input and output. This is needed for   *
+ *          the "puts" function for printing error messages should the        *
+ *          program fail.                                                     *
+ *  2.) stdlib:                                                               *
+ *          The C standard library header file. This contains the "exit"      *
+ *          function which is used to terminate a program if an error occurs. *
+ *  2.) rss_ringoccs_complex.h:                                               *
+ *          Header file where rssringoccs_ComplexDouble is defined, as well   *
+ *          as the prototypes for the functions.                              *
+ ******************************************************************************
+ *                                 WARNINGS                                   *
+ *  1.) These functions use "exit" to abort a computation if an error occurs. *
+ *      This is to prevent the possibility of a segmentation fault, which     *
+ *      often prints out a less-than-helpful error message. Improper use of   *
+ *      this function will cause your program to crash (However, the error    *
+ *      message will tell you why).                                           *
+ ******************************************************************************
+ *  Author:     Ryan Maguire, Wellesley College                               *
+ *  Date:       November 12, 2020                                             *
+ ******************************************************************************/
+
 /*  Needed for the 'puts' function.                                           */
 #include <stdio.h>
 
 /*  And the 'exit' function is located here.                                  */
 #include <stdlib.h>
-
 
 /*  Where the prototypes are declared and where complex types are defined.    */
 #include "rss_ringoccs_complex.h"
@@ -24,7 +71,7 @@ rssringoccs_Complex_Poly_Real_Coeffs(double *coeffs, unsigned int degree,
     if (coeffs == NULL)
     {
         puts("Error Encountered: rss_ringoccs\n"
-             "\tFunction: rssringoccs_ComplexPolyRealCoeffs\n\n"
+             "\tFunction: rssringoccs_Complex_Poly_Real_Coeffs\n\n"
              "The input coefficients pointer is NULL. Trying to access this\n"
              "will result in a segmentation fault. Aborting computation.\n\n");
         exit(0);
@@ -43,7 +90,7 @@ rssringoccs_Complex_Poly_Real_Coeffs(double *coeffs, unsigned int degree,
      *  be a complex number so we can add it to the complex z value.          */
     next_coeff = rssringoccs_Complex_Rect(coeffs[degree-1], 0.0);
 
-    /*  Set poly equal to a_{N}*z, whre N is the degree of the polynomial.    */
+    /*  Set poly equal to a_{N}*z, where N is the degree of the polynomial.   */
     poly = rssringoccs_Complex_Scale(coeffs[degree], z);
 
     /*  Reset poly to a_{N}*z + a_{N-1}.                                      */
@@ -58,6 +105,51 @@ rssringoccs_Complex_Poly_Real_Coeffs(double *coeffs, unsigned int degree,
         /*  Use Horner's method with the current complex coefficients.        */
         poly = rssringoccs_Complex_Multiply(z, poly);
         poly = rssringoccs_Complex_Add(poly, next_coeff);
+    }
+
+    return poly;
+}
+
+/*  Use Horner's method to compute a polynomial of a complex variable z with  *
+ *  complex coefficients.                                                     */
+rssringoccs_ComplexDouble
+rssringoccs_Complex_Poly_Complex_Coeffs(rssringoccs_ComplexDouble *coeffs,
+                                        unsigned int degree,
+                                        rssringoccs_ComplexDouble z)
+{
+    /*  Declare necessary variables. C89 requires declarations at the top.    */
+    rssringoccs_ComplexDouble poly;
+    unsigned int n;
+
+    /*  If the input coefficient pointer is NULL, trying to access it will    *
+     *  result in a segmentation fault. Check this and abort the computation  *
+     *  if it's NULL.                                                         */
+    if (coeffs == NULL)
+    {
+        puts("Error Encountered: rss_ringoccs\n"
+             "\tFunction: rssringoccs_Complex_Poly_Complex_Coeffs\n\n"
+             "The input coefficients pointer is NULL. Trying to access this\n"
+             "will result in a segmentation fault. Aborting computation.\n\n");
+        exit(0);
+    }
+
+    /*  Degree should be at least one, otherwise this is not a polynomial but *
+     *  a constant. Check this. If degree is zero, we'll just return the      *
+     *  zeroth coefficient (a constant polynomial).                           */
+    if (degree == 0)
+        return coeffs[0];
+
+    /*  Set poly equal to a_{N}*z, where N is the degree of the polynomial.   */
+    poly = rssringoccs_Complex_Multiply(coeffs[degree], z);
+
+    /*  Reset poly to a_{N}*z + a_{N-1}.                                      */
+    poly = rssringoccs_Complex_Add(poly, coeffs[degree-1]);
+
+    /*  Use Horner's method of polynomial computation.                        */
+    for (n=2; n<=degree; ++n)
+    {
+        poly = rssringoccs_Complex_Multiply(z, poly);
+        poly = rssringoccs_Complex_Add(poly, coeffs[degree-n]);
     }
 
     return poly;

011a7718a8df8acf1df4f808eb04be21a1bd49cb
ryanmaguire <rmaguire@protonmail.com>
2020-11-17 13:41:50 -0500

    Adding examples and updating some code.

---
+++ rss_ringoccs/src/numerical/rss_ringoccs_newton_raphson.c
@@ -0,0 +1,121 @@
+#include <rss_ringoccs/include/rss_ringoccs_bool.h>
+#include <rss_ringoccs/include/rss_ringoccs_math.h>
+#include <rss_ringoccs/include/rss_ringoccs_numerical.h>
+
+#define __do_newton_raphson(type, Type)                                        \
+type rssringoccs_Newton_Raphson_##Type(type x, type (*f)(type),                \
+                                       type (*f_prime)(type))                  \
+{                                                                              \
+    /*  Declare necessary variables. C89 requires this at the top.           */\
+    type dx, y, y_prime;                                                       \
+    unsigned int n, max_iter;                                                  \
+                                                                               \
+    /*  Set the max iteration to 20, which should be more than enough for    */\
+    /*  any reasonable setting. The number of decimals of precision doubles  */\
+    /*  with every iteration of Newton-Raphson, so if convergence doesn't    */\
+    /*  happen after a few iterations it probably won't converge.            */\
+    max_iter = 20;                                                             \
+                                                                               \
+    /*  Evaluate the perturbation term, then compute the next iteration.     */\
+    y = (*f)(x);                                                               \
+    y_prime = (*f_prime)(x);                                                   \
+                                                                               \
+    /*  If the derivative is zero at your initial guess, Newton-Raphson      */\
+    /*  fails. Return Not-a-Number in this case.                             */\
+    if (y_prime == 0.0)                                                        \
+        return rssringoccs_NaN;                                                \
+                                                                               \
+    /*  Compute the first iteration of Newton-Raphson.                       */\
+    dx = y/y_prime;                                                            \
+    x -= dx;                                                                   \
+                                                                               \
+    /*  The first iteration has been computed above, so set n to 1.          */\
+    n = 1;                                                                     \
+                                                                               \
+    /*  Continuing this computation until the error is below the threshold.  */\
+    while(rssringoccs_Abs_##Type(dx) > EPS)                                    \
+    {                                                                          \
+        y = (*f)(x);                                                           \
+        y_prime = (*f_prime)(x);                                               \
+                                                                               \
+        if (y_prime == 0.0)                                                    \
+            return rssringoccs_NaN;                                            \
+                                                                               \
+        dx = y/y_prime;                                                        \
+        x -= dx;                                                               \
+        ++n;                                                                   \
+                                                                               \
+        /*  Break if too many iterations have been run.                      */\
+        if (n > max_iter)                                                      \
+            break;                                                             \
+    }                                                                          \
+                                                                               \
+    return x;                                                                  \
+}
+
+/*  For floating type, set the error to 10^-8.                                */
+#define EPS 1.0e-8
+
+__do_newton_raphson(float, Float)
+
+/*  Reset the error to 10^-16 for double and long double.                     */
+#undef EPS
+#define EPS 1.0e-16
+
+__do_newton_raphson(double, Double)
+__do_newton_raphson(long double, LongDouble)
+
+
+rssringoccs_ComplexDouble
+rssringoccs_Newton_Raphson_Complex(
+    rssringoccs_ComplexDouble z,
+    rssringoccs_ComplexDouble (*f)(rssringoccs_ComplexDouble),
+    rssringoccs_ComplexDouble (*f_prime)(rssringoccs_ComplexDouble)
+)
+{
+    /*  Declare necessary variables. C89 requires this at the top.            */
+    rssringoccs_ComplexDouble dz, w, w_prime;
+    unsigned int n, max_iter;
+
+    /*  Set the max iteration to 20, which should be more than enough for     *
+     *  any reasonable setting. The number of decimals of precision doubles   *
+     *  with every iteration of Newton-Raphson, so if convergence doesn't     *
+     *  happen after a few iterations it probably won't converge.             */
+    max_iter = 20;
+
+    /*  Evaluate the perturbation term, then compute the next iteration.      */
+    w = (*f)(z);
+    w_prime = (*f_prime)(z);
+
+    /*  If the derivative is zero at your initial guess, Newton-Raphson       *
+     *  fails. Return Not-a-Number in this case.                              */
+    if (rssringoccs_Complex_Compare(w_prime, rssringoccs_Complex_Zero))
+        return rssringoccs_Complex_NaN;
+
+    /*  Compute the first iteration of Newton-Raphson.                        */
+    dz = rssringoccs_Complex_Divide(w, w_prime);
+    z  = rssringoccs_Complex_Subtract(z, dz);
+
+    /*  The first iteration has been computed above, so set n to 1.           */
+    n = 1;
+
+    /*  Continuing this computation until the error is below the threshold.   */
+    while(rssringoccs_Complex_Abs(dz) > EPS)
+    {
+        w = (*f)(z);
+        w_prime = (*f_prime)(z);
+
+        if (rssringoccs_Complex_Compare(w_prime, rssringoccs_Complex_Zero))
+            return rssringoccs_Complex_NaN;
+
+        dz = rssringoccs_Complex_Divide(w, w_prime);
+        z  = rssringoccs_Complex_Subtract(z, dz);
+        ++n;
+
+        /*  Break if too many iterations have been run.                       */
+        if (n > max_iter)
+            break;
+    }
+
+    return z;
+}

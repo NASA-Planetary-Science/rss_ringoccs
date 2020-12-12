34747461067abdb1b38bbd0dfd3edd71289772d5
Ryan Maguire <ryan_maguire@student.uml.edu>
2020-12-11 20:21:03 -0500

    Adding more examples.

--- tests/math_tests/absf_time_test.c
+++ tests/math_tests/absf_time_test.c
@@ -18,7 +18,7 @@
  ******************************************************************************
  *  Purpose:                                                                  *
  *      Provide tests for the accuracy and efficiency of rss_ringoccs         *
- *      absolute value function at long double precision compared to the one  *
+ *      absolute value function at single precision compared to the one       *
  *      provided by the C99 standard in math.h.                               *
  ******************************************************************************
  *  Author:     Ryan Maguire, Wellesley College                               *
@@ -38,26 +38,34 @@
 /*  Needed for malloc.                                                        */
 #include <stdlib.h>
 
+/*  Routine for comparing fabsf with rssringoccs_Float_Abs.                   */
 int main(void)
 {
-    /*  Declare necessary variables.                                          */
-    long double max_err, temp, start, end, x, dx;
-    long double *y0, *y1;
-    unsigned int n, N, ind;
+    /*  Set the start and end for the values we're testing.                   */
+    float start = -100.0;
+    float end   =  100.0;
+
+    /*  Declare variables for sampling the region [start, end].               */
+    float x, dx;
+
+    /*  Declare variables for computing the maximum difference between fabsf  *
+     *  and rssringoccs_Float_Abs.                                            */
+    float max_err = 0.0;
+    float temp;
+
+    /*  Declare two pointers to represent arrays for fabsf(x) and             *
+     *  rssringoccs_Float_Abs(x), respectively.                               */
+    float *y0, *y1;
+
+    /*  Declare a dummy variable for indexing and a variable for the number   *
+     *  of points we're sampling in the range [start, end].                   */
+    unsigned int n;
+    unsigned int N = 1e8;
+
+    /*  Declare variables for computing computation time.                     */
     clock_t t1, t2;
 
-    /*  We'll do our time test with 100 million points.                       */
-    N = 1e8;
-
-    /*  We'll have the variable range from -100 to 100.                       */
-    start = 100.0L;
-    end = 100.0L;
-
-    /*  Set the initial values for ind and max_err to zero.                   */
-    ind = 0;
-    max_err = 0.0L;
-
-    /*  And we'll increment evenly throughout the region.                     */
+    /*  We'll increment evenly throughout the region.                         */
     dx = (end - start) / N;
 
     /*  Allocate memory for the two pointers we've declared.                  */
@@ -65,13 +73,13 @@ int main(void)
     y1 = malloc(sizeof(*y1) * N);
 
     /*  Set x to the starting value and grab the current time.                */
-    x = -start;
+    x = start;
     t1 = clock();
 
-    /*  Perform the calculation for the C99 standard library function fabsl.  */
+    /*  Perform the calculation for the C99 standard library function fabsf.  */
     for (n=0; n<N; ++n)
     {
-        y0[n] = fabsl(x);
+        y0[n] = fabsf(x);
         x += dx;
     }
 
@@ -79,20 +87,20 @@ int main(void)
     t2 = clock();
 
     /*  t2-t1 is the number of clock cycles that have passed between grabbing *
-     *  t1 and t2. To convert this to second, use the macro CLOCKS_PER_SEC    *
+     *  t1 and t2. To convert this to seconds, use the macro CLOCKS_PER_SEC   *
      *  provided in time.h.                                                   */
     printf("C99:          %f\n", (double)(t2-t1)/CLOCKS_PER_SEC);
 
     /*  Restart the computation for the rss_ringoccs function.                */
-    x = -start;
+    x = start;
 
     /*  Reset the clock.                                                      */
     t1 = clock();
 
-    /*  Perform the computation using rssringoccs_LDouble_Abs instead.        */
+    /*  Perform the computation using rssringoccs_Float_Abs instead of fabsf. */
     for (n=0; n<N; ++n)
     {
-        y1[n] = rssringoccs_LDouble_Abs(x);
+        y1[n] = rssringoccs_Float_Abs(x);
         x += dx;
     }
 
@@ -102,29 +110,33 @@ int main(void)
     /*  Print out how long it took for rss_ringoccs to compute.               */
     printf("rss_ringoccs: %f\n", (double)(t2-t1)/CLOCKS_PER_SEC);
 
+    /*  NOTE:                                                                 *
+     *      Without the following comparison of the two pointers y0 and y1,   *
+     *      some compilers may see the above computations as redundant with   *
+     *      optimization on, and skip them. The resulting times will be close *
+     *      to zero for both fabsf and rssringoccs_Float_Abs.                 */
+
     /*  Compute the maximum absolute error between rss_ringoccs and C99.      */
     for (n=0; n<N; ++n)
     {
         /*  We'll use the standard library function to check the error.       */
-        temp = fabsl(y0[n] - y1[n]);
+        temp = fabsf(y0[n] - y1[n]);
 
         /*  Check if the error got larger and set max_err accordingly.        */
         if (max_err < temp)
-        {
             max_err = temp;
-            ind = n;
-        }
     }
-    /*  End of for loop computing |y0-y1|.                                    */
+    /*  End of for-loop computing |y0-y1|.                                    */
 
-    /*  Print out the error to 24 decimals (assumes 96-bit precision).        */
-    printf("Max Error: %.24Lf\n", max_err);
+    /*  Print out the error to 8 decimals (assumes 32-bit precision).         */
+    printf("Max Error: %.8f\n", max_err);
 
     /*  Free the pointers we've malloc'd.                                     */
     free(y0);
     free(y1);
     return 0;
 }
+/*  End of main.                                                              */
 
 /*  This was compiled on an iMac 2017 3.4GHz quad-core running MacOS Catalina *
  *  10.15.7. It produces the following times:                                 *
@@ -134,14 +146,15 @@ int main(void)
  *      This is NOT the regular gcc from GNU. To use this on apple devices    *
  *      requires homebrew.                                                    *
  *                                                                            *
- *  -O3 optimization:                                                         *
- *      gcc -Wconversion -O3 -Wall -Wextra -Wpedantic                         *
- *          -pedantic absl_time_test.c -o test -lrssringoccs                  *
- *      C99:          0.667850                                                *
- *      rss_ringoccs: 1.083205                                                *
- *      Max Error: 0.000000000000000000000000                                 *
  *  No optimization:                                                          *
- *      gcc absl_time_test.c -o test -lrssringoccs                            *
- *      C99:          1.154483                                                *
- *      rss_ringoccs: 1.566479                                                *
- *      Max Error: 0.000000000000000000000000                                 */
+ *      gcc -Wall -Wextra -Wconversion -Wpedantic -pedantic -pedantic-errors  *
+ *          -std=c89 -ansi absf_time_test.c -o test -lrssringoccs             *
+ *      C99:          0.374127                                                *
+ *      rss_ringoccs: 0.405311                                                *
+ *      Max Error: 0.00000000                                                 *
+ *  -O3 optimization:                                                         *
+ *      gcc -Wall -Wextra -Wconversion -Wpedantic -pedantic -pedantic-errors  *
+ *          -O3 -std=c89 -ansi absf_time_test.c -o test -lrssringoccs         *
+ *      C99:          0.129045                                                *
+ *      rss_ringoccs: 0.290840                                                *
+ *      Max Error: 0.00000000                                                 */

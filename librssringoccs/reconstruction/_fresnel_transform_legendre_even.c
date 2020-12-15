941a2630738cba0f8d61f06321a3a600fda0c9fd
ryanmaguire <rmaguire@protonmail.com>
2020-12-15 15:49:23 -0500

    Updating reconstruction code.

--- librssringoccs/reconstruction/_fresnel_transform_legendre_even.c
+++ librssringoccs/reconstruction/_fresnel_transform_legendre_even.c
@@ -1,6 +1,25 @@
-#include <math.h>
-#include <complex.h>
-#include <rss_ringoccs_math_constants.h>
+/******************************************************************************
+ *                                 LICENSE                                    *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify it   *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************/
+
+#include <rss_ringoccs/include/rss_ringoccs_math.h>
+#include <rss_ringoccs/include/rss_ringoccs_complex.h>
+#include <rss_ringoccs/include/rss_ringoccs_reconstruction.h>
 
 /******************************************************************************
  *  Function:                                                                 *

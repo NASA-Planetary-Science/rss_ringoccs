bf6cb4ec2cb8175a6f389e9c0080874604b17ab8
Ryan Maguire <ryan_maguire@student.uml.edu>
2021-01-11 12:27:46 -0500

    Updating config script.

--- config_rss_ringoccs.sh
+++ config_rss_ringoccs.sh
@@ -7,10 +7,10 @@ osstring=`uname`
 
 if [ "$osstring" = "Darwin" ]; then
 	BASH_FILE=bash_profile
-	fil="Anaconda3-2019.07-MacOSX-x86_64.sh"
+	fil="Anaconda3-2020.02-MacOSX-x86_64.sh"
 elif [ "$osstring" = "Linux" ]; then
 	BASH_FILE=bashrc
-	fil="Anaconda3-2019.07-Linux-x86_64.sh"
+	fil="Anaconda3-2020.02-Linux-x86_64.sh"
 else
 	echo "Operating System not recognized"
 	echo "Only MacOSX and Linux supported"
@@ -20,8 +20,6 @@ fi
 
 echo "Running rss_ringoccs_config Script:"
 
-exit 1
-
 web="https://repo.continuum.io/archive/"
 webfil="$web$fil"
 
@@ -29,7 +27,7 @@ webfil="$web$fil"
 MY_DIRECTORY=$(pwd)
 cd ~
 # Check to see if anaconda3 exists on your computer.
-if [ -d anaconda3 ]; then
+if [ -d ~/opt/anaconda3 ]; then
 	echo -e ' \t ' "You already have Anaconda on your computer."
 	echo -e ' \t ' "Running updates..."
 else
@@ -52,11 +50,11 @@ source .$BASH_FILE
 
 # Check that anaconda3 is included in your path
 echo -e ' \t ' "Checking your PATH variable..."
-if [[ ":$PATH:" == *"/anaconda3/bin"* ]]; then
+if [[ ":$PATH:" == *"~/opt/anaconda3/bin"* ]]; then
   echo -e ' \t ' "PATH variable is set"
 else
-	echo -e ' \t ' "Adding ~/anaconda3/bin to your PATH"
-	echo 'export PATH="~/anaconda3/bin:$PATH"' >>~/.$BASH_FILE
+	echo -e ' \t ' "Adding ~/opt/anaconda3/bin to your PATH"
+	echo 'export PATH="~/opt/anaconda3/bin:$PATH"' >>~/.$BASH_FILE
 fi
 
 # Source .bashrc/.bash_profile to update you path.
@@ -70,6 +68,7 @@ yes | conda update conda
 
 # Install Spiceypy
 echo -e ' \t ' "Installing spiceypy..."
+conda config --add channels conda-forge
 yes | conda install -c conda-forge spiceypy
 
 # Install PyMieScatt
@@ -82,15 +81,15 @@ source .$BASH_FILE
 cd "$MY_DIRECTORY"
 
 # Install rss_ringoccs.
-cd ./rss_ringoccs/diffrec/
-
-# Remove any old compiled binary files.
-rm -f *.so
+echo -e ' \t ' "Building librssringoccs..."
+cd librssringoccs/
+chmod +x config_librssringoccs.sh
+./config_librssringoccs.sh
 
 # Compile the new binary files.
-python setup.py config --compiler=gnu99 build_ext --inplace
-rm -rf build/
-
-cd "$MY_DIRECTORY"
+echo -e ' \t ' "Building modules..."
+cd ../
+chmod +x config_modules.sh
+./config_modules.sh
 
 echo "Installation complete"

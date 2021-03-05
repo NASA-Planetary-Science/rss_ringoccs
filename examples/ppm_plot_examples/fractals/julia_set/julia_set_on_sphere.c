3e47d1ba32b6152c4e8ddb836b89ddf74b626822
ryanmaguire <rmaguire@protonmail.com>
2021-03-04 23:32:18 -0500

    Fixed typo.

--- examples/ppm_plot_examples/fractals/julia_set/julia_set_on_sphere.c
+++ examples/ppm_plot_examples/fractals/julia_set/julia_set_on_sphere.c
@@ -102,7 +102,7 @@ int main(void)
 
     /*  Normalize the camera vector and set this to u. First check that the   *
      *  user didn't provide the zero vector since this will cause an error.   */
-    norm = rssringoccs_Euclidean_Norm_3D(camera_pos);
+    norm = rssringoccs_ThreeVector_Euclidean_Norm(camera_pos);
 
     if (norm == 0.0)
     {
@@ -114,7 +114,7 @@ int main(void)
         exit(0);
     }
     else
-        u = rssringoccs_Normalize_ThreeVector(camera_pos);
+        u = rssringoccs_ThreeVector_Normalize(camera_pos);
 
     /* Open and name the file and give it write permission.                   */
     fp = fopen("julia_set_on_sphere.ppm", "w");

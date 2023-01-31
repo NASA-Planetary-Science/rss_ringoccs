17adc289c1e2b87fa12a48a5daf421b7e23e6af8
ryanmaguire <rmaguire@protonmail.com>
2023-01-30 21:44:56 -0500

    Adding back occgeo header.

---
+++ include/rss_ringoccs_occultation_geometry.h
@@ -0,0 +1,9 @@
+
+#ifndef RSS_RINGOCCS_OCCULTATION_GEOMETRY_H
+#define RSS_RINGOCCS_OCCULTATION_GEOMETRY_H
+
+#include <libtmpl/include/tmpl_vec3.h>
+
+extern double rssringoccs_Double_Light_Distance(double t0, double t1);
+
+#endif

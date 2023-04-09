0677aa82584c203f8c7035a9bafd8fa766d93498
Ty <ty.cumby@gmail.com>
2023-04-09 11:22:52 -0600

    Add a fetch for libtmpl

---
+++ cmake/FetchLibTmpl.cmake
@@ -0,0 +1,27 @@
+cmake_minimum_required(VERSION 3.20)
+include_guard()
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
+
+set(LIBTMPL_BUILD_EXAMPLES OFF)
+set(LIBTMPL_BUILD_TESTS OFF)
+set(LIBTMPL_USE_ASM ON)
+set(LIBTMPL_USE_MATH ON)
+if(WIN32)
+    set(LIBTMPL_USE_FASM OFF)
+    set(LIBTMPL_USE_OMP OFF)
+elseif(APPLE)
+    set(LIBTMPL_USE_FASM OFF)
+    set(LIBTMPL_USE_OMP ON)
+else()
+    set(LIBTMPL_USE_FASM ON)
+    set(LIBTMPL_USE_OMP ON)
+endif()
+
+fetchcontent_declare(
+    tmpl_checkout
+    GIT_REPOSITORY https://github.com/tcumby/libtmpl.git
+    GIT_TAG add-cmake-build-system-redo
+)
+
+fetchcontent_makeavailable(tmpl_checkout)

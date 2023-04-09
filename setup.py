ae3198681b40f5d0ce1ec1e572fbf3e66223a91d
Ty <ty.cumby@gmail.com>
2023-04-09 11:49:10 -0600

    Swtich build over to scikit-build

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-# Python is swapping distutils with setuptools.
-try:
-    from setuptools import setup, Extension
-except ImportError:
-    from distutils.core import setup, Extension
-
-import os, platform
-
-# Numpy is needed for the get_include function which tells us where various
-# header files, like numpy/arrayobject.h, live.
-import numpy
+from skbuild import setup  # type: ignore
+import sys
 
 # This seems to be needed to ensure Python uses the correct gcc. Without this
 # You may get a linker warning, for example:
@@ -19,42 +10,53 @@ import numpy
 # Python may use the wrong compiler, causing this error. Setting the following
 # CFLAG fixed the issue.
 
-#   We only need this fix for macOS, so check what operating system is used.
-if (platform.system() == "Darwin"):
-    os.environ["CFLAGS"] = "-mmacosx-version-min=%s" % platform.mac_ver()[0]
-
-srclist = []
-
-for file in os.listdir("rss_ringoccs/crssringoccs"):
-
-    # Only add .c files.
-    if (file[-1] == "c"):
-        srclist.append("rss_ringoccs/crssringoccs/%s" % file)
+# #   We only need this fix for macOS, so check what operating system is used.
+# if (platform.system() == "Darwin"):
+#     os.environ["CFLAGS"] = "-mmacosx-version-min=%s" % platform.mac_ver()[0]
+#
+# srclist = []
+#
+# for file in os.listdir("rss_ringoccs/crssringoccs"):
+#
+#     # Only add .c files.
+#     if (file[-1] == "c"):
+#         srclist.append("rss_ringoccs/crssringoccs/%s" % file)
 
 setup(
-    name = "rss_ringoccs",
-    version = "1.3",
-    description = "C Tools for rss_ringoccs",
-    author = "Ryan Maguire",
-    ext_modules=[
-        Extension(
-            "crssringoccs",
-            srclist,
-            include_dirs=[
-                numpy.get_include(),
-                "/usr/local/include"
-            ],
-            library_dirs=["/usr/local/lib"],
-            libraries=["tmpl", "rssringoccs"]
-        )
-    ],
+    name="rss_ringoccs",
+    version="1.3",
+    description="C Tools for rss_ringoccs",
+    author="Ryan Maguire",
     packages=[
         "rss_ringoccs",
         "rss_ringoccs.calibration",
         "rss_ringoccs.occgeo",
         "rss_ringoccs.rsr_reader",
         "rss_ringoccs.scatter",
-        "rss_ringoccs.tools"
-    ]
+        "rss_ringoccs.tools",
+    ],
+    url="https://github.com/NASA-Planetary-Science/rss_ringoccs",
+    project_urls={
+        "Documentation": "https://rss-ringoccs.readthedocs.io/en/master/",
+        "Bug Reports": "https://github.com/NASA-Planetary-Science/rss_ringoccs/issues",
+        "Source": "https://github.com/NASA-Planetary-Science/rss_ringoccs",
+    },
+    install_requires=[
+        "cmake",
+        "numpy",
+        "scipy",
+        "spiceypy",
+        "matplotlib",
+        "mayavi",
+        "pandas",
+        "PyMieScatt",
+    ],
+    cmake_install_dir="rss_ringoccs",
+    cmake_args=[
+        "-DBUILD_EXAMPLES:BOOL=OFF",
+        "-DBUILD_TESTS:BOOL=OFF",
+        "-DPython_VERSION={}.{}.{}".format(
+            sys.version_info.major, sys.version_info.minor, sys.version_info.micro
+        ),
+    ],
 )
-

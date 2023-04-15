a8ba9f9e6c90565290dbf1d38ab2ed81b2e8ef69
Ty <ty.cumby@gmail.com>
2023-04-15 09:51:46 -0600

    Switch to conan

---
+++ cmake/ConanHelpers.cmake
@@ -0,0 +1,15 @@
+function(conan_install)
+    set(prefix _CONAN_INSTALL)
+    set(noValues "")
+    set(singleValues SOURCE_DIR OUTPUT_DIR)
+    set(multiValues "")
+    # Process the arguments passed in
+    include(CMakeParseArguments)
+    cmake_parse_arguments(${prefix} "${noValues}" "${singleValues}" "${multiValues}" ${ARGN})
+
+    execute_process(
+        COMMAND conan install --build=missing --output-folder ${${prefix}OUTPUT_DIR} ${${prefix}SOURCE_DIR}
+        COMMAND_ECHO STDOUT
+        COMMAND_ERROR_IS_FATAL
+    )
+endfunction()

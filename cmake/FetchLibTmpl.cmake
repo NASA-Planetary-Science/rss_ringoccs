190982db1230c04709a2135b972cc25cbac2adb9
Ty <ty.cumby@gmail.com>
2023-04-22 12:18:16 -0600

    Fix libtmpl fetch source dir and set to C11

--- cmake/FetchLibTmpl.cmake
+++ cmake/FetchLibTmpl.cmake
@@ -7,6 +7,7 @@ set(LIBTMPL_BUILD_EXAMPLES OFF)
 set(LIBTMPL_BUILD_TESTS OFF)
 set(LIBTMPL_USE_ASM ON)
 set(LIBTMPL_USE_MATH ON)
+set(LIBTMPL_STDVER "-std=c11")
 if(WIN32)
     set(LIBTMPL_USE_FASM OFF)
     set(LIBTMPL_USE_OMP OFF)
@@ -22,6 +23,7 @@ fetchcontent_declare(
     tmpl_checkout
     GIT_REPOSITORY https://github.com/tcumby/libtmpl.git
     GIT_TAG add-cmake-build-system-redo
+    SOURCE_DIR libtmpl
 )
 
 fetchcontent_makeavailable(tmpl_checkout)

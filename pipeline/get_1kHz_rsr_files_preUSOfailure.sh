378e7f9c7108224c88cf33e1a21774198d9ea489
ryanmaguire <rmaguire@protonmail.com>
2023-07-15 19:02:56 -0400

    Updating shell scripts for retrieving data.

--- pipeline/get_1kHz_rsr_files_preUSOfailure.sh
+++ pipeline/get_1kHz_rsr_files_preUSOfailure.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ ! "$BASH_VERSION" ] ; then
-        echo "Please use BASH to run this script ($0)" 1>&2
-        exit 1
+    echo "Please use BASH to run this script ($0)" 1>&2
+    exit 1
 fi
+
 web="https://atmos.nmsu.edu/pdsd/archive/data/"
+
 # pre-USO failure 1 kHz RSR files
 input=../tables/rsr_1kHz_files_before_USO_failure.txt
 
@@ -14,23 +16,21 @@ while IFS= read -r dirfil
 do
     localdirfil="../data/$dirfil"
     webdirfil=$web$dirfil
-    if [ ! -e $localdirfil ]
-    then
-        echo Current File $localdirfil does not locally exist. Fetching.
+    if [ ! -e $localdirfil ]; then
+
+        echo "Fetching $localdirfil."
+
         if curl --output /dev/null --silent --head --fail "$webdirfil"; then
-            echo "URL exists: $webdirfil"
             curl --create-dirs -o $localdirfil $webdirfil
         else
-            echo "URL does not exist: $webdirfil"
             echo "URL does not exist: $webdirfil" >> get_rsr_1khz_error_log.txt
         fi
     else
-            echo Current File "$localdirfil" already exists.
+        echo "$localdirfil already exists."
     fi
 done < "$input"
 
 # If the error log is empty, remove it.
-if [ ! -s get_rsr_1khz_error_log.txt ]
-then
+if [ ! -s get_rsr_1khz_error_log.txt ]; then
     rm -f get_rsr_1khz_error_log.txt
 fi
--- pipeline/get_kernels.sh
+++ pipeline/get_kernels.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
 if [ ! "$BASH_VERSION" ] ; then
-	echo "Please use BASH to run this script ($0)" 1>&2
-	exit 1
+    echo "Please use BASH to run this script ($0)" 1>&2
+    exit 1
 fi
-if [ "$#" != 1 ] && [ "$#" != 2 ]; then
-	echo "Illegal number of arguments: 2 required. Number given: $#"
-	echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
-	echo "Example: list_of_kernels.txt containing:"
-	echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
-	echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
-	echo "./get_kernels list_of_kernels.txt ../kernels/"
-	exit 1
-fi
+
 web="https://naif.jpl.nasa.gov/pub/"
-input="$1"
+input="../tables/complete_list_of_kernels.txt"
+
+# Create an error log.
+touch get_kernels_error_log.txt
+
 while IFS= read -r dirfil
 do
-	if [ "$#" = 1 ]; then
-		localdirfil=$dirfil
-	elif [ "$#" = 2 ]; then
-		localdirfil="$2$dirfil"
-	else
-		echo "Illegal number of arguments: Two required"
-		echo "Useage: get_kernels.sh list_of_kernels.txt /path/to/kernels"
-		echo "Example: list_of_kernels.txt containing:"
-		echo "naif/CASSINI/kernels/spk/061129R_SCPSE_06292_06307.bsp"
-		echo "naif/CASSINI/kernels/spk/061213R_SCPSE_06308_06318.bsp"
-		echo "./get_kernels list_of_kernels.txt ../kernels/"
-		exit 1
-	fi
-	webdirfil=$web$dirfil
-	if [ ! -e $localdirfil ]
-	then
-    	echo Current File $localdirfil does not locally exist. Fetching.
-		if curl --output /dev/null --silent --head --fail "$webdirfil"; then
- 			echo "URL exists: $webdirfil"
-			curl --create-dirs -o $localdirfil $webdirfil
-		else
-			echo "URL does not exist: $webdirfil"
-			exit 1
-		fi
-	else
-    		echo Current File "$localdirfil" already exists.
-	fi
+    localdirfil="../kernels/$dirfil"
+    webdirfil=$web$dirfil
+    if [ ! -e $localdirfil ]; then
+
+        echo "Current File $localdirfil. Fetching..."
+
+        if curl --output /dev/null --silent --head --fail "$webdirfil"; then
+            curl --create-dirs -o $localdirfil $webdirfil
+        else
+            echo "URL does not exist: $webdirfil" >> get_kernels_error_log.txt
+        fi
+    else
+        echo Current File "$localdirfil" already exists.
+    fi
 done < "$input"
+
+# If the error log is empty, remove it.
+if [ ! -s get_kernels_error_log.txt ]; then
+    rm -f get_kernels_error_log.txt
+fi

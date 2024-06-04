9e600b20b073d7a0ae9f1aeced70369b9a59ca38
ryanmaguire <rmaguire@protonmail.com>
2024-06-04 14:03:16 -0400

    Adding global list for geo columnn names.

---
+++ graveyard/Python/geo_columns.py
@@ -0,0 +1,47 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of rss_ringoccs.                                         #
+#                                                                              #
+#   rss_ringoccs is free software: you can redistribute it and/or              #
+#   modify it under the terms of the GNU General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   rss_ringoccs is distributed in the hope that it will be useful             #
+#   but WITHOUT ANY WARRANTY# without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+#   Purpose:                                                                   #
+#       Provides the column names for a GEO.TAB file.                          #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   2018                                                               #
+################################################################################
+"""
+
+GEO_NAMES = [
+    "t_oet_spm_vals",
+    "t_ret_spm_vals",
+    "t_set_spm_vals",
+    "rho_km_vals",
+    "phi_rl_deg_vals",
+    "phi_ora_deg_vals",
+    "B_deg_vals",
+    "D_km_vals",
+    "rho_dot_kms_vals",
+    "phi_rl_dot_kms_vals",
+    "F_km_vals",
+    "R_imp_km_vals",
+    "rx_km_vals",
+    "ry_km_vals",
+    "rz_km_vals",
+    "vx_kms_vals",
+    "vy_kms_vals",
+    "vz_kms_vals"
+]

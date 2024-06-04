0527a17d21e4b62dfe89aca7f3c9b22ddd82be74
ryanmaguire <rmaguire@protonmail.com>
2024-06-04 09:25:56 -0400

    Adding region dictionary.

---
+++ graveyard/Python/regions.py
@@ -0,0 +1,51 @@
+"""
+################################################################################
+#                                   LICENSE                                    #
+################################################################################
+#   This file is part of rss_ringoccs.                                         #
+#                                                                              #
+#   rss_ringoccs is free software: you can redistribute it and/or              #
+#   modify it under the terms of the GNU General Public License as published   #
+#   by the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   rss_ringoccs is distributed in the hope that it will be useful             #
+#   but WITHOUT ANY WARRANTY# without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+#   Purpose:                                                                   #
+#       Provides a dictionary with points of interest for the rings of Saturn. #
+################################################################################
+#   Author: Ryan Maguire                                                       #
+#   Date:   2018                                                               #
+################################################################################
+"""
+
+# Dictionary containing regions of interest within the Saturnian Rings.
+REGIONS = {
+    "all": [1.0, 400000.0],
+    "besselbarnard": [120210.0, 120330.0],
+    "bessel-barnard": [120210.0, 120330.0],
+    "cringripples": [77690.0, 77760.0],
+    "encke": [132900.0, 134200.0],
+    "enckegap": [132900.0, 134200.0],
+    "herschel": [118100.0, 118380.0],
+    "herschelgap": [118100.0, 118380.0],
+    "huygens": [117650.0, 117950.0],
+    "huygensringlet": [117650.0, 117950.0],
+    "janusepimetheus": [96200.0, 96800.0],
+    "jeffreys": [118900.0, 119000.0],
+    "jeffreysgap": [118900.0, 119000.0],
+    "kuiper": [119300.0, 119500.0],
+    "kuipergap": [119300.0, 119500.0],
+    "maxwell": [87410.0, 87610.0],
+    "maxwellringlet": [87410.0, 87610.0],
+    "russell": [118550.0, 118660.0],
+    "russellgap": [118550.0, 118660.0],
+    "titan": [77870.0, 77930.0],
+    "titanringlet": [77870.0, 77930.0]
+}

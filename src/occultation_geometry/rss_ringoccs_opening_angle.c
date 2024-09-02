9521886788a0ed9e0d33e78523f9b4f17de14f42
ryanmaguire <rmaguire@protonmail.com>
2024-09-02 15:36:45 -0400

    Removing explicit include of SpiceUsr.h

--- src/occultation_geometry/rss_ringoccs_opening_angle.c
+++ src/occultation_geometry/rss_ringoccs_opening_angle.c
@@ -1,6 +1,11 @@
 #include <rss_ringoccs/include/rss_ringoccs_occultation_geometry.h>
 #include <libtmpl/include/tmpl.h>
-#include <cspice/include/SpiceUsr.h>
+
+extern void kclear_c(void);
+extern void furnsh_c(const char *kernels);
+extern void
+spkpos_c(const char *targ, double et, const char *ref, const char *abcorr,
+         const char *obs, double ptarg[3], double *lt);
 
 double
 rssringoccs_Double_Ring_Opening_Angle(const double time,
@@ -21,6 +26,6 @@ rssringoccs_Double_Ring_Opening_Angle(const double time,
 
     spkpos_c(dsn, time, ref, "CN", spacecraft, ptarg.dat, &ltime);
 
-    opening_radians = tmpl_Pi_By_Two - vsep_c(ptarg.dat, nhat->dat);
+    opening_radians = tmpl_Pi_By_Two - tmpl_3DDouble_Angle(&ptarg, nhat);
     return opening_radians * tmpl_Rad_to_Deg;
 }

18fc6e55637d9cffe87cba645b11acdc6ad94f80
ryanmaguire <rmaguire@protonmail.com>
2024-12-29 17:50:00 -0500

    Fixing includes.

--- rss_ringoccs/crssringoccs/get_merged_csv_data/crssringoccs_get_merged_csv_data_steal.c
+++ rss_ringoccs/crssringoccs/get_merged_csv_data/crssringoccs_get_merged_csv_data_steal.c
@@ -16,6 +16,7 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
  ******************************************************************************/
+#include <libtmpl/include/tmpl_string.h>
 #include "../crssringoccs.h"
 
 /*  Macro for the crssringoccs_set_var function to shorten the syntax.        */

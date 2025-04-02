ef8499f28e1410377cda3f9e842b16e7a3a05534
ryanmaguire <rmaguire@protonmail.com>
2025-04-02 18:23:22 -0400

    Simplifying shell scripts. Improving POSIX compliance.

--- pipeline/get_1kHz_rsr_files_postUSOfailure.sh
+++ pipeline/get_1kHz_rsr_files_postUSOfailure.sh
@@ -1,26 +1,21 @@
-#!/bin/bash
-if [ ! "$BASH_VERSION" ] ; then
-        echo "Please use BASH to run this script ($0)" 1>&2
-        exit 1
-fi
-web="https://atmos.nmsu.edu/pdsd/archive/data/"
-# post-USO failure 1 kHz RSR files with uplink station documentation
-input=../tables/rsr_1kHz_files_after_USO_failure_withuplink.txt
-while IFS= read -r dirfil
-do
-localdirfil="../data/$dirfil"
-webdirfil=$web$dirfil
-if [ ! -e $localdirfil ]
-then
-echo Current File $localdirfil does not locally exist. Fetching.
-if curl --output /dev/null --silent --head --fail "$webdirfil"; then
-echo "URL exists: $webdirfil"
-curl --create-dirs -o $localdirfil $webdirfil
-else
-echo "URL does not exist: $webdirfil"
-exit 1
-fi
-else
-echo Current File "$localdirfil" already exists.
-fi
-done < "$input"
+################################################################################
+#                                    LICENSE                                   #
+################################################################################
+#   This file is part of rss_ringoccs.                                         #
+#                                                                              #
+#   rss_ringoccs is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   rss_ringoccs is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+. get_rsr_data.sh
+InputFile="../tables/rsr_1kHz_files_after_USO_failure_withuplink.txt"
+get_rsr_data "$InputFile"
--- pipeline/get_1kHz_rsr_files_preUSOfailure.sh
+++ pipeline/get_1kHz_rsr_files_preUSOfailure.sh
@@ -1,36 +1,21 @@
-#!/bin/bash
-if [ ! "$BASH_VERSION" ] ; then
-    echo "Please use BASH to run this script ($0)" 1>&2
-    exit 1
-fi
-
-web="https://atmos.nmsu.edu/pdsd/archive/data/"
-
-# pre-USO failure 1 kHz RSR files
-input=../tables/rsr_1kHz_files_before_USO_failure.txt
-
-# Create an error log.
-touch get_rsr_1khz_error_log.txt
-
-while IFS= read -r dirfil
-do
-    localdirfil="../data/$dirfil"
-    webdirfil=$web$dirfil
-    if [ ! -e $localdirfil ]; then
-
-        echo "Fetching $localdirfil."
-
-        if curl --output /dev/null --silent --head --fail "$webdirfil"; then
-            curl --create-dirs -o $localdirfil $webdirfil
-        else
-            echo "URL does not exist: $webdirfil" >> get_rsr_1khz_error_log.txt
-        fi
-    else
-        echo "$localdirfil already exists."
-    fi
-done < "$input"
-
-# If the error log is empty, remove it.
-if [ ! -s get_rsr_1khz_error_log.txt ]; then
-    rm -f get_rsr_1khz_error_log.txt
-fi
+################################################################################
+#                                    LICENSE                                   #
+################################################################################
+#   This file is part of rss_ringoccs.                                         #
+#                                                                              #
+#   rss_ringoccs is free software: you can redistribute it and/or modify       #
+#   it under the terms of the GNU General Public License as published by       #
+#   the Free Software Foundation, either version 3 of the License, or          #
+#   (at your option) any later version.                                        #
+#                                                                              #
+#   rss_ringoccs is distributed in the hope that it will be useful,            #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of             #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
+#   GNU General Public License for more details.                               #
+#                                                                              #
+#   You should have received a copy of the GNU General Public License          #
+#   along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.     #
+################################################################################
+. get_rsr_data.sh
+InputFile="../tables/rsr_1kHz_files_before_USO_failure.txt"
+get_rsr_data "$InputFile"

77b4af6658a4fa7d01686eb00051296f69c0149f
ryanmaguire <rmaguire@protonmail.com>
2025-05-27 07:36:17 -0400

    Using newest latest-and-greatest reconstruction ideas.

--- src/fresnel_transform/rss_ringoccs_fresnel_transform_newton.c
+++ src/fresnel_transform/rss_ringoccs_fresnel_transform_newton.c
@@ -19,114 +19,76 @@
 #include <libtmpl/include/tmpl.h>
 #include <rss_ringoccs/include/rss_ringoccs_fresnel_transform.h>
 
-/******************************************************************************
- *  Function:                                                                 *
- *      Fresnel_Transform_Newton_Double / Fresnel_Transform_Newton_Norm_Double*
- *  Purpose:                                                                  *
- *      Performs diffraction correction by using the Newton-Raphson method of *
- *      root-finding to compute the stationary value of the Fresnel kernel.   *
- *  Arguments:                                                                *
- *      x_arr (double *):                                                     *
- *          Defined as rho-rho0, where rho0 is the ring radius of the point   *
- *          being reconstructed and rho is the dummy variable of integration  *
- *          which varies from rho0-W/2 to rho+W/2, W being the window width.  *
- *          This should contain n_pts (see below) number of elements.         *
- *      phi_arr (double *):                                                   *
- *          The ring azimuth angle corresponding to rho, in radians.          *
- *          This should contain n_pts (see below) number of elements.         *
- *      T_in (char *):                                                        *
- *          The diffracted data. Must contain at least n_pts (see below)      *
- *          points to the left and right of the starting point, or a          *
- *          segmentation fault will occur. This error check check is perform  *
- *          when the function is called in _diffraction_functions.c, as well  *
- *          as in the Python DiffractionCorrection class found in             *
- *          diffraction_correction.py.                                        *
- *      w_func (double *):                                                    *
- *          The window/tapering function, as a function of x_arr.             *
- *          This should contain n_pts (see below) number of elements.         *
- *      kd (double):                                                          *
- *          The wavenumber k scaled by D (See above).                         *
- *      r (double):                                                           *
- *          The radius of the current point be reconstructed.                 *
- *      B (double):                                                           *
- *          The ring opening angle, in radians.                               *
- *      D (double):                                                           *
- *          The distance from the spacecraft to the ring-intercept point.     *
- *      EPS (double):                                                         *
- *          The allowable error in the computation of the stationary value of *
- *          the Fresnel kernel.                                               *
- *      toler (long):                                                         *
- *          The maximum number of iterations the Newton-Raphson method is     *
- *          allowed to undergo.                                               *
- *      dx (double):                                                          *
- *          The sample spacing, equivalent to x_arr[1] - x_arr[0].            *
- *      F (double):                                                           *
- *          The Fresnel scale, in the same units as D and dx.                 *
- *      n_pts (long):                                                         *
- *          Half the number of points in the window width. The symmetry of    *
- *          the quadratic approximation allows one to perform the inversion   *
- *      T_in_steps (long):                                                    *
- *          The number of steps in memory of the nth point to the (n+1)th     *
- *          point in the T_in pointer.                                        *
- *  Outputs:                                                                  *
- *      T_out (complex double):                                               *
- *          The diffraction corrected profile.                                *
- ******************************************************************************/
 void
-rssringoccs_Fresnel_Transform_Newton(rssringoccs_TAUObj *tau,
-                                     const double *w_func,
-                                     size_t n_pts,
-                                     size_t center)
+rssringoccs_Fresnel_Transform_Newton(
+    rssringoccs_TAUObj * TMPL_RESTRICT const tau,
+    const double * TMPL_RESTRICT const w_func,
+    size_t n_pts,
+    size_t center
+)
 {
-    /*  Declare all necessary variables. i and j are used for indexing.       */
+    /*  m is used for indexing the Riemann sum, offset is the index for the   *
+     *  off-center point in the window, the dummy variable of integration.    */
     size_t m, offset;
 
-    /*  The Fresnel kernel and ring azimuth angle.                            */
-    double psi, phi, factor;
-    tmpl_ComplexDouble exp_psi, integrand;
+    /*  Scale factor for the integral is (1 + i) / 2F, where F is the Fresnel *
+     *  scale. The Riemann sum also has a factor of "dx", so the scalar part  *
+     *  of the factor is dx / 2F. The "1 + i" part is included later.         */
+    const double factor = 0.5 * tau->dx_km / tau->F_km_vals[center];
+
+    /*  Complex variable for the express T_hat * w * exp(-i psi). T_hat is    *
+     *  the diffracted data, w is the window function, and psi is the         *
+     *  Fresnel kernel.                                                       */
+    tmpl_ComplexDouble integrand;
 
     /*  Initialize T_out and norm to zero so we can loop over later.          */
     tau->T_out[center] = tmpl_CDouble_Zero;
-    factor = 0.5 * tau->dx_km / tau->F_km_vals[center];
 
     /*  Symmetry is lost without the Legendre polynomials, or Fresnel         *
      *  quadratic. Must compute everything from -W/2 to W/2.                  */
-    offset = center - (n_pts - 1UL)/2UL;
+    offset = center - ((n_pts - 1) >> 1);
 
     /*  Use a Riemann Sum to approximate the Fresnel Inverse Integral.        */
-    for (m = 0; m<n_pts; ++m)
+    for (m = 0; m < n_pts; ++m)
     {
-        /*  Calculate the stationary value of psi with respect to phi.        */
-        phi = tmpl_Double_Ideal_Stationary_Cyl_Fresnel_Phi_Newton_Deg(
-            tau->k_vals[center],        /* Wavenumber. */
-            tau->rho_km_vals[center],   /* Dummy radius. */
-            tau->rho_km_vals[offset],   /* Ring radius. */
-            tau->phi_deg_vals[offset],  /* Dummy azimuthal angle. */
-            tau->phi_deg_vals[offset],  /* Ring azimuth angle. */
-            tau->B_deg_vals[center],    /* Ring opening angle. */
-            tau->D_km_vals[center],     /* Observer distance. */
-            tau->EPS,                   /* Allowed error. */
-            tau->toler                  /* Max number of iterations. */
+        /*  This function does not assume the ideal geometry present in MTR86 *
+         *  where u . y = 0, u being the vector from the spacecraft to the    *
+         *  ring intercept point. Instead we compute using the full Fresnel   *
+         *  kernel. To do this requires rho, rho0, and R as vectors in their  *
+         *  Cartesian coordinates. Compute this.                              */
+        const tmpl_TwoVectorDouble rho0 = tmpl_2DDouble_Polar(
+            tau->rho_km_vals[offset],
+            tau->phi_deg_vals[offset] * tmpl_Double_Deg_To_Rad
         );
 
-        /*  Compute the left side of exp(-ipsi) using Euler's Formula.        */
-        psi = tmpl_Double_Ideal_Cyl_Fresnel_Psi_Deg(
-            tau->k_vals[center],        /* Wavenumber. */
-            tau->rho_km_vals[center],   /* Dummy radius. */
-            tau->rho_km_vals[offset],   /* Ring radius. */
-            phi,                        /* Stationary azimuth angle. */
-            tau->phi_deg_vals[offset],  /* Ring azimuth angle. */
-            tau->B_deg_vals[center],    /* Ring opening angle. */
-            tau->D_km_vals[center]      /* Observer distance. */
+        const tmpl_TwoVectorDouble rho = tmpl_2DDouble_Polar(
+            tau->rho_km_vals[center],
+            tau->phi_deg_vals[offset] * tmpl_Double_Deg_To_Rad
         );
 
-        exp_psi = tmpl_CDouble_Polar(w_func[m], -psi);
+        const tmpl_ThreeVectorDouble R = tmpl_3DDouble_Rect(
+            tau->rx_km_vals[offset],
+            tau->ry_km_vals[offset],
+            tau->rz_km_vals[offset]
+        );
 
-        /*  Compute the transform with a Riemann sum. If the T_in pointer     *
-         *  does not contain at least 2*n_pts+1 points, n_pts to the left and *
-         *  right of the center, then this will create a segmentation fault.  */
+        /*  Compute the Fresnel kernel at the stationary azimuth angle, phi_s.*/
+        const double psi = tmpl_Double_Stationary_Cyl_Fresnel_Psi(
+            tau->k_vals[center], &rho, &rho0, &R, tau->EPS, tau->toler
+        );
+
+        /*  The complex exponentiated Fresnel kernel is simply exp(-i psi).   *
+         *  This is scaled by the window function and integrated over.        */
+        const tmpl_ComplexDouble exp_psi = tmpl_CDouble_Polar(w_func[m], -psi);
+
+        /*  The integrand is the diffracted data times the tapered complex    *
+         *  exponentiated Fresnel kernel. Compute and add to the sum.         */
         integrand = tmpl_CDouble_Multiply(exp_psi, tau->T_in[offset]);
-        tau->T_out[center] = tmpl_CDouble_Add(tau->T_out[center], integrand);
+
+        tmpl_CDouble_AddTo(&tau->T_out[center], &integrand);
+
+        /*  We start at the left-most edge of the window and move to the      *
+         *  right. We increment "offset" up to center + (n_pts - 1) / 2.      */
         offset += 1;
     }
 
--- src/fresnel_transform/rss_ringoccs_fresnel_transform_newton_norm.c
+++ src/fresnel_transform/rss_ringoccs_fresnel_transform_newton_norm.c
@@ -26,52 +26,57 @@ rssringoccs_Fresnel_Transform_Newton_Norm(rssringoccs_TAUObj *tau,
                                           size_t n_pts,
                                           size_t center)
 {
-    /*  Declare all necessary variables. i and j are used for indexing.       */
+    /*  m is used for indexing the Riemann sum, offset is the index for the   *
+     *  off-center point in the window, the dummy variable of integration.    */
     size_t m, offset;
 
     /*  The Fresnel kernel and the stationary ring azimuth angle.             */
-    double psi, phi, real_norm, abs_norm;
-    tmpl_ComplexDouble exp_psi, norm, integrand;
+    double real_norm, abs_norm;
+    tmpl_ComplexDouble integrand;
 
     /*  Initialize T_out and norm to zero so we can loop over later.          */
+    tmpl_ComplexDouble norm = tmpl_CDouble_Zero;
     tau->T_out[center] = tmpl_CDouble_Zero;
-    norm = tmpl_CDouble_Zero;
 
     /*  Symmetry is lost without the Legendre polynomials, or Fresnel         *
      *  quadratic. Must compute everything from -W/2 to W/2.                  */
-    offset = center - (n_pts - 1UL) / 2UL;
+    offset = center - ((n_pts - 1) >> 1);
 
     /*  Use a Riemann Sum to approximate the Fresnel Inverse Integral.        */
-    for (m = 0; m<n_pts; ++m)
+    for (m = 0; m < n_pts; ++m)
     {
-        /*  Calculate the stationary value of psi with respect to phi.        */
-        phi = tmpl_Double_Ideal_Stationary_Cyl_Fresnel_Phi_Newton_Deg(
-            tau->k_vals[center],        /* Wavenumber. */
-            tau->rho_km_vals[center],   /* Dummy radius. */
-            tau->rho_km_vals[offset],   /* Ring radius. */
-            tau->phi_deg_vals[offset],  /* Dummy azimuthal angle. */
-            tau->phi_deg_vals[offset],  /* Ring azimuth angle. */
-            tau->B_deg_vals[center],    /* Ring opening angle. */
-            tau->D_km_vals[center],     /* Observer distance. */
-            tau->EPS,                   /* Allowed error. */
-            tau->toler                  /* Max number of iterations. */
+        /*  This function does not assume the ideal geometry present in MTR86 *
+         *  where u . y = 0, u being the vector from the spacecraft to the    *
+         *  ring intercept point. Instead we compute using the full Fresnel   *
+         *  kernel. To do this requires rho, rho0, and R as vectors in their  *
+         *  Cartesian coordinates. Compute this.                              */
+        const tmpl_TwoVectorDouble rho0 = tmpl_2DDouble_Polar(
+            tau->rho_km_vals[offset],
+            tau->phi_deg_vals[offset] * tmpl_Double_Deg_To_Rad
         );
 
-        /*  Compute the left side of exp(-ipsi) using Euler's Formula.        */
-        psi = tmpl_Double_Ideal_Cyl_Fresnel_Psi_Deg(
-            tau->k_vals[center],        /* Wavenumber. */
-            tau->rho_km_vals[center],   /* Dummy radius. */
-            tau->rho_km_vals[offset],   /* Ring radius. */
-            phi,                        /* Stationary azimuth angle. */
-            tau->phi_deg_vals[offset],  /* Ring azimuth angle. */
-            tau->B_deg_vals[center],    /* Ring opening angle. */
-            tau->D_km_vals[center]      /* Observer distance. */
+        const tmpl_TwoVectorDouble rho = tmpl_2DDouble_Polar(
+            tau->rho_km_vals[center],
+            tau->phi_deg_vals[offset] * tmpl_Double_Deg_To_Rad
         );
 
-        exp_psi = tmpl_CDouble_Polar(w_func[m], -psi);
+        const tmpl_ThreeVectorDouble R = tmpl_3DDouble_Rect(
+            tau->rx_km_vals[offset],
+            tau->ry_km_vals[offset],
+            tau->rz_km_vals[offset]
+        );
+
+        /*  Compute the Fresnel kernel at the stationary azimuth angle, phi_s.*/
+        const double psi = tmpl_Double_Stationary_Cyl_Fresnel_Psi(
+            tau->k_vals[center], &rho, &rho0, &R, tau->EPS, tau->toler
+        );
+
+        /*  The complex exponentiated Fresnel kernel is simply exp(-i psi).   *
+         *  This is scaled by the window function and integrated over.        */
+        const tmpl_ComplexDouble exp_psi = tmpl_CDouble_Polar(w_func[m], -psi);
 
         /*  Compute the norm using a Riemann sum as well.                     */
-        norm = tmpl_CDouble_Add(norm, exp_psi);
+        tmpl_CDouble_AddTo(&norm, &exp_psi);
 
         /*  Compute the transform with a Riemann sum. If the T_in pointer     *
          *  does not contain at least 2*n_pts+1 points, n_pts to the left and *

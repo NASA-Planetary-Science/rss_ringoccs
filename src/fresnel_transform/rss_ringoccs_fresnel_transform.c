4b3123a6a408ff6dff4b87fb6fc09264b5733666
ryanmaguire <rmaguire@protonmail.com>
2025-05-28 13:42:17 -0400

    Modifying how forward transforms are dealt with.

--- src/fresnel_transform/rss_ringoccs_fresnel_transform.c
+++ src/fresnel_transform/rss_ringoccs_fresnel_transform.c
@@ -16,46 +16,10 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
  ******************************************************************************/
-#include <libtmpl/include/tmpl.h>
+#include <libtmpl/include/tmpl_complex.h>
+#include <libtmpl/include/constants/tmpl_math_constants.h>
 #include <rss_ringoccs/include/rss_ringoccs_fresnel_transform.h>
 
-/******************************************************************************
- *  Function:                                                                 *
- *      Fresnel_Transform_Double / Fresnel_Transform_Norm_Double              *
- *  Purpose:                                                                  *
- *      Perform diffraction correction on diffraction limited data using the  *
- *      classic Fresnel quadratic approximation to the Fresnel kernel.        *
- *  Arguments:                                                                *
- *      x_arr (double *):                                                     *
- *          Defined as pi/2 (rho-rho0)^2, where rho is the ring radius of     *
- *          the point being reconstructed, and rho0 is the dummy variable of  *
- *          integration which varies from rho-W/2 to zero, W being the        *
- *          window width. This should point to n_pts number of elements.      *
- *      T_in (char *):                                                        *
- *          The diffracted data. Must contain at least n_pts (see below)      *
- *          points to the left and right of the starting point, or a          *
- *          segmentation fault will occur. This error check check is perform  *
- *          when the function is called in _diffraction_functions.c, as well  *
- *          as in the Python DiffractionCorrection class found in             *
- *          diffraction_correction.py.                                        *
- *      w_func (double *):                                                    *
- *          The window/tapering function, as a function of x_arr. This should *
- *          point to n_pts number of elements.                                *
- *      F (double):                                                           *
- *          The Fresnel scale at rho.                                         *
- *      dx (double):                                                          *
- *          The sample spacing, equivalent to x_arr[1] - x_arr[0].            *
- *      n_pts (long):                                                         *
- *          Half the number of points in the window width. The symmetry of    *
- *          the quadratic approximation allows one to perform the inversion   *
- *          with only half of the window. This saves a lot of computation.    *
- *      T_in_steps (long):                                                    *
- *          The number of steps in memory of the nth point to the (n+1)th     *
- *          point in the T_in pointer.                                        *
- *  Outputs:                                                                  *
- *      T_out (complex double):                                               *
- *          The diffraction corrected profile.                                *
- ******************************************************************************/
 void
 rssringoccs_Fresnel_Transform(rssringoccs_TAUObj *tau,
                               const double *x_arr,
@@ -66,12 +30,24 @@ rssringoccs_Fresnel_Transform(rssringoccs_TAUObj *tau,
     /*  Declare all necessary variables. i and j are used for indexing.       */
     size_t m, n;
 
-    /*  rcpr_F and rcpr_F2 are the reciprocal of the Fresnel scale, and the   *
-     *  square of this. x is used as the argument of the Fresnel kernel.      */
-    double x, rcpr_F, rcpr_F2, factor;
+    /*  Variables for the real and imaginary parts of the output.             */
+    double real, imag;
+
+    /*  Division is more expensive than multiplication, so store the          *
+     *  reciprocal of F as a variable and compute with that.                  */
+    const double rcpr_fresnel_scale = 1.0 / tau->F_km_vals[center];
+
+    /*  The forward transform can be computed by negating the psi factor. Set *
+     *  the sign to 1 for the inverse transform and -1 for the forward one.   */
+    const double sign = (tau->use_fwd ? -1.0 : 1.0);
+    const double psi_factor = sign * rcpr_fresnel_scale * rcpr_fresnel_scale;
+
+    /*  Division is more expensive than multiplication, so store the          *
+     *  reciprocal of F as a variable and compute with that.                  */
+    const double scale = 0.5 * tau->dx_km * rcpr_fresnel_scale;
 
     /*  exp_negative_ix is used for the Fresnel kernel.                       */
-    tmpl_ComplexDouble exp_negative_ix, integrand, arg;
+    tmpl_ComplexDouble exp_negative_ipsi, integrand;
 
     /*  Start with the central point in the Riemann sum. This is center of    *
      *  window function. That is, where w_func = 1. This is just T_in at      *
@@ -81,33 +57,33 @@ rssringoccs_Fresnel_Transform(rssringoccs_TAUObj *tau,
     /*  From symmetry we need only compute -W/2 to zero, so start at -n_pts.  */
     n = n_pts;
 
-    /*  Division is more expensive than multiplication, so store the          *
-     *  reciprocal of F as a variable and compute with that.                  */
-    rcpr_F = 1.0 / tau->F_km_vals[center];
-    rcpr_F2 = rcpr_F*rcpr_F;
-    factor = 0.5*tau->dx_km*rcpr_F;
-
     /*  Use a Riemann Sum to approximate the Fresnel Inverse Integral.        */
     for (m = 0; m<n_pts; ++m)
     {
-        x = x_arr[m]*rcpr_F2;
+        /*  The x array passed to us contains (pi/2)(r - r0)^2. The Fresnel   *
+         *  approximation is psi = (pi/2) * (r - r0)^2 / F^2.                 */
+        const double psi = psi_factor * x_arr[m];
 
         /*  Use Euler's Theorem to compute exp(-ix). Scale by window function.*/
-        exp_negative_ix = tmpl_CDouble_Polar(w_func[m], -x);
+        exp_negative_ipsi = tmpl_CDouble_Polar(w_func[m], -psi);
 
         /*  Take advantage of the symmetry of the quadratic approximation.    *
          *  This cuts the number of computations roughly in half. If the T_in *
          *  pointer does not contain at least 2*n_pts+1 points, n_pts to the  *
          *  left and n_pts to the right of the center, then this will create  *
          *  a segmentation fault, crashing the program.                       */
-        integrand = tmpl_CDouble_Add(tau->T_in[center - n],
-                                     tau->T_in[center + n]);
-        integrand = tmpl_CDouble_Multiply(exp_negative_ix, integrand);
+        integrand = tmpl_CDouble_Add(tau->T_in[center-n], tau->T_in[center+n]);
+        tmpl_CDouble_MultiplyBy(&integrand, &exp_negative_ipsi);
         tmpl_CDouble_AddTo(&tau->T_out[center], &integrand);
+
+        /*  Decrement the offset index to the next point.                     */
         n--;
     }
 
     /*  Multiply result by the coefficient found in the Fresnel inverse.      */
-    arg = tmpl_CDouble_Rect(factor, factor);
-    tau->T_out[center] = tmpl_CDouble_Multiply(arg, tau->T_out[center]);
+    real = scale * (tau->T_out[center].dat[0] - tau->T_out[center].dat[1]);
+    imag = scale * (tau->T_out[center].dat[0] + tau->T_out[center].dat[1]);
+    tau->T_out[center].dat[0] = real;
+    tau->T_out[center].dat[1] = imag;
 }
+/*  End of rssringoccs_Fresnel_Transform.                                     */

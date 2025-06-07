5db74df4a635c52b623be5c0124a702e91b8272c
ryanmaguire <rmaguire@protonmail.com>
2025-06-07 13:59:50 -0400

    Rewriting main Newton routine. Moving several intermediate steps into their own functions, improving efficiency a bit too.

--- src/reconstruction/rss_ringoccs_diffraction_correction_newton.c
+++ src/reconstruction/rss_ringoccs_diffraction_correction_newton.c
@@ -1,151 +1,168 @@
-#include <stdlib.h>
-#include <math.h>
+#include <libtmpl/include/compat/tmpl_cast.h>
+#include <libtmpl/include/compat/tmpl_free.h>
+#include <libtmpl/include/compat/tmpl_malloc.h>
 #include <libtmpl/include/tmpl_math.h>
-#include <libtmpl/include/tmpl_string.h>
+#include <rss_ringoccs/include/rss_ringoccs_tau.h>
 #include <rss_ringoccs/include/rss_ringoccs_fresnel_transform.h>
 #include <rss_ringoccs/include/rss_ringoccs_reconstruction.h>
 
-/******************************************************************************
- *  Function:                                                                 *
- *      DiffractionCorrectionNewton                                           *
- *  Purpose:                                                                  *
- *      Compute the Fresnel transform using the Newton-Raphson method to      *
- *      compute the stationary value of the Fresnel-Kernel.                   *
- *  Arguments:                                                                *
- *      dlp (DLPObj *):                                                       *
- *          An instance of the DLPObj structure defined in                    *
- *          _diffraction_correction.h. This contains all of the necessary     *
- *          data for diffraction correction, including the geometry of the    *
- *          occultation and actual power and phase data.                      *
- *  Output:                                                                   *
- *      Nothing (void):                                                       *
- *          This is a void function, so no actual output is provided. However *
- *          the T_out pointer within the dlp structure will be changed at the *
- *          end, containing the diffraction correction data.                  *
- *  Notes:                                                                    *
- *      1.) This method is the most accurate, but much slower than the        *
- *          Fresnel and Legendre options. It is accurate for every Rev of the *
- *          Cassini mission with the exception of the Rev133 occultation      *
- *          of which only the Ka band produces accurate results. For X and S  *
- *          bands one needs to use the Perturbed Newton method.               *
- *      2.) The polynomials from the MTR86 are available via the dlp.interp   *
- *          variable. These polynomials are slower and less accurate than the *
- *          normal Newton method since to compute the polynomials the         *
- *          Newton-Raphson method must be performed, and hence the            *
- *          polynomials increase the number of computations needed. The real  *
- *          use of them arises if one uses FFT methods. This routine does NOT *
- *          use FFTs, but rather ordinary integration.                        *
- ******************************************************************************/
-void rssringoccs_Diffraction_Correction_Newton(rssringoccs_TAUObj *tau)
+/*  Performs the Newton transform on a Tau object.                            */
+void rssringoccs_Diffraction_Correction_Newton(rssringoccs_TAUObj * const tau)
 {
-    /*  Variables for indexing. nw_pts is the number of points in the window. */
-    size_t i, j, offset, nw_pts, center;
+    /*  Variables for indexing the inner most loop.                           */
+    size_t n;
 
-    /*  Some variables needed for reconstruction.                             */
-    double w_init, dx, two_dx;
-    double *w_func;
+    /*  Variable for the number of points in the window, and the index of the *
+     *  central point in the window.                                          */
+    size_t nw_pts, center;
 
-    /*  Declare a function pointer for the transform function.                */
-    rssringoccs_FresnelNewtonTransform fresnel_transform;
+    /*  w_init is the initial window width. We do not need to repeatedly      *
+     *  update the window function, we only need to recompute once the width  *
+     *  grows by at least two bins (one on the left and one on the right).    *
+     *  That is, once abs(w_km_vals[center] - w_init) > 2 * dx is true.       */
+    double w_init;
+
+    /*  The distance, dx, between samples. 2 * dx is used frequently enough,  *
+     *  store this value in a variable as well.                               */
+    double dx, two_dx;
+
+    /*  Pointer to a double array for the tapering function. The value        *
+     *  w_func[n] corresponds to the evaluation of w(x[n]) at the nth point   *
+     *  in the window. w is often some combination of Bessel functions, it is *
+     *  beneficial to precompute the tapering function once and use that.     */
+    double *w_func = NULL;
+
+    /*  Dummy variable for realloc. Used to avoid memory leaks.               */
+    void *tmp = NULL;
+
+    /*  Variable for the actual transform used. We'll select it later.        */
+    rssringoccs_FresnelNewtonTransform newton_transform;
+
+    /*  Check that tau isn't NULL before trying to access its members.        */
+    if (!tau)
+        return;
 
     /*  Check that the pointers to the data are not NULL.                     */
     rssringoccs_Tau_Check_Core_Data(tau);
 
-    if (tau->error_occurred)
-        return;
-
-    /* Compute first window width and window function. */
-    center = tau->start;
-
-    /*  Compute some more variables.                                          */
-    w_init = tau->w_km_vals[center];
-    dx     = tau->rho_km_vals[center+1] - tau->rho_km_vals[center];
-    two_dx = 2.0*dx;
-    nw_pts = 2UL*((size_t)(w_init / two_dx)) + 1UL;
-    offset = center - (nw_pts - 1UL) / 2UL;
-
-    /* Check to ensure you have enough data to the left.                      */
+    /* Check to ensure you have enough data to process the transform.         */
     rssringoccs_Tau_Check_Data_Range(tau);
 
+    /*  The previous functions set the error_occurred Boolean to true on      *
+     *  error. Do not proceed if an error occurred, check for this.           */
     if (tau->error_occurred)
         return;
 
+    /*  We have enough data to the left and right of the starting point to    *
+     *  perform the transform. Initialize center to the starting index.       */
+    center = tau->start;
+
+    /*  We will not recompute the window function at each point. We update    *
+     *  once this value differs from w_km_vals[n] by more than 2 * dx.        */
+    w_init = tau->w_km_vals[center];
+
+    /*  The rho_km_vals array is required to have equally spaced samples. The *
+     *  displacement between bins can be computed from the difference of any  *
+     *  two successive points.                                                */
+    dx = tau->rho_km_vals[center + 1] - tau->rho_km_vals[center];
+    two_dx = 2.0 * dx;
+
+    /*  The windows must contain an odd number of samples, half to the left   *
+     *  of the center, half to the right, and the center itself.              */
+    nw_pts = (TMPL_CAST(w_init / two_dx, size_t) << 1) + 1;
+
     /*  Set the correct function pointer.                                     */
-    if (tau->use_norm)
-    {
-        if (tau->psinum == rssringoccs_PsiType_Newton)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton_Norm;
-        else if (tau->psinum == rssringoccs_PsiType_NewtonPerturb)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Perturbed_Newton_Norm;
-        else if (tau->psinum == rssringoccs_PsiType_NewtonElliptical)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton_Elliptical_Norm;
-        else
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton_Quartic_Norm;
-    }
-    else
-    {
-        if (tau->psinum == rssringoccs_PsiType_Newton)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton;
-        else if (tau->psinum == rssringoccs_PsiType_NewtonPerturb)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Perturbed_Newton;
-        else if (tau->psinum == rssringoccs_PsiType_NewtonElliptical)
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton_Elliptical;
-        else
-            fresnel_transform = rssringoccs_Fresnel_Transform_Newton_Quartic;
-    }
+    newton_transform = rssringoccs_Tau_Select_Newton_Transform(tau);
 
-    /*  Allocate memory for these required variables.                         */
-    w_func = malloc(sizeof(*w_func) * nw_pts);
+    /*  The selector function checks if the psinum in the Tau object does not *
+     *  represent a Newton transform. It will set the error_occurred Boolean  *
+     *  should this take place. Check.                                        */
+    if (tau->error_occurred)
+        return;
 
-    if (!(w_func))
+    /*  At the time of this writing, not every Newton transform has been      *
+     *  implemented (there are many!). If the selector function returned NULL,*
+     *  the user requested a psitype that is not currently available.         *
+     *                                                                        *
+     *      TODO:                                                             *
+     *          Implement all psitypes and remove this error check.           */
+    if (!newton_transform)
     {
         tau->error_occurred = tmpl_True;
         tau->error_message =
-            "\n\rError Encountered: rss_ringoccs\n\n"
-            "\r\tDiffractionCorrectionFresnel\n\n"
-            "\rMalloc failed and returned NULL for w_func. Returning.\n\n";
+            "\n\rError Encountered: rss_ringoccs\n"
+            "\r\trssringoccs_Diffraction_Correction_Newton\n\n"
+            "\rrssringoccs_Tau_Select_Newton_Transform returned NULL.\n"
+            "\rRequested psitype not implemented yet.\n";
 
         return;
     }
 
-    /*  Compute the rho and phi variables, and the window function.           */
-    for (j=0; j<nw_pts; ++j)
-        w_func[j] = tau->window_func(
-            tau->rho_km_vals[offset+j] - tau->rho_km_vals[center], w_init
-        );
+    /*  Create an array for the pre-computed tapering function.               */
+    w_func = TMPL_MALLOC(double, nw_pts);
 
-    /*  Check tau->use_norm outside of the inner for loop to prevent          *
-     *  redundantly checking an if-then statement over and over again.        */
-
-    /*  Run diffraction correction point by point.                        */
-    for (i=0; i < tau->n_used; ++i)
+    if (!w_func)
     {
-        /*  If the window width changes significantly, recompute w_func.  */
-        if (fabs(w_init - tau->w_km_vals[center]) >= two_dx)
+        tau->error_occurred = tmpl_True;
+        tau->error_message =
+            "\n\rError Encountered: rss_ringoccs\n"
+            "\r\trssringoccs_Diffraction_Correction_Newton\n\n"
+            "\rmalloc returned NULL for w_func.\n";
+
+        return;
+    }
+
+    /*  Compute the values in the window function for the entire array.       */
+    rssringoccs_Tau_Compute_Window(tau, w_func, nw_pts, center);
+
+    /*  Loop through all of the points in the processing range and run the    *
+     *  transform (either forward or inverse) on the data.                    */
+    for (n = 0; n < tau->n_used; ++n)
+    {
+        /*  The window width varies with the Fresnel scale, which depends on  *
+         *  the geometry and hence varies across the data set. Compute how    *
+         *  much the width has changed so far.                                */
+        const double w_err = tmpl_Double_Abs(w_init - tau->w_km_vals[center]);
+
+        /*  If the window width changes significantly, recompute w_func.      */
+        if (w_err >= two_dx)
         {
-            /* Reset w_init and recompute window function.                */
+            /* Reset w_init and recompute window function.                    */
             w_init = tau->w_km_vals[center];
-            nw_pts = 2*((size_t)(w_init / (2.0 * dx))) + 1UL;
-            offset = center - (nw_pts - 1UL) / 2UL;
+            nw_pts = (TMPL_CAST(w_init / two_dx, size_t) << 1) + 1;
 
-            /*  Reallocate memory since the sizes have changed.           */
-            w_func = realloc(w_func, sizeof(*w_func) * nw_pts);
+            /*  Reallocate memory since the sizes have changed.               */
+            tmp = realloc(w_func, sizeof(*w_func) * nw_pts);
 
-            /*  Recompute rho, phi, and the window function.              */
-            for (j=0; j<nw_pts; ++j)
-                w_func[j] = tau->window_func(
-                    tau->rho_km_vals[offset+j] - tau->rho_km_vals[center],
-                    w_init
-                );
+            /*  Make sure realloc did not fail. It returns NULL on failure.   */
+            if (!tmp)
+            {
+                tau->error_occurred = tmpl_True;
+                tau->error_message =
+                    "\n\rError Encountered: rss_ringoccs\n"
+                    "\r\trssringoccs_Diffraction_Correction_Newton\n\n"
+                    "\rrealloc returned NULL for w_func.\n";
+
+                /*  The w_func array still points to allocated memory. Free   *
+                 *  this before returning to avoid memory leaks.              */
+                TMPL_FREE(w_func);
+                return;
+            }
+
+            /*  realloc was successful. Reset the pointer for the window.     */
+            w_func = tmp;
+
+            /*  Compute the window function for the new window width.         */
+            rssringoccs_Tau_Compute_Window(tau, w_func, nw_pts, center);
         }
 
-        /*  Compute the fresnel tranform about the current point.         */
-        fresnel_transform(tau, w_func, nw_pts, center);
+        /*  Compute the Newton transform about the current point.             */
+        newton_transform(tau, w_func, nw_pts, center);
 
-        /*  Increment pointers using pointer arithmetic.                  */
+        /*  Move to the next point. We are computing left-to-right, increment.*/
         center += 1;
     }
 
     /*  Free variables allocated by malloc.                                   */
-    free(w_func);
+    TMPL_FREE(w_func);
 }

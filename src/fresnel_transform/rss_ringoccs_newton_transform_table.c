afdec0c2ff76333cf22376492100b90ea65ec0d1
ryanmaguire <rmaguire@protonmail.com>
2026-01-15 20:01:15 -0500

    Adding new methods to the function table.

--- src/fresnel_transform/rss_ringoccs_newton_transform_table.c
+++ src/fresnel_transform/rss_ringoccs_newton_transform_table.c
@@ -4,8 +4,8 @@
 const rssringoccs_FresnelNewtonTransform
 rssringoccs_newton_transform_table[5] = {
     rssringoccs_Fresnel_Transform_Newton_Riemann,
-    rssringoccs_Fresnel_Transform_Newton_Linear_Filon,
-    NULL,
+    rssringoccs_Fresnel_Transform_Newton_Filon01,
+    rssringoccs_Fresnel_Transform_Newton_Filon11,
     rssringoccs_Fresnel_Transform_Elliptical_Newton,
     rssringoccs_Fresnel_Transform_Perturbed_Newton
 };

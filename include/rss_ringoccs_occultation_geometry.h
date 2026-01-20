83e0e447ed2594443640d84994fd4aafd36de4fb
ryanmaguire <rmaguire@protonmail.com>
2026-01-20 11:06:51 -0500

    Adding 'Deg' to function names to indicate output is in degrees.

--- include/rss_ringoccs_occultation_geometry.h
+++ include/rss_ringoccs_occultation_geometry.h
@@ -2,14 +2,24 @@
 #ifndef RSS_RINGOCCS_OCCULTATION_GEOMETRY_H
 #define RSS_RINGOCCS_OCCULTATION_GEOMETRY_H
 
-#include <libtmpl/include/tmpl.h>
+#include <libtmpl/include/tmpl_config.h>
+#include <libtmpl/include/types/tmpl_vec3_double.h>
 
 extern double
-rssringoccs_Double_Ring_Opening_Angle(const double time,
-                                      const tmpl_ThreeVectorDouble * const nhat,
-                                      const char * const spacecraft,
-                                      const char * const dsn,
-                                      const char * const kernels,
-                                      const char * const ref);
+rssringoccs_Double_Ring_Opening_Angle_Deg(
+    const double time,
+    const tmpl_ThreeVectorDouble * TMPL_RESTRICT const nhat,
+    const char * TMPL_RESTRICT const spacecraft,
+    const char * TMPL_RESTRICT const dsn,
+    const char * TMPL_RESTRICT const ref
+);
+
+extern double
+rssringoccs_Double_Elevation_Angle_Deg(
+    const double time,
+    const char * TMPL_RESTRICT const target,
+    const char * TMPL_RESTRICT const observer,
+    const char * TMPL_RESTRICT const ref
+);
 
 #endif

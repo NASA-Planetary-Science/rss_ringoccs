0b888dc19b8b728dff2b03ee65189f405705ae21
ryanmaguire <rmaguire@protonmail.com>
2026-03-05 20:30:45 -0500

    Removing old directories from setup.

--- setup.py
+++ setup.py
@@ -171,7 +171,6 @@ def static_library_paths():
 
 # Add all of the crssringoccs files (the C-Python API wrapper) to the build.
 add_directory("auxiliary")
-add_directory("common")
 add_directory("diffraction_correction")
 add_directory("extract_csv_data")
 add_directory("get_merged_csv_data")

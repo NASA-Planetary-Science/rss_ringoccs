06196f0a977533152c47a54a265698e5e27d7cb3
ryanmaguire <rmaguire@protonmail.com>
2026-03-10 10:03:57 -0400

    Adding license.

--- src/tau/rss_ringoccs_tau_create_from_dlp.c
+++ src/tau/rss_ringoccs_tau_create_from_dlp.c
@@ -1,3 +1,30 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       November 28, 2023                                             *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/02/21: Ryan Maguire                                                  *
+ *      Using new helper functions, reorganized a bit.                        *
+ ******************************************************************************/
+
 /*  NULL pointers are given here.                                             */
 #include <stdlib.h>
 

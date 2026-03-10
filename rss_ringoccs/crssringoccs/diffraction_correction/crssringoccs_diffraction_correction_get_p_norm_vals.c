0c27e852935bf7b8c523d1806b6dac30abd9749d
ryanmaguire <rmaguire@protonmail.com>
2026-03-10 12:08:17 -0400

    Adding getters and setters for p_norm_vals.

---
+++ rss_ringoccs/crssringoccs/diffraction_correction/crssringoccs_diffraction_correction_get_p_norm_vals.c
@@ -0,0 +1,106 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       March 10, 2026                                                *
+ ******************************************************************************/
+
+/*  Function prototype and typedefs for structs given here.                   */
+#include "../crssringoccs.h"
+
+/*  TMPL_MALLOC macro found here, providing C vs. C++ compatibility.          */
+#include <libtmpl/include/compat/tmpl_malloc.h>
+
+/*  Complex numbers and functions given here.                                 */
+#include <libtmpl/include/tmpl_complex.h>
+
+/*  NULL macro found here.                                                    */
+#include <stddef.h>
+
+PyObject *
+crssringoccs_DiffractionCorrection_Get_P_Norm_Vals(PyObject *op, void *closure)
+{
+    /*  Variable for the diffraction correction class instance.               */
+    crssringoccs_PyDiffrecObj *self;
+
+    /*  Buffer for the diffracted power data.                                 */
+    double *diffracted_power = NULL;
+
+    /*  Variable for indexing over the array.                                 */
+    size_t n;
+
+    /*  If the input is NULL, there's nothing to be done.                     */
+    if (!op)
+        return NULL;
+
+    /*  Get a pointer to the actual DiffractionCorrection instance.           */
+    self = (crssringoccs_PyDiffrecObj *)op;
+
+    /*  If we have already computed p_norm_vals, increment the reference      *
+     *  counter and return this object to the caller.                         */
+    if (self->p_norm_vals)
+    {
+        Py_INCREF(self->p_norm_vals);
+        return self->p_norm_vals;
+    }
+
+    /*  The real-valued p_norm_vals array is computed from the complex T_in   *
+     *  array. If T_in is NULL, set p_norm_vals to None.                      */
+    if (!self->tau->T_in)
+    {
+        PyObject *tmp = self->p_norm_vals;
+        Py_INCREF(Py_None);
+        self->p_norm_vals = Py_None;
+        Py_CLEAR(tmp);
+
+        return self->p_norm_vals;
+    }
+
+    /*  Otherwise, compute the diffracted power from the T_in array.          */
+    diffracted_power = TMPL_MALLOC(double, self->tau->n_used);
+
+    /*  Check to make sure malloc did not fail.                               */
+    if (!diffracted_power)
+        return PyErr_NoMemory();
+
+    /*  Otherwise, we have memory for the data. Compute the power.            */
+    for (n = 0; n < self->tau->n_used; ++n)
+    {
+        /*  The tau variables are shifted over to the start of the requested  *
+         *  range for processing. Shift the index for T_in and grab the data. */
+        const size_t index = self->tau->start + n;
+        const tmpl_ComplexDouble transmittance = self->tau->T_in[index];
+
+        /*  We have T_hat = sqrt(power) * exp(i phase). From this, the power  *
+         *  is given by the square of the modulus. Compute this.              */
+        diffracted_power[n] = tmpl_CDouble_Abs_Squared(transmittance);
+    }
+
+    /*  Wrap the data in a numpy array for ease of use.                       */
+    crssringoccs_Create_Real_Numpy_Array(
+        &self->p_norm_vals,             /*  The Python object.                */
+        diffracted_power,               /*  The real-valued C data.           */
+        crssringoccs_Capsule_Cleanup,   /*  Cleanup function for freeing data.*/
+        self->tau->n_used               /*  Number of points in the array.    */
+    );
+
+    /*  We are returning a reference to this new array. Increment the counter.*/
+    Py_INCREF(self->p_norm_vals);
+    return self->p_norm_vals;
+}
+/*  End of crssringoccs_DiffractionCorrection_Get_P_Norm_Vals.                */
---
+++ rss_ringoccs/crssringoccs/diffraction_correction/crssringoccs_diffraction_correction_set_p_norm_vals.c
@@ -0,0 +1,55 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       March 10, 2026                                                *
+ ******************************************************************************/
+
+/*  Function prototype and typedefs for structs given here.                   */
+#include "../crssringoccs.h"
+
+int
+crssringoccs_DiffractionCorrection_Set_P_Norm_Vals(PyObject *op,
+                                                   PyObject *value,
+                                                   void *closure)
+{
+    /*  Variable for the diffraction correction class instance.               */
+    crssringoccs_PyDiffrecObj *self;
+
+    /*  A NULL pointer for the first argument should be treated as an error.  */
+    if (!op)
+    {
+        PyErr_Format(
+            PyExc_RuntimeError,
+            "\n\rError Encountered: rss_ringoccs\n"
+            "\r\tcrssringoccs_DiffractionCorrection_Set_P_Norm_Vals\n\n"
+            "\rInput 'op' is NULL.\n\n"
+        );
+
+        return -1;
+    }
+
+    /*  Get a pointer to the actual DiffractionCorrection instance.           */
+    self = (crssringoccs_PyDiffrecObj *)op;
+
+    /*  Set p_norm_vals to the input value. There are no checks for what type *
+     *  of object this is, the user is free to set it to anything.            */
+    Py_XSETREF(self->p_norm_vals, Py_XNewRef(value));
+    return 0;
+}
+/*  End of crssringoccs_DiffractionCorrection_Set_P_Norm_Vals.                */

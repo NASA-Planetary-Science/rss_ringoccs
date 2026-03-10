4f44df5950f9008cd9e738e6b6ec2dbfdf820347
ryanmaguire <rmaguire@protonmail.com>
2026-03-10 08:43:22 -0400

    Adding license and comments.

--- src/dlp/rss_ringoccs_dlp_init.c
+++ src/dlp/rss_ringoccs_dlp_init.c
@@ -1,11 +1,36 @@
+/******************************************************************************
+ *                                  LICENSE                                   *
+ ******************************************************************************
+ *  This file is part of rss_ringoccs.                                        *
+ *                                                                            *
+ *  rss_ringoccs is free software: you can redistribute it and/or modify      *
+ *  it under the terms of the GNU General Public License as published by      *
+ *  the Free Software Foundation, either version 3 of the License, or         *
+ *  (at your option) any later version.                                       *
+ *                                                                            *
+ *  rss_ringoccs is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ *  GNU General Public License for more details.                              *
+ *                                                                            *
+ *  You should have received a copy of the GNU General Public License         *
+ *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
+ *  Date:       February 27, 2026                                             *
+ ******************************************************************************/
+
+/*  Booleans provided here.                                                   */
+#include <libtmpl/include/tmpl_bool.h>
+
+/*  Header file with the DLP object definition.                               */
+#include <rss_ringoccs/include/types/rss_ringoccs_dlpobj.h>
+
 /*  NULL pointer is given here.                                               */
 #include <stddef.h>
 
-/*  Booleans provided by this library.                                        */
-#include <libtmpl/include/tmpl.h>
-
-/*  Header file with the DLP definition and function prototype.               */
-#include <rss_ringoccs/include/rss_ringoccs_dlp.h>
+/*  Forward declaration / function prototype.                                 */
+extern void rssringoccs_DLP_Init(rssringoccs_DLPObj * const dlp);
 
 /*  Function for initializing all members in a dlp object to NULL.            */
 void rssringoccs_DLP_Init(rssringoccs_DLPObj * const dlp)
@@ -15,7 +40,11 @@ void rssringoccs_DLP_Init(rssringoccs_DLPObj * const dlp)
         return;
 
     /*  Initialize all pointers to NULL. This prevents things like double     *
-     *  double free's, leaking memory by calling malloc twice, etc.           */
+     *  free's, leaking memory by calling malloc twice, etc. The functions    *
+     *  that handle memory management with DLP objects will assume that       *
+     *  either these pointers are NULL, or we successfully initialized using  *
+     *  either malloc or calloc. Setting everything to NULL at the start      *
+     *  helps reduce the chance of errors.                                    */
     dlp->rho_km_vals = NULL;
     dlp->phi_deg_vals = NULL;
     dlp->B_deg_vals = NULL;

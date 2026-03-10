abdd2d22625ef69b08ff0e94cbcb4c7c73a6d893
ryanmaguire <rmaguire@protonmail.com>
2026-03-10 08:39:06 -0400

    Zeroing out error members, adding comments and explicit include for stddef.

--- src/dlp/rss_ringoccs_dlp_destroy.c
+++ src/dlp/rss_ringoccs_dlp_destroy.c
@@ -17,20 +17,37 @@
  *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
  ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
- *  Date:       January 5, 2020                                               *
+ *  Date:       February 27, 2026                                             *
  ******************************************************************************/
+
+/*  TMPL_FREE macro provided here, free's and nullifies a pointer.            */
 #include <libtmpl/include/compat/tmpl_free.h>
+
+/*  DLP object typedef found here and helper routines provided.               */
 #include <rss_ringoccs/include/rss_ringoccs_dlp.h>
 
+/*  NULL macro provided here.                                                 */
+#include <stddef.h>
+
+/*  Function for destroying all of the memory in a dlp object.                */
 void rssringoccs_DLP_Destroy(rssringoccs_DLPObj ** const dlp)
 {
+    /*  If the input pointer is NULL, do not try to access it. Just return.   */
     if (!dlp)
         return;
 
+    /*  Similarly, if the object pointer itself is NULL, simply return.       */
     if (!(*dlp))
         return;
 
+    /*  Free all of the memory allocated to the arrays inside the object.     */
     rssringoccs_DLP_Destroy_Members(*dlp);
+
+    /*  Set the error members to their zero values.                           */
+    (*dlp)->error_message = NULL;
+    (*dlp)->error_occurred = tmpl_False;
+
+    /*  Lastly, free and nullify the DLP object itself.                       */
     TMPL_FREE(*dlp);
 }
 /*  End of rssringoccs_DLP_Destroy.                                           */

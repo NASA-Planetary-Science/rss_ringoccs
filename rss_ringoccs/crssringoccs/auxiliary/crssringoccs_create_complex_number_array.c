488d6ea3d8676e1d26534c82e5d64f7b7a4f09c1
ryanmaguire <rmaguire@protonmail.com>
2026-03-12 09:43:17 -0400

    Adding more error checks for the numpy array functions.

--- rss_ringoccs/crssringoccs/auxiliary/crssringoccs_create_complex_number_array.c
+++ rss_ringoccs/crssringoccs/auxiliary/crssringoccs_create_complex_number_array.c
@@ -29,7 +29,8 @@ crssringoccs_Create_Complex_Numpy_Array(PyObject ** const py_ptr,
                                         void (*cleanup)(PyObject *),
                                         const size_t len)
 {
-    PyObject *arr, *tmp, *capsule;
+    PyObject *arr = NULL;
+    PyObject *capsule = NULL;
     npy_intp pylength = (npy_intp)len;
 
     /*  If the pointer has memory allocated to it, create a numpy array.      */
@@ -38,31 +39,65 @@ crssringoccs_Create_Complex_Numpy_Array(PyObject ** const py_ptr,
         /*  Numpy API function for creating numpy arrays from existing data.  */
         arr = PyArray_SimpleNewFromData(1, &pylength, NPY_CDOUBLE, ptr);
 
+        /*  Check if numpy failed to create a new array.                      */
+        if (!arr)
+        {
+            PyErr_Format(
+                PyExc_RuntimeError,
+                "\n\rError Encountered: rss_ringoccs\n"
+                "\r\tcrssringoccs_Create_Complex_Numpy_Array\n\n"
+                "\rPyArray_SimpleNewFromData returned NULL.\n\n"
+            );
+
+            *py_ptr = NULL;
+            return;
+        }
+
+        /*  Check if we need to attach a memory cleanup function to the array.*/
         if (cleanup)
         {
             /*  Create a capsule for this pointer so it is free'd when the    *
              *  array is destroyed. Avoids memory leaks for the end-user.     */
             capsule = PyCapsule_New(ptr, NULL, cleanup);
 
+            /*  Check if the new capsule is valid.                            */
+            if (!capsule)
+            {
+                PyErr_Format(
+                    PyExc_RuntimeError,
+                    "\n\rError Encountered: rss_ringoccs\n"
+                    "\r\tcrssringoccs_Create_Complex_Numpy_Array\n\n"
+                    "\rPyCapsule_New returned NULL.\n\n"
+                );
+
+                Py_CLEAR(arr);
+                *py_ptr = NULL;
+                return;
+            }
+
             /*  Link the array to the capsule. "del arr" in Python now free's *
              *  the memory allocated for the C pointer.                       */
             if (PyArray_SetBaseObject((PyArrayObject *)arr, capsule) == -1)
             {
+                PyErr_Format(
+                    PyExc_RuntimeError,
+                    "\n\rError Encountered: rss_ringoccs\n"
+                    "\r\tcrssringoccs_Create_Complex_Numpy_Array\n\n"
+                    "\rPyArray_SetBaseObject failed to set capsule.\n\n"
+                );
+
+                Py_CLEAR(capsule);
                 Py_CLEAR(arr);
+                *py_ptr = NULL;
                 return;
             }
         }
 
-        *py_ptr = Py_BuildValue("N", arr);
+        *py_ptr = arr;
     }
 
     /*  Otherwise set the variable to a "None" object.                        */
     else
-    {
-        tmp = *py_ptr;
-        Py_INCREF(Py_None);
-        *py_ptr = Py_None;
-        Py_CLEAR(tmp);
-    }
+        MAKE_NONE(*py_ptr);
 }
 /*  End of crssringoccs_Create_Complex_Numpy_Array.                           */
--- rss_ringoccs/crssringoccs/auxiliary/crssringoccs_create_real_number_array.c
+++ rss_ringoccs/crssringoccs/auxiliary/crssringoccs_create_real_number_array.c
@@ -16,12 +16,6 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
  ******************************************************************************
- *                        Diffraction Correction Class                        *
- ******************************************************************************
- *  Purpose:                                                                  *
- *      Defines the DiffractionCorrection class for rss_ringoccs. This uses   *
- *      the C-Python API to build an extension module containing the class.   *
- ******************************************************************************
  *  Author:     Ryan Maguire                                                  *
  *  Date:       June 22, 2019                                                 *
  ******************************************************************************/
@@ -35,40 +29,75 @@ crssringoccs_Create_Real_Numpy_Array(PyObject ** const py_ptr,
                                      void (*cleanup)(PyObject *),
                                      const size_t len)
 {
-    PyObject *arr, *tmp, *capsule;
+    PyObject *arr = NULL;
+    PyObject *capsule = NULL;
     npy_intp pylength = (npy_intp)len;
 
     /*  If the pointer has memory allocated to it, create a numpy array.      */
-    if (ptr != NULL)
+    if (ptr)
     {
         /*  Numpy API function for creating numpy arrays from existing data.  */
         arr = PyArray_SimpleNewFromData(1, &pylength, NPY_DOUBLE, ptr);
 
+        /*  Check if numpy failed to create a new array.                      */
+        if (!arr)
+        {
+            PyErr_Format(
+                PyExc_RuntimeError,
+                "\n\rError Encountered: rss_ringoccs\n"
+                "\r\tcrssringoccs_Create_Real_Numpy_Array\n\n"
+                "\rPyArray_SimpleNewFromData returned NULL.\n\n"
+            );
+
+            *py_ptr = NULL;
+            return;
+        }
+
+        /*  Check if we need to attach a memory cleanup function to the array.*/
         if (cleanup)
         {
             /*  Create a capsule for this pointer so it is free'd when the    *
              *  array is destroyed. Avoids memory leaks for the end-user.     */
             capsule = PyCapsule_New(ptr, NULL, cleanup);
 
+            /*  Check if the new capsule is valid.                            */
+            if (!capsule)
+            {
+                PyErr_Format(
+                    PyExc_RuntimeError,
+                    "\n\rError Encountered: rss_ringoccs\n"
+                    "\r\tcrssringoccs_Create_Real_Numpy_Array\n\n"
+                    "\rPyCapsule_New returned NULL.\n\n"
+                );
+
+                Py_CLEAR(arr);
+                *py_ptr = NULL;
+                return;
+            }
+
             /*  Link the array to the capsule. "del arr" in Python now free's *
              *  the memory allocated for the C pointer.                       */
             if (PyArray_SetBaseObject((PyArrayObject *)arr, capsule) == -1)
             {
+                PyErr_Format(
+                    PyExc_RuntimeError,
+                    "\n\rError Encountered: rss_ringoccs\n"
+                    "\r\tcrssringoccs_Create_Real_Numpy_Array\n\n"
+                    "\rPyArray_SetBaseObject failed to set capsule.\n\n"
+                );
+
+                Py_CLEAR(capsule);
                 Py_CLEAR(arr);
+                *py_ptr = NULL;
                 return;
             }
         }
 
-        *py_ptr = Py_BuildValue("N", arr);
+        *py_ptr = arr;
     }
 
     /*  Otherwise set the variable to a "None" object.                        */
     else
-    {
-        tmp = *py_ptr;
-        Py_INCREF(Py_None);
-        *py_ptr = Py_None;
-        Py_XDECREF(tmp);
-    }
+        MAKE_NONE(*py_ptr);
 }
-/*  End of crssringoccs_Create_Numpy_Array.                                   */
+/*  End of crssringoccs_Create_Real_Numpy_Array.                              */

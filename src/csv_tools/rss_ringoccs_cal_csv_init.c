7771dc8978dea2aa1973ffd671da77d3d94f8d59
ryanmaguire <rmaguire@protonmail.com>
2026-03-17 16:16:21 -0400

    Adding docstring, cleaning up a bit.

--- src/csv_tools/rss_ringoccs_cal_csv_init.c
+++ src/csv_tools/rss_ringoccs_cal_csv_init.c
@@ -16,28 +16,68 @@
  *  You should have received a copy of the GNU General Public License         *
  *  along with rss_ringoccs.  If not, see <https://www.gnu.org/licenses/>.    *
  ******************************************************************************
+ *                          rss_ringoccs_cal_csv_init                         *
+ ******************************************************************************
  *  Purpose:                                                                  *
  *      Initialize all members of the Cal CSV object to their zero values.    *
  ******************************************************************************
- *  Author:     Ryan Maguire, Wellesley College                               *
+ *                             DEFINED FUNCTIONS                              *
+ ******************************************************************************
+ *  Function Name:                                                            *
+ *      rssringoccs_CalCSV_Init                                               *
+ *  Purpose:                                                                  *
+ *      Initializes all members of a CalCSV object to their zero values.      *
+ *  Arguments:                                                                *
+ *      cal (rssringoccs_CalCSV * const):                                     *
+ *          A pointer to the Calibration CSV object we are initializing.      *
+ *  Output:                                                                   *
+ *      None (void).                                                          *
+ *  Called Functions:                                                         *
+ *      None.                                                                 *
+ *  Method:                                                                   *
+ *      Set each pointer to NULL, each Boolean to False, and each number to 0.*
+ *  Notes:                                                                    *
+ *      1.) If cal is NULL, nothing is done.                                  *
+ ******************************************************************************
+ *                                DEPENDENCIES                                *
+ ******************************************************************************
+ *  1.) tmpl_free.h:                                                          *
+ *          Header file providing the TMPL_FREE macro.                        *
+ *  2.) rss_ringoccs_calcsv.h:                                                *
+ *          Header file containing the rssringoccs_CalCSV typedef.            *
+ *  3.) stddef.h:                                                             *
+ *          Standard header file providing the NULL macro.                    *
+ ******************************************************************************
+ *  Author:     Ryan Maguire                                                  *
  *  Date:       September 1, 2024                                             *
+ ******************************************************************************
+ *                              Revision History                              *
+ ******************************************************************************
+ *  2026/03/17: Ryan Maguire                                                  *
+ *      Added docstring, cleaned up a bit.                                    *
  ******************************************************************************/
+
+/*  Booleans (True and False) provided here.                                  */
+#include <libtmpl/include/tmpl_bool.h>
+
+/*  rssringoccs_CalCSV typedef found here.                                    */
+#include <rss_ringoccs/include/types/rss_ringoccs_calcsv.h>
 
 /*  NULL is found here.                                                       */
 #include <stddef.h>
 
-/*  Booleans provided here.                                                   */
-#include <libtmpl/include/tmpl_bool.h>
-
-/*  rssringoccs_CalCSV typedef here, and function prototype given.            */
-#include <rss_ringoccs/include/rss_ringoccs_csv_tools.h>
+/*  Forward declaration / function prototype.                                 */
+extern void rssringoccs_CalCSV_Init(rssringoccs_CalCSV * const cal);
 
 /* Sets all variables in a CAL CSV to their default values.                   */
-void rssringoccs_CalCSV_Init(rssringoccs_CalCSV *cal)
+void rssringoccs_CalCSV_Init(rssringoccs_CalCSV * const cal)
 {
     /*  If the pointer is NULL, there's nothing to do. Simply return.         */
     if (!cal)
         return;
+
+    /*  No history yet. Set this to NULL.                                     */
+    cal->history = NULL;
 
     /*  All arrays should be empty, for now.                                  */
     cal->t_oet_spm_vals = NULL;
@@ -49,8 +89,5 @@ void rssringoccs_CalCSV_Init(rssringoccs_CalCSV *cal)
     /*  And no error has occurred, yet.                                       */
     cal->error_message = NULL;
     cal->error_occurred = tmpl_False;
-
-    /*  No history yet. Set this to NULL.                                     */
-    cal->history = NULL;
 }
 /*  End of rssringoccs_CalCSV_Init.                                           */

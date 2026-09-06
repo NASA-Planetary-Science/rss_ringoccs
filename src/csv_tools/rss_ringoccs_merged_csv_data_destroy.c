a8596403e19c9644187fa168f5342faebdb57a85
ryanmaguire <rmaguire@protonmail.com>
2026-09-06 09:59:38 -0400

    Cleaning deallocating functions for DLPM object.

--- src/csv_tools/rss_ringoccs_merged_csv_data_destroy.c
+++ src/csv_tools/rss_ringoccs_merged_csv_data_destroy.c
@@ -24,46 +24,36 @@
  *  Date:       September 30, 2024                                            *
  ******************************************************************************/
 
-/*  free is found here, as is NULL.                                           */
-#include <stdlib.h>
+/*  Booleans (True and False) provided here.                                  */
+#include <libtmpl/include/tmpl_bool.h>
+
+/*  Provides the TMPL_FREE macro for freeing a pointer and setting it to NULL.*/
+#include <libtmpl/include/compat/tmpl_free.h>
 
 /*  rssringoccs_MergedCSVData typedef here, and function prototype given.     */
 #include <rss_ringoccs/include/rss_ringoccs_csv_tools.h>
 
 /*  Function for freeing the memory in a MergedCSVData object.                */
-void rssringoccs_MergedCSVData_Destroy(rssringoccs_MergedCSVData **dlpm)
+void rssringoccs_MergedCSVData_Destroy(rssringoccs_MergedCSVData ** const dlpm)
 {
-    /*  Variable for a pointer to the DLPCSV object.                          */
-    rssringoccs_MergedCSVData *dlpm_inst;
-
     /*  If the input pointer is NULL, do not attempt to free it.              */
     if (!dlpm)
         return;
 
-    /*  Get a pointer to the DLPCSV object.                                   */
-    dlpm_inst = *dlpm;
-
     /*  If this pointer is NULL, there is nothing to free.                    */
-    if (!dlpm_inst)
+    if (!(*dlpm))
         return;
 
     /*  Free all of the members inside the DLPM object.                       */
-    rssringoccs_MergedCSVData_Destroy_Members(dlpm_inst);
+    rssringoccs_MergedCSVData_Destroy_Members(*dlpm);
 
-    /*  If an error occurred, the error_message variable is malloced and a    *
-     *  string is stored inside of it. Free this pointer if this is true.     */
-    if (dlpm_inst->error_message != NULL)
-    {
-        free(dlpm_inst->error_message);
-
-        /*  Set the pointer to NULL to avoid freeing it twice.                */
-        dlpm_inst->error_message = NULL;
-    }
+    /*  The error_message member is a pointer to a constant string, it does   *
+     *  not need to be freed. Set the pointer to NULL to avoid reading it.    */
+    (*dlpm)->error_occurred = tmpl_False;
+    (*dlpm)->error_message = NULL;
 
     /*  Free the DLPCSV object pointer and set it to NULL to prevent trying   *
      *  to free the pointer twice.                                            */
-    free(dlpm_inst);
-    *dlpm = NULL;
-    return;
+    TMPL_FREE(*dlpm);
 }
 /*  End of rssringoccs_MergedCSVData_Destroy.                                 */

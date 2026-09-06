bd7e60108241848260b7c7877f889debf066fd76
ryanmaguire <rmaguire@protonmail.com>
2026-09-06 13:04:24 -0400

    Adding necessary includes.

--- src/tau/rss_ringoccs_tau_check_periapse.c
+++ src/tau/rss_ringoccs_tau_check_periapse.c
@@ -81,6 +81,9 @@
 /*  Booleans provided here.                                                   */
 #include <libtmpl/include/tmpl_bool.h>
 
+/*  Constants like pi and sqrt(2) are found here.                             */
+#include <libtmpl/include/constants/tmpl_math_constants.h>
+
 /*  tmpl_Double_Is_NaN and tmpl_Double_Is_Inf declared here.                  */
 #include <libtmpl/include/tmpl_math.h>
 

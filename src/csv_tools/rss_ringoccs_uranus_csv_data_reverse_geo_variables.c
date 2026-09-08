d0473d360268a092cbbddeafc1ee9a60df44b081
ryanmaguire <rmaguire@protonmail.com>
2026-09-08 06:19:09 -0400

    Removing tmpl_string.h dependency.

--- src/csv_tools/rss_ringoccs_uranus_csv_data_reverse_geo_variables.c
+++ src/csv_tools/rss_ringoccs_uranus_csv_data_reverse_geo_variables.c
@@ -1,16 +1,10 @@
-
-
 #include <libtmpl/include/tmpl_bool.h>
-#include <libtmpl/include/tmpl_string.h>
 #include <libtmpl/include/tmpl_math.h>
 #include <rss_ringoccs/include/rss_ringoccs_csv_tools.h>
 
 void
 rssringoccs_UranusCSVData_Reverse_Geo_Variables(rssringoccs_UranusCSVData *csv)
 {
-    /*  Buffer for an error message, should an error occur.                   */
-    char err_mes[1024];
-
     if (!csv)
         return;
 
@@ -20,11 +14,10 @@ rssringoccs_UranusCSVData_Reverse_Geo_Variables(rssringoccs_UranusCSVData *csv)
     if (!csv->geo)
     {
         csv->error_occurred = tmpl_True;
-        csv->error_message = tmpl_String_Duplicate(
+        csv->error_message =
             "\nError Encountered: rss_ringoccs\n"
             "\trssringoccs_UranusCSVData_Reverse_Geo_Variables\n\n"
-            "csv->geo is NULL. Aborting\n"
-        );
+            "csv->geo is NULL.\n\n";
 
         return;
     }
@@ -32,32 +25,10 @@ rssringoccs_UranusCSVData_Reverse_Geo_Variables(rssringoccs_UranusCSVData *csv)
     if (csv->geo->error_occurred)
     {
         csv->error_occurred = tmpl_True;
-
-        /*  Keep track of error messages. Copy the previous geo message.      */
-        if (csv->geo->error_message)
-        {
-            sprintf(
-                err_mes,
-                "\nError Encountered: rss_ringoccs\n"
-                "\trssringoccs_UranusCSVData_Reverse_Geo_Variables\n\n"
-                "csv->geo has 'error_occurred' set to True.\n"
-                "csv->geo set the following message:\n\n%s",
-                csv->geo->error_message
-            );
-
-            csv->error_message = tmpl_String_Duplicate(err_mes);
-        }
-
-        /*  If the function failed and no error message was set, it is likely *
-         *  malloc failed. Give a generic message.                            */
-        else
-        {
-            csv->error_message = tmpl_String_Duplicate(
-                "\nError Encountered: rss_ringoccs\n"
-                "\trssringoccs_UranusCSVData_Reverse_Geo_Variables\n\n"
-                "csv->geo has 'error_occurred' set to True.\n"
-            );
-        }
+        csv->error_message =
+            "\nError Encountered: rss_ringoccs\n"
+            "\trssringoccs_UranusCSVData_Reverse_Geo_Variables\n\n"
+            "csv->geo has error_occurred set to True.\n\n";
 
         return;
     }
